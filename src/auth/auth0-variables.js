58bcccfa811af26b3cf5d65d980dd3fb8d8297fc
mfrank <mfrankwork@gmail.com>
2018-04-08 19:31:40 +0000

    Convert auth0-variables to VUE_APP env variables

--- src/auth/auth0-variables.js
+++ src/auth/auth0-variables.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
-    clientId: process.env.AUTH0_CLIENT_ID || '{CLIENT_ID}',
-    domain: process.env.AUTH0_DOMAIN || '{DOMAIN}',
-    callbackUrl: process.env.C9_HOSTNAME ? 'http://localhost:8080/callback' : 'http://localhost:3000/callback',
-    apiUrl: process.env.AUTH0_API_ID || '{API_IDENTIFIER}'
+    clientId: process.env.VUE_APP_AUTH0_CLIENT_ID || '{CLIENT_ID}',
+    domain: process.env.VUE_APP_AUTH0_DOMAIN || '{DOMAIN}',
+    callbackUrl: process.env.VUE_APP_HOSTNAME + '#/callback' || 'http://localhost:4200/#/callback',
+    apiUrl: process.env.VUE_APP_AUTH0_API_ID || '{API_IDENTIFIER}'
 }
