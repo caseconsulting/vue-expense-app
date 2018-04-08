8e7b18152b74dd88bc0db286f5818c1391d2a633
Paul Correll <pcorrell@consultwithcase.com>
2018-04-08 20:03:24 +0000

    added some CRUD cruft that needs to be fleshed out

--- src/views/ExpenseTypes.vue
+++ src/views/ExpenseTypes.vue
@@ -1,18 +1,49 @@
 <template>
   <div>
     <h1>Expense Types</h1>
-  <ul v-if="expenseTypes && expenseTypes.length">
-    <li v-for="expTypes of expenseTypes">
-      <p><strong>{{expTypes.budgetName}}</strong></p>
-      <p>{{expTypes.description}}</p>
-    </li>
-  </ul>
-
-  <ul v-if="errors && errors.length">
-    <li v-for="error of errors">
-      {{error.message}}
-    </li>
-  </ul>
+    <b-alert :show="loading" variant="info">Loading...</b-alert>
+    <b-row>
+      <b-col>
+        <table class="table table-striped">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Budget Name</th>
+              <th>Description</th>
+              <th>Budget</th>
+              <th>&nbsp;</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="expTypes in expenseTypes" :key="expTypes.id">
+              <td>{{ expTypes.id }}</td>
+              <td>{{ expTypes.budgetName }}</td>
+              <td>{{ expTypes.description }}</td>
+              <td>{{ expTypes.budget }}</td>
+              <td class="text-right">
+                <!--<a href="#" @click.prevent="populateExpenseTypeToEdit(expType)">Edit</a> - -->
+                <!--<a href="#" @click.prevent="deleteExpenseType(expType.id)">Delete</a>-->
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </b-col>
+      <b-col lg="3">
+        <!--<b-card :title="(model.id ? 'Edit Post ID#' + model.id : 'New Post')">-->
+        <!--  <form @submit.prevent="savePost">-->
+        <!--    <b-form-group label="Title">-->
+        <!--      <b-form-input type="text" v-model="model.title"></b-form-input>-->
+        <!--    </b-form-group>-->
+        <!--    <b-form-group label="Body">-->
+        <!--      <b-form-textarea rows="4" v-model="model.body"></b-form-textarea>-->
+        <!--    </b-form-group>-->
+        <!--    <div>-->
+        <!--      <b-btn type="submit" variant="success">Save Post</b-btn>-->
+        <!--    </div>-->
+        <!--  </form>-->
+        <!--</b-card>-->
+      </b-col>
+    </b-row>
   </div>
 </template>
 
