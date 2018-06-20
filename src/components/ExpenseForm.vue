898dcb257e2d2f68b1a54934eb4c176681a99ec9
Jacob <jacobboisseau@gmail.com>
2018-06-20 13:41:41 -0400

    removing-commented-junk: removed comments

--- src/components/ExpenseForm.vue
+++ src/components/ExpenseForm.vue
@@ -10,18 +10,11 @@
       </b-input-group>
     </b-form-group>
     <b-form-group label="Expense Type">
-      <v-select @input="onInput()" :items="expenseTypes" :filter="customFilter" v-model="model.expenseTypeId" item-text="text" label="Select" autocomplete></v-select>
-      <!-- <vs-select vs-autocomplete class="selectExample" label="Expense Type" v-model="model.expenseTypeId" :options="expenseTypes"></vs-select> -->
+      <v-select :items="expenseTypes" :filter="customFilter" v-model="model.expenseTypeId" item-text="text" label="Select" autocomplete></v-select>
+
     </b-form-group>
     <b-form-group label="Employee">
-      <!-- <vs-select
-    vs-autocomplete
-    class="selectExample"
-      label="Employees"
-      v-model="model.userId"
-      :options="employees"
-      ></vs-select> -->
-      <v-select @input="onInput()" :items="employees" :filter="customFilter" v-model="model.userId" item-text="text" label="Select" autocomplete></v-select>
+      <v-select :items="employees" :filter="customFilter" v-model="model.userId" item-text="text" label="Select" autocomplete></v-select>
     </b-form-group>
     <b-form-group label="Note">
       <b-form-textarea rows="2" type="text" v-model="model.note"></b-form-textarea>
@@ -34,18 +27,15 @@
     </b-form-group>
 
     <div slot="footer" class="all-footer-buttons">
-      <!-- <vs-button vs-type="danger-gradient" vs-icon="delete" @click="$emit('delete-form')" variant="default" class="mr-1">Delete</vs-button> -->
       <v-btn outline color="error" @click="$emit('delete-form')">
         <icon class="mr-1" name="trash"></icon>
         Delete
       </v-btn>
       <div class="footer-buttons">
-        <!-- <vs-button vs-type="dark-line-down" vs-icon="cancel" @click="$emit('clear-form')" variant="default" class="mr-1">Cancel</vs-button> -->
         <v-btn color="white" @click="$emit('clear-form')">
           <icon class="mr-1" name="ban"></icon>
           Cancel
         </v-btn>
-        <!-- <vs-button vs-type="success-gradient" vs-icon="save" @click="$emit('submit-form')" type="submit" variant="success">Save Expense</vs-button> -->
         <v-btn outline color="success" @click="$emit('submit-form')" type="submit">
           <icon class="mr-1" name="save"></icon>
           Submit
