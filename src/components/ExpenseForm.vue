f98754d8c0138ff27c745165916b00ebaf680fac
Fahad <fahada95@vt.edu>
2018-06-21 10:39:30 -0400

    expense-form-fixes: reimburse date added as well

--- src/components/ExpenseForm.vue
+++ src/components/ExpenseForm.vue
@@ -1,69 +1,36 @@
 <template>
 <!-- <form @submit.prevent=""> -->
-  <v-card :header="(model.id ? 'Edit Expense' : 'New Expense')">
-    <v-text-field
-    v-model="model.description"
-    :counter="1"
-    label="Description"
-    data-vv-name="Description"
-    required
-  ></v-text-field>
-  <v-text-field
-  v-model="model.cost"
-  :counter="1"
-  label="Cost"
-  data-vv-name="Cost"
-  required
-></v-text-field>
-      <v-select :items="expenseTypes" :filter="customFilter" v-model="model.expenseTypeId" item-text="text" label="Expense Type" autocomplete></v-select>
-      <v-select :items="employees" :filter="customFilter" v-model="model.userId" item-text="text" label="Employee" autocomplete></v-select>
-      <v-text-field
-      v-model="model.note"
-      label="Notes"
-      data-vv-name="Description"
-      multi-line
-    ></v-text-field>
-    <v-menu
-  ref="menu1"
-  :close-on-content-click="false"
-  v-model="menu1"
-  :nudge-right="40"
-  lazy
-  transition="scale-transition"
-  offset-y
-  full-width
-  max-width="290px"
-  min-width="290px"
->
-  <v-text-field
-    slot="activator"
-    v-model="dateFormatted"
-    label="Date"
-    hint="MM/DD/YYYY format"
-    persistent-hint
-    prepend-icon="event"
-    @blur="model.purchaseDate = parseDate(dateFormatted)"
-  ></v-text-field>
-  <v-date-picker v-model="model.purchaseDate" no-title @input="menu1 = false"></v-date-picker>
-</v-menu>
-      <datepicker v-model="model.reimbursedDate" format="MM/dd/yyyy" initial-view="year" :bootstrap-styling="true" :typeable=true :placeholder=" 'MM/DD/YYYY' "></datepicker>
-    <!-- <div slot="footer" class="all-footer-buttons"> -->
-      <v-btn outline color="error" @click="$emit('delete-form')">
-        <icon class="mr-1" name="trash"></icon>
-        Delete
-      </v-btn>
-      <!-- <div class="footer-buttons"> -->
-        <v-btn color="white" @click="$emit('clear-form')">
-          <icon class="mr-1" name="ban"></icon>
-          Cancel
-        </v-btn>
-        <v-btn outline color="success" @click="$emit('submit-form')" type="submit">
-          <icon class="mr-1" name="save"></icon>
-          Submit
-        </v-btn>
-      <!-- </div> -->
-    <!-- </div> -->
-  </v-card>
+<v-card :header="(model.id ? 'Edit Expense' : 'New Expense')">
+  <v-text-field v-model="model.description" :counter="1" label="Description" data-vv-name="Description" required></v-text-field>
+  <v-text-field v-model="model.cost" :counter="1" label="Cost" data-vv-name="Cost" required></v-text-field>
+  <v-select :items="expenseTypes" :filter="customFilter" v-model="model.expenseTypeId" item-text="text" label="Expense Type" autocomplete></v-select>
+  <v-select :items="employees" :filter="customFilter" v-model="model.userId" item-text="text" label="Employee" autocomplete></v-select>
+  <v-menu ref="menu1" :close-on-content-click="false" v-model="menu1" :nudge-right="40" lazy transition="scale-transition" offset-y full-width max-width="290px" min-width="290px">
+    <v-text-field slot="activator" v-model="purchaseDateFormatted" label="Purchase Date" hint="MM/DD/YYYY format" persistent-hint prepend-icon="event" @blur="model.purchaseDate = parseDate(purchaseDateFormatted)"></v-text-field>
+    <v-date-picker v-model="model.purchaseDate" no-title @input="menu1 = false"></v-date-picker>
+  </v-menu>
+  <v-menu ref="menu2" :close-on-content-click="false" v-model="menu2" :nudge-right="40" lazy transition="scale-transition" offset-y full-width max-width="290px" min-width="290px">
+    <v-text-field slot="activator" v-model="reimbursedDateFormatted" label="Reimburse Date" hint="MM/DD/YYYY format" persistent-hint prepend-icon="event" @blur="model.reimbursedDate = parseDate(reimbursedDateFormatted)"></v-text-field>
+    <v-date-picker v-model="model.reimbursedDate" no-title @input="menu1 = false"></v-date-picker>
+  </v-menu>
+  <v-text-field v-model="model.note" label="Notes" data-vv-name="Description" multi-line></v-text-field>
+  <!-- <div slot="footer" class="all-footer-buttons"> -->
+  <v-btn outline color="error" @click="$emit('delete-form')">
+    <icon class="mr-1" name="trash"></icon>
+    Delete
+  </v-btn>
+  <!-- <div class="footer-buttons"> -->
+  <v-btn color="white" @click="$emit('clear-form')">
+    <icon class="mr-1" name="ban"></icon>
+    Cancel
+  </v-btn>
+  <v-btn outline color="success" @click="$emit('submit-form')" type="submit">
+    <icon class="mr-1" name="save"></icon>
+    Submit
+  </v-btn>
+  <!-- </div> -->
+  <!-- </div> -->
+</v-card>
 <!-- </form> -->
 </template>
 
@@ -77,12 +44,14 @@ export default {
   data() {
     return {
       date: null,
-      dateFormatted: null,
+      purchaseDateFormatted: null,
+      reimbursedDateFormatted: null,
       selectedExpenseType: {},
       expenseTypes: [],
       selectedEmployee: {},
       employees: [],
       menu1: false,
+      menu2: false,
       customFilter(item, queryText, itemText) {
         const hasValue = val => (val != null ? val : '');
         const text = hasValue(item.text);
@@ -98,15 +67,13 @@ export default {
   },
   watch: {
     'model.purchaseDate': function(val) {
-      this.dateFormatted = this.formatDate(this.model.purchaseDate);
+      this.purchaseDateFormatted = this.formatDate(this.model.purchaseDate);
+    },
+    'model.reimbursedDate': function(val) {
+      this.reimbursedDateFormatted = this.formatDate(this.model.reimbursedDate);
     }
   },
   props: ['model'],
-  computed: {
-    computedDateFormatted() {
-      return this.formatDate(this.model.purchaseDate);
-    }
-  },
   methods: {
     formatDate(date) {
       if (!date) return null;
@@ -124,7 +91,10 @@ export default {
   async created() {
     let expenseTypes = await api.getItems(api.EXPENSE_TYPES);
     this.expenseTypes = expenseTypes.map(expenseType => {
-      return { text: expenseType.budgetName, value: expenseType.id };
+      return {
+        text: expenseType.budgetName,
+        value: expenseType.id
+      };
     });
 
     let employees = await api.getItems(api.EMPLOYEES);
