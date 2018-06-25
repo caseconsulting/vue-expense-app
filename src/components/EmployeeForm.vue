ac7cc1da0359d5ffc6f15f6f03e03eb9d528e39a
Fahad <fahada95@vt.edu>
2018-06-25 15:32:16 -0400

    153-Convert-from-Bootstrap-Library-to-Vuetify-Library: converted employee table to vuetify

--- src/components/EmployeeForm.vue
+++ src/components/EmployeeForm.vue
@@ -1,28 +1,30 @@
 <template>
-  <form @submit.prevent="saveEmployee">
-    <b-card :header="(model.id ? 'Edit Employee' : 'New Employee')">
-      <b-form-group label="Employee ID">
-        <b-form-input type="text" v-model="model.empId"></b-form-input>
-      </b-form-group>
-      <b-form-group label="First Name">
-        <b-form-input type="text" v-model="model.firstName"></b-form-input>
-      </b-form-group>
-      <b-form-group label="Middle Name">
-        <b-form-input type="text" v-model="model.middleName"></b-form-input>
-      </b-form-group>
-      <b-form-group label="Last Name">
-        <b-form-input type="text" v-model="model.lastName"></b-form-input>
-      </b-form-group>
-      <b-form-group label="Hire Date">
-        <datepicker v-model="model.hireDate" format="MM/dd/yyyy" initial-view="month" :bootstrap-styling="true"
-        :typeable=true :placeholder=" 'MM/DD/YYYY' "></datepicker>
-      </b-form-group>
-      <div slot="footer" class="footer-buttons">
-        <b-btn @click.prevent="clearEmployeeToEdit()" variant="default" class="mr-1">Cancel</b-btn>
-        <b-btn type="submit" variant="success">Save Employee</b-btn>
-      </div>
-    </b-card>
-  </form>
+<v-card hover :header="(model.id ? 'Edit Expense' : 'New Expense')">
+  <v-card-title>
+    <h3>Expense Form</h3>
+  </v-card-title>
+  <v-container fluid>
+<v-form ref="form" v-model="valid" lazy-validation >
+  <v-text-field v-model="model.description" :rules="descriptionRules" label="Description" data-vv-name="Description" required></v-text-field>
+  <v-text-field v-model="model.cost" :rules="costRules" label="Cost" data-vv-name="Cost" required></v-text-field>
+  <v-select :items="expenseTypes" :rules="componentRules" :filter="customFilter" v-model="model.expenseTypeId" item-text="text" label="Expense Type" autocomplete></v-select>
+  <v-select :items="employees" :rules="componentRules" :filter="customFilter" v-model="model.userId" item-text="text" label="Employee" autocomplete></v-select>
+  <v-menu ref="menu1" :close-on-content-click="false" v-model="menu1" :nudge-right="40" lazy transition="scale-transition" offset-y full-width max-width="290px" min-width="290px">
+    <v-text-field slot="activator" v-model="purchaseDateFormatted" :rules="componentRules" label="Purchase Date" hint="MM/DD/YYYY format" persistent-hint prepend-icon="event" @blur="model.purchaseDate = parseDate(purchaseDateFormatted)"></v-text-field>
+    <v-date-picker v-model="model.purchaseDate" no-title @input="menu1 = false"></v-date-picker>
+  </v-menu>
+  <v-menu ref="menu2" :close-on-content-click="false" v-model="menu2" :nudge-right="40" lazy transition="scale-transition" offset-y full-width max-width="290px" min-width="290px">
+    <v-text-field slot="activator" v-model="reimbursedDateFormatted" label="Reimburse Date" hint="MM/DD/YYYY format" persistent-hint prepend-icon="event" @blur="model.reimbursedDate = parseDate(reimbursedDateFormatted)"></v-text-field>
+    <v-date-picker v-model="model.reimbursedDate" no-title @input="menu2 = false"></v-date-picker>
+  </v-menu>
+  <v-text-field v-model="model.note" label="Notes" data-vv-name="Description" multi-line></v-text-field>
+  <v-btn outline color="error" @click="$emit('delete-form')">
+    <icon class="mr-1" name="trash"></icon>Delete</v-btn>
+  <v-btn color="white" @click="clear"><icon class="mr-1" name="ban"></icon>Cancel</v-btn>
+  <v-btn outline color="success" @click="submit" :disabled="!valid"><icon class="mr-1" name="save"></icon>Submit</v-btn>
+  </v-form>
+  </v-container>
+</v-card>
 </template>
 
 <script>
@@ -34,21 +36,94 @@ export default {
   },
   data() {
     return {
+      date: null,
+      purchaseDateFormatted: null,
+      reimbursedDateFormatted: null,
       selectedExpenseType: {},
       expenseTypes: [],
       selectedEmployee: {},
-      employees: []
+      employees: [],
+      menu1: false,
+      menu2: false,
+      descriptionRules: [v => !!v || 'Description is required'],
+      costRules: [
+        v => !!v || 'Cost is required',
+        v => /^\d+$/.test(v) || 'Cost must be a number'
+      ],
+      componentRules: [v => !!v || 'Something must be selected'],
+      valid: false,
+
+      // TODO: Move this filter to methods
+      customFilter(item, queryText, itemText) {
+        const hasValue = val => (val != null ? val : '');
+        const text = hasValue(item.text);
+        const query = hasValue(queryText);
+        return (
+          text
+            .toString()
+            .toLowerCase()
+            .indexOf(query.toString().toLowerCase()) > -1
+        );
+      }
     };
   },
+  watch: {
+    'model.purchaseDate': function(val) {
+      this.purchaseDateFormatted = this.formatDate(this.model.purchaseDate);
+    },
+    'model.reimbursedDate': function(val) {
+      this.reimbursedDateFormatted = this.formatDate(this.model.reimbursedDate);
+    }
+  },
   props: ['model'],
+  // watch: {
+  //   model: function(val) {
+  //     console.log('val', val);
+  //     if (val === { purchaseDate: null, reimbursedDate: null }) {
+  //       console.log('matching value');
+  //       this.$refs.form.reset();
+  //     }
+  //   }
+  // },
+  methods: {
+    formatDate(date) {
+      if (!date) return null;
+
+      const [year, month, day] = date.split('-');
+      return `${month}/${day}/${year}`;
+    },
+    parseDate(date) {
+      if (!date) return null;
+
+      const [month, day, year] = date.split('/');
+      return `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`;
+    },
+    async submit() {
+      if (this.$refs.form.validate()) {
+        if (this.model.id) {
+          await api.updateItem(api.EXPENSES, this.model.id, this.model);
+        } else {
+          console.log('Creating new item');
+          await api.createItem(api.EXPENSES, this.model);
+        }
+        this.clear();
+        this.$emit('submit-form');
+      }
+    },
+    clear() {
+      this.$refs.form.reset();
+    }
+  },
   async created() {
     let expenseTypes = await api.getItems(api.EXPENSE_TYPES);
     this.expenseTypes = expenseTypes.map(expenseType => {
-      return { text: expenseType.budgetName, value: expenseType.id };
+      return {
+        text: expenseType.budgetName,
+        value: expenseType.id
+      };
     });
 
     let employees = await api.getItems(api.EMPLOYEES);
-    console.log(await employees);
     this.employees = employees.map(employee => {
       return {
         text: `${employee.firstName} ${employee.middleName} ${
@@ -60,3 +135,5 @@ export default {
   }
 };
 </script>
+<style>
+</style>
--- src/views/Employees.vue
+++ src/views/Employees.vue
@@ -1,112 +1,89 @@
 <template>
-  <div>
-    <h1>Employees</h1>
-    <b-alert :show="loading" variant="info">Loading...</b-alert>
-    <b-row>
-      <b-col>
-        <table class="table table-striped">
-          <thead>
-            <tr>
-              <th>Employee ID</th>
-              <th>Name</th>
-              <th>Hire Date</th>
-              <th>&nbsp;</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="employee in employees" :key="employee.id">
-              <td>{{ employee.empId }}</td>
-              <td>{{ employee.firstName }} {{ employee.middleName }} {{ employee.lastName }}</td>
-              <td>{{ employee.hireDate }}</td>
-              <td class="text-right">
-                <a href="#" @click.prevent="populateEmployeeToEdit(employee)">Edit</a> -
-                <a href="#" @click.prevent="deleteEmployee(employee.id)">Delete</a>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </b-col>
-      <b-col lg="4">
-        <form @submit.prevent="saveEmployee">
-          <b-card :header="(model.id ? 'Edit Employee' : 'New Employee')">
-            <b-form-group label="Employee ID">
-              <b-form-input type="text" v-model="model.empId"></b-form-input>
-            </b-form-group>
-            <b-form-group label="First Name">
-              <b-form-input type="text" v-model="model.firstName"></b-form-input>
-            </b-form-group>
-            <b-form-group label="Middle Name">
-              <b-form-input type="text" v-model="model.middleName"></b-form-input>
-            </b-form-group>
-            <b-form-group label="Last Name">
-              <b-form-input type="text" v-model="model.lastName"></b-form-input>
-            </b-form-group>
-            <b-form-group label="Hire Date">
-              <datepicker v-model="model.hireDate" format="MM/dd/yyyy" initial-view="month" :bootstrap-styling="true" 
-              :typeable=true :placeholder=" 'MM/DD/YYYY' "></datepicker>
-            </b-form-group>
-            <div slot="footer" class="footer-buttons">
-              <b-btn @click.prevent="clearEmployeeToEdit()" variant="default" class="mr-1">Cancel</b-btn>
-              <b-btn type="submit" variant="success">Save Employee</b-btn>
-            </div>
-          </b-card>
-        </form>
-      </b-col>
-    </b-row>
-  </div>
+<div>
+  <v-layout row wrap>
+    <v-flex lg8 md12 sm12>
+      <v-card>
+        <v-card-title>
+          <h2>Expenses</h2>
+          <v-spacer></v-spacer>
+          <v-text-field v-model="search" append-icon="search" label="Search" single-line hide-details></v-text-field>
+        </v-card-title>
+        <v-data-table :headers="headers" :items="employees" :search="search" item-key="name" class="elevation-1">
+          <template slot="items" slot-scope="props">
+              <tr @click="onSelect(props.item)">
+                <td class="text-xs-left">{{ props.item.firstName }}</td>
+                <td class="text-xs-left">{{ props.item.middleName }}</td>
+                <td class="text-xs-left">{{ props.item.lastName }}</td>
+                <td class="text-xs-left">{{ props.item.hireDate }}</td>
+                <td class="text-xs-left">{{ props.item.empId }}</td>
+              </tr>
+            </template>
+          <v-alert slot="no-results" :value="true" color="error" icon="warning">
+            Your search for "{{ search }}" found no results.
+          </v-alert>
+        </v-data-table>
+      </v-card>
+    </v-flex>
+    <v-flex lg4 md12 sm12>
+      <employee-form :model="model" v-on:submit-form="refreshEmployees"></employee-form>
+    </v-flex>
+  </v-layout>
+</div>
 </template>
 
 <script>
 import api from '@/shared/api.js';
-import Datepicker from 'vuejs-datepicker';
-
+import EmployeeForm from '../components/EmployeeForm.vue';
 export default {
   data() {
     return {
+      search: '',
       loading: false,
       employees: [],
       errors: [],
-      model: {}
-    }
+      headers: [
+        {
+          text: 'First Name',
+          value: 'firstName'
+        },
+        { text: 'Middle Name', value: 'middleName' }, //change value to call a function
+        { text: 'Last Name', value: 'lastName' },
+        { text: 'Hire Date', value: 'hireDate' },
+        { text: 'Employee ID', value: 'empId' }
+      ],
+      model: {
+        hireDate: null
+      }
+    };
   },
   components: {
-    Datepicker
+    EmployeeForm
   },
   async created() {
-    // Fetches posts when the component is created.
-    this.refreshEmployees()
+    this.refreshEmployees();
   },
   methods: {
-    async refreshEmployees () {
-      this.loading = true
-      this.employees = await api.getItems(api.EMPLOYEES)
-      this.loading = false
+    async refreshEmployees() {
+      this.loading = true;
+      this.employees = await api.getItems(api.EMPLOYEES);
+      this.loading = false;
     },
-    async populateEmployeeToEdit (employee) {
-      this.model = Object.assign({}, employee);
-    },
-    async clearEmployeeToEdit () {
-      this.model = {};
-    },
-    async saveEmployee () {
-      if (this.model.id) {
-        await api.updateItem(api.EMPLOYEES, this.model.id, this.model);
-      } else {
-        await api.createItem(api.EMPLOYEES, this.model);
-      }
-      this.model = {}; // reset form
-      await this.refreshEmployees();
-    },
-    async deleteEmployee (id) {
+    async deleteExpense(employee) {
       if (confirm('Are you sure you want to delete this employee?')) {
-        // if we are editing an expense type we deleted, remove it from the form
-        if (this.model.id === id) {
+        if (this.model.id === employee.id) {
           this.model = {};
         }
-        await api.deleteItem(api.EMPLOYEES, id)
+        await api.deleteItem(api.EMPLOYEES, employee.id);
         await this.refreshEmployees();
       }
+    },
+    onSelect(item) {
+      this.model.firstName = item.firstName;
+      this.model.middleName = item.middleName;
+      this.model.lastName = item.lastName;
+      this.model.empId = item.empId;
+      this.model.hireDate = item.hireDate;
     }
   }
-}
+};
 </script>
