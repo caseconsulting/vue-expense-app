0d36f7081c246e8bbc02988e353ab55c9ce65d05
Joseph Thompson <josephthompson@Josephs-MacBook-Pro.local>
2018-07-19 08:55:15 -0400

    177-employee-remaining-budgets: copied Home.vue into EmployeeHome.vue and skeletonized it

--- src/views/EmployeeHome.vue
+++ src/views/EmployeeHome.vue
@@ -1,55 +1,21 @@
 <template>
 <v-layout row wrap>
   <v-flex offset-lg1 offset-md2 lg9 md10 sm12>
-    <rollup-expense-type-table @selection="selection"></rollup-expense-type-table>
+    <h2>Remaining Balances Go Here</h2>
   </v-flex>
   <v-flex lg2 md2 sm12>
-    <expense-info class="jerryrig"></expense-info>
-    <expense-type-totals :selectedObj="tableSelected.data"></expense-type-totals>
+
   </v-flex>
 </v-layout>
 </template>
 
 <script>
-import ReimbursedTable from '../components/ReimbursedTable.vue';
-import UnreimbursedTable from '../components/UnreimbursedTable.vue';
-import ExpenseInfo from '../components/ExpenseInfo.vue';
-import ExpenseTypeTotals from '../components/ExpenseTypeTotals.vue';
-import RollupExpenseTypeTable from '../components/RollupExpenseTypeTable.vue';
-
 export default {
   data() {
-    return {
-      tableSelected: {},
-    };
+    return {};
   },
-  watch: {
-    tableSelected: {
-      handler: function(val, oldVal) {
-        console.log('val', val);
-        console.log('oldVal', oldVal);
-        this.tableSelected = val;
-      },
-      deep: true
-    }
-  },
-  methods: {
-    selection(item) {
-      this.tableSelected = item;
-    }
-  },
-
-  components: {
-    UnreimbursedTable,
-    ReimbursedTable,
-    ExpenseInfo,
-    ExpenseTypeTotals,
-    RollupExpenseTypeTable
-  }
+  components: {}
 };
 </script>
 <style>
-.jerryrig {
-  margin-bottom: 15px;
-}
 </style>
