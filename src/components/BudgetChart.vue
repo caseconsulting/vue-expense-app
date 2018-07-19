9ec42c4a6dc5b17ac86dbcc003b6fba11ddab86b
Joseph Thompson <josephthompson@Josephs-MacBook-Pro.local>
2018-07-19 14:48:59 -0400

    177-employee-remaining-budgets: got a demo of the budget chart working on the EmployeeHome.vue page

--- src/components/BudgetChart.vue
+++ src/components/BudgetChart.vue
@@ -1,20 +1,68 @@
 <script>
 import {
   Bar
-} from 'vue-chartjs'
+} from 'vue-chartjs';
 
 export default {
   extends: Bar,
   mounted() {
     // Overwriting base render method with actual data.
     this.renderChart({
-      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+      labels: [
+        'AWS',
+        'Company Phones',
+        'Cowboy Training',
+        'Dragon Balls',
+        'Equipment',
+        'Ninja Training',
+        'Overall',
+        'Parking',
+        'Pirate Training',
+        'Pokemon Training'
+      ],
       datasets: [{
-        label: 'GitHub Commits',
-        backgroundColor: '#f87979',
-        data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11]
-      }]
-    })
+          type: 'bar',
+          label: 'Reimbursed',
+          backgroundColor: '#12c44c',
+          data: [1050, 900, 1000, 850, 820, 420, 700, 1010, 999, 340]
+        },
+        {
+          type: 'bar',
+          label: 'Unreimbursed',
+          backgroundColor: 'red',
+          data: [0, 0, 0, 0, 0, 0, 0, 800, 120, 90]
+        },
+        {
+          type: 'bar',
+          label: 'Budget',
+          backgroundColor: '#e1e7f2',
+          fill: false,
+          data: [100, 100, 100, 100, 100, 100, 100, 100, 100, 100]
+        }
+      ]
+    }, {
+      scales: {
+        yAxes: [{
+          stacked: true,
+          ticks: {
+            beginAtZero: true
+          }
+        }],
+        xAxes: [{
+          stacked: true,
+          categoryPercentage: 0.5,
+          barPercentage: 1,
+          ticks: {
+            autoSkip: false
+          }
+        }]
+      },
+      legend: {
+        display: true
+      },
+      responsive: true,
+      maintainAspectRatio: false
+    });
   }
-}
+};
 </script>
--- src/views/EmployeeHome.vue
+++ src/views/EmployeeHome.vue
@@ -1,20 +1,25 @@
 <template>
-<v-layout row wrap>
-  <v-flex offset-lg1 offset-md2 lg9 md10 sm12>
-    <h2>Remaining Balances Go Here</h2>
-  </v-flex>
-  <v-flex lg2 md2 sm12>
-
-  </v-flex>
-</v-layout>
+<v-container fill-height>
+  <v-layout row wrap align-center justify-center>
+    <v-flex text-xs-center lg10 md6 sm6>
+      <h2>Remaining Balances Go Here</h2>
+    </v-flex>
+    <v-flex text-xs-center lg7 md6 sm6>
+      <budget-chart></budget-chart>
+    </v-flex>
+  </v-layout>
+</v-container>
 </template>
 
 <script>
+import BudgetChart from '../components/BudgetChart.vue';
 export default {
   data() {
     return {};
   },
-  components: {}
+  components: {
+    BudgetChart
+  }
 };
 </script>
 <style>
