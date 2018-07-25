700ca8937122adb5998e5c88dede438f497da09b
Joseph Thompson <josephthompson@Josephs-MacBook-Pro.local>
2018-07-25 15:42:36 -0400

    177-employee-remaining-budgets: added currency labels to the y axis and the tooltip on hover

--- src/components/BudgetChart.vue
+++ src/components/BudgetChart.vue
@@ -1,70 +1,86 @@
 <script>
-import {
-  Bar
-} from 'vue-chartjs';
+import { Bar } from 'vue-chartjs';
 
 export default {
   props: ['employee', 'budgets'],
   extends: Bar,
   mounted() {
     // Overwriting base render method with actual data.
-    this.renderChart({
-      labels: this.budgets.names,
-       //[
-      //   'AWS',
-      //   'Company Phones',
-      //   'Cowboy Training',
-      //   'Dragon Balls',
-      //   'Equipment',
-      //   'Ninja Training',
-      //   'Overall',
-      //   'Parking',
-      //   'Pirate Training',
-      //   'Pokemon Training'
-      // ],
-      datasets: [{
-          type: 'bar',
-          label: 'Reimbursed',
-          backgroundColor: '#12c44c',
-          data: this.budgets.reimbursed
-        },
-        {
-          type: 'bar',
-          label: 'Unreimbursed',
-          backgroundColor: 'red',
-          data: this.budgets.unreimbursed
-        },
-        {
-          type: 'bar',
-          label: 'Budget',
-          backgroundColor: '#e1e7f2',
-          fill: false,
-          data: this.budgets.difference
-        }
-      ]
-    }, {
-      scales: {
-        yAxes: [{
-          stacked: true,
-          ticks: {
-            beginAtZero: true
+    this.renderChart(
+      {
+        labels: this.budgets.names,
+        datasets: [
+          {
+            type: 'bar',
+            label: 'Reimbursed',
+            backgroundColor: '#12c44c',
+            data: this.budgets.reimbursed
+          },
+          {
+            type: 'bar',
+            label: 'Unreimbursed',
+            backgroundColor: 'red',
+            data: this.budgets.unreimbursed
+          },
+          {
+            type: 'bar',
+            label: 'Remaining Budget',
+            backgroundColor: '#e1e7f2',
+            fill: false,
+            data: this.budgets.difference
           }
-        }],
-        xAxes: [{
-          stacked: true,
-          categoryPercentage: 0.5,
-          barPercentage: 1,
-          ticks: {
-            autoSkip: false
+        ]
+      },
+      {
+        scales: {
+          yAxes: [
+            {
+              stacked: true,
+              ticks: {
+                beginAtZero: true,
+                callback: function(value, index, values) {
+                  return value.toLocaleString('en-US', {
+                    style: 'currency',
+                    currency: 'USD'
+                  });
+                }
+              }
+            }
+          ],
+          xAxes: [
+            {
+              stacked: true,
+              categoryPercentage: 0.5,
+              barPercentage: 1,
+              ticks: {
+                autoSkip: false
+              }
+            }
+          ]
+        },
+        tooltips: {
+          callbacks: {
+            label: function(tooltipItem, data) {
+              return (
+                '$' +
+                Number(tooltipItem.yLabel)
+                  .toFixed(0)
+                  .replace(/./g, function(c, i, a) {
+                    return i > 0 && c !== '.' && (a.length - i) % 3 === 0
+                      ? ',' + c
+                      : c;
+                  })
+              );
+            }
           }
-        }]
-      },
-      legend: {
-        display: true
-      },
-      responsive: true,
-      maintainAspectRatio: false
-    });
+        },
+        legend: {
+          display: true
+        },
+        responsive: true,
+        maintainAspectRatio: false
+      }
+    );
   }
 };
 </script>
