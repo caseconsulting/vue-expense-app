3f6a5ef99591bafbe7128594fec50157c20c848c
speake <samp97@vt.edu>
2018-07-26 11:16:21 -0400

    177-remaining-budgets-tables: Made budget table more neat

--- src/components/BudgetTable.vue
+++ src/components/BudgetTable.vue
@@ -5,112 +5,81 @@
 
     <v-data-iterator :items="employee.expenseTypeData" content-tag="v-layout" hide-actions row wrap>
 
-      <v-flex slot="item" slot-scope="props" xs12 sm6 md4 lg3>
-        <v-card>
-          <v-card-title>
-            <h4>{{ props.item.budgetName }}</h4></v-card-title>
-          <v-divider></v-divider>
-          <v-list dense>
-            <v-list-tile>
-              <v-list-tile-content>Budget:</v-list-tile-content>
-              <v-list-tile-content class="align-end">{{ props.item.budget }}</v-list-tile-content>
-            </v-list-tile>
-            <v-list-tile>
-              <div>
+
+        <v-flex
+          slot="item"
+          slot-scope="props"
+          xs12
+          sm6
+          md4
+          lg3
+        >
+          <v-card color="blue">
+            <v-card-title><h4>{{ props.item.budgetName }}</h4></v-card-title>
+            <v-divider></v-divider>
+            <v-list dense>
+              <v-list-tile>
+                <v-list-tile-content>Budget:</v-list-tile-content>
+                <v-list-tile-content class="align-end">{{ props.item.budget | moneyValue }}</v-list-tile-content>
+              </v-list-tile>
+              <v-list-tile>
                 <v-list-tile-content>Reimbursed:</v-list-tile-content>
-              </div>
-              <div>
-                <v-list-tile-content class="align-end">{{ props.item.reimbursed }}</v-list-tile-content>
-              </div>
-            </v-list-tile>
-            <v-list-tile>
-              <div>
-                <v-list-tile-content>Unreimbursed:</v-list-tile-content>
-              </div>
-              <div>
-                <v-list-tile-content class="align-end">{{ props.item.unreimbursed }}</v-list-tile-content>
-              </div>
-            </v-list-tile>
-            <v-list-tile>
-              <v-list-tile-content>Overdraft:</v-list-tile-content>
-              <v-list-tile-content class="align-end">{{ odFlagMessage(props.item) }}</v-list-tile-content>
-            </v-list-tile>
-          </v-list>
-        </v-card>
-      </v-flex>
+                <v-list-tile-content class="align-end">{{ props.item.reimbursed | moneyValue }}</v-list-tile-content>
+              </v-list-tile>
+              <v-list-tile>
+                  <v-list-tile-content>Unreimbursed:</v-list-tile-content>
+                <v-list-tile-content class="align-end">{{ props.item.unreimbursed | moneyValue }}</v-list-tile-content>
+              </v-list-tile>
+              <v-list-tile>
+                  <v-list-tile-content>Remaining Amount:</v-list-tile-content>
+                <v-list-tile-content class="align-end">{{ remaining(props.item)| moneyValue }}</v-list-tile-content>
+              </v-list-tile>
+              <v-list-tile>
+                <v-list-tile-content>Overdraft:</v-list-tile-content>
+                <v-list-tile-content class="align-end">{{ odFlagMessage(props.item) }}</v-list-tile-content>
+              </v-list-tile>
+            </v-list>
+          </v-card>
+        </v-flex>
 
     </v-data-iterator>
   </v-container>
 </div>
 </template>
 <script>
-export default {
-  props: ['employee'],
-  data: () => ({
-    rowsPerPageItems: [1, 4, 8, 12],
-    pagination: {
-      rowsPerPage: 4
-    },
 
 
-    // expenseTypes: [
-    //   {
-    //     value: 'total',
-    //     budget: 60000,
-    //     budgetName: 'Overall',
-    //     description: "Overall budget of all of the employee's expenses.",
-    //     odFlag: true
-    //   },
-    //   {
-    //     value: 'training',
-    //     budget: 500,
-    //     budgetName: 'Training',
-    //     description: "Training in new technologies",
-    //     odFlag: true
-    //
-    //   },
-    //   {
-    //     value: 'total',
-    //     budget: 60000,
-    //     budgetName: 'Overall',
-    //     description: "Overall budget of all of the employee's expenses.",
-    //     odFlag: true
-    //
-    //   },
-    //   {
-    //     value: 'total',
-    //     budget: 60000,
-    //     budgetName: 'Overall',
-    //     description: "Overall budget of all of the employee's expenses.",
-    //     odFlag: true
-    //
-    //   },
-    //   {
-    //     value: 'total',
-    //     budget: 60000,
-    //     budgetName: 'Overall',
-    //     description: "Overall budget of all of the employee's expenses.",
-    //     odFlag: true
-    //
-    //   },
-    //   {
-    //     value: 'total',
-    //     budget: 60000,
-    //     budgetName: 'Overall',
-    //     description: "Overall budget of all of the employee's expenses.",
-    //     odFlag: true
-    //
-    //   }
-    // ]
-  }),
-  methods: {
 
-    odFlagMessage(expenseType) {
-      return (expenseType.odFlag ? "Allowed" : "Not Allowed");
-    },
-    setColor(expenseType) {
-      return (expenseType.value ? "Allowed" : "Not Allowed");
-    }
+  export default {
+    props: ['employee'],
+    data: () => ({
+     rowsPerPageItems: [1, 4, 8, 12],
+     pagination: {
+       rowsPerPage: 4
+     }
+   }),
+   filters: {
+     moneyValue: value => {
+       return `${new Intl.NumberFormat('en-US', {
+         style: 'currency',
+         currency: 'USD',
+         minimumFractionDigits: 2,
+         maximumFractionDigits: 2
+       }).format(value)}`;
+     }
+   },
+   methods: {
+
+     odFlagMessage(expenseType) {
+       return (expenseType.odFlag ? "Allowed" : "Not Allowed");
+     },
+     setColor(expenseType) {
+       return (expenseType.value ? "Allowed" : "Not Allowed");
+     },
+     remaining(item) {
+       return item.budget - item.reimbursed - item.unreimbursed;
+     }
+   }
   }
 }
 </script>
