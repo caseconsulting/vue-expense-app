cc5e51074f1eda52515c0ed64f669423bec517b7
speake <samp97@vt.edu>
2018-08-01 08:41:52 -0400

    Sam-223-Require-a-Note-for-partial-reimbursement: Made notes field required for partially reimbursed expenses in ConfirmationBox.vue

--- src/components/ConfirmationBox.vue
+++ src/components/ConfirmationBox.vue
@@ -5,13 +5,18 @@
       <v-card-title class="headline">You've reached the budget limit for this expense type</v-card-title>
       <v-card-text>
         <p>
-          The expense you are about to submit is only covered up to {{ expense.budget|moneyValue }} you will be reimbursed {{expense.remaining|moneyValue}} of {{expense.cost|moneyValue}} Do you want to continue?
+          The expense you are about to submit is only covered up to {{ expense.budget|moneyValue }} you will be reimbursed {{expense.remaining|moneyValue}} of {{expense.cost|moneyValue}} <p v-if="expense.note">Do you want to continue?</p> <p v-else>You do not have a note for this expense. A partially covered expense needs to have a note when submitting. Please fill out the "Notes" box on the form to submit this expense.</p>
         </p>
       </v-card-text>
       <v-card-actions>
         <v-spacer></v-spacer>
-        <v-btn color="red" flat @click.native="emit('canceledSubmit')">No, take me back</v-btn>
-        <v-btn color="green darken-1" flat @click.native="emit('confirmSubmit')">Yes, submit</v-btn>
+        <div v-if="expense.note">
+          <v-btn color="red" flat @click.native="emit('canceledSubmit')">No, take me back</v-btn>
+          <v-btn color="green darken-1" flat @click.native="emit('confirmSubmit')">Yes, submit</v-btn>
+        </div>
+        <div v-else>
+          <v-btn color="green darken-1" flat @click.native="emit('canceledSubmit')">OK</v-btn>
+        </div>
       </v-card-actions>
     </v-card>
   </v-dialog>
