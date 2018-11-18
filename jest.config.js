224045b8041ce9b479afd0ce0ee51a0da41fe3d3
Michael Frank <mfrankwork@gmail.com>
2018-11-18 17:47:29 -0500

    module-updates: Add C:/Program Files/Git/tests/e2e/ to testPathIgnorePatterns for jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 /* globals module */
 module.exports = {
   moduleFileExtensions: ['js', 'jsx', 'json', 'vue'],
+  testPathIgnorePatterns: ['/node_modules/', '/tests/e2e/'],
   transform: {
     '^.+\\.vue$': 'vue-jest',
     '^.+\\.jsx?$': 'babel-jest'
