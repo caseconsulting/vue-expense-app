a316f195140016958b08c51155d9477681d56be3
Michael Frank <mfrankwork@gmail.com>
2018-11-18 17:41:18 -0500

    module-updates: Update example Cypress test

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -3,6 +3,6 @@
 describe("My First Test", () => {
   it("Visits the Kitchen Sink", () => {
     cy.visit("/");
-    cy.contains("h1", "Welcome to Your Vue.js App");
+    cy.contains("h1", "Portal");
   });
 });
