1a665f188ce8aa3f92e6bae9d3c06f26fd14408c
Jacob Boisseau <jacobboisseau@gmail.com>
2019-01-07 14:31:37 -0500

    added error alert for big files

--- src/components/FileUpload.vue
+++ src/components/FileUpload.vue
@@ -2,6 +2,18 @@
   <div>
   			<v-container fluid>
   				<v-flex xs12 class="text-xs-center text-sm-center text-md-center text-lg-center">
+            <!-- <h4 v-show="wrongFileType" class='text-danger align-center'>
+      The file you selected is a {{ fileType }} file. Valid file extensions are: {{ fileAccept }}.
+    </h4> -->
+
+
+  
+    <v-alert
+      :value="fileTooBig"
+      type="error"
+    >
+The file you selected is {{ megabytes }} MBs which exceeds the maximum file size of 6MB.
+    </v-alert>
             <!-- previewing images works but not pdfs etc.  -->
   					<!-- <img :src="previewURL" height="150" v-if="previewURL"/> -->
   					<v-text-field label="Select Receipt" @click='pickFile' v-model='fileName' prepend-icon='attach_file'></v-text-field>
@@ -19,15 +31,38 @@
 </template>
 
 <script>
+//COMPUTED
+function fileTooBig() {
+  return this.megabytes > 6;
+}
+function megabytes() {
+  return this.fileSize / 1000000;
+}
+function wrongFileType() {
+  return (
+    !_.isEmpty(this.fileName) &&
+    (!_.includes(this.fileName, 'image') ||
+      !_.includes(this.fileName, 'pdf') ||
+      !_.includes(this.fileName, 'excel') ||
+      !_.includes(this.fileName, 'msword'))
+  );
+}
+
 export default {
   data: () => ({
     title: 'Image Upload',
     dialog: false,
+    fileSize: 0,
     fileName: '',
     previewURL: '',
     fileBlob: '',
     fileExts: ['.pdf', '.doc', '.docx', '.xls', '.xlsx', 'image/*'].join(',')
   }),
+  computed: {
+    fileTooBig,
+    megabytes,
+    wrongFileType
+  },
 
   methods: {
     pickFile() {
@@ -45,6 +80,7 @@ export default {
         fr.readAsDataURL(files[0]);
         fr.addEventListener('load', () => {
           this.previewURL = fr.result;
+          this.fileSize = files[0].size;
           this.fileBlob = files[0]; // file that can be sent to s3
           this.$emit('fileSelected', this.fileBlob);
         });
