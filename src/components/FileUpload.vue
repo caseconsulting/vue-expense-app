ed372bbf6c7b3f6570ecf8ef55102ce0063c6fd1
Jacob Boisseau <jacobboisseau@gmail.com>
2019-01-07 14:42:29 -0500

    fixed memory error with very large files

--- src/components/FileUpload.vue
+++ src/components/FileUpload.vue
@@ -1,33 +1,17 @@
 <template>
-  <div>
-  			<v-container fluid>
-  				<v-flex xs12 class="text-xs-center text-sm-center text-md-center text-lg-center">
-            <!-- <h4 v-show="wrongFileType" class='text-danger align-center'>
-      The file you selected is a {{ fileType }} file. Valid file extensions are: {{ fileAccept }}.
-    </h4> -->
-
-
-  
-    <v-alert
-      :value="fileTooBig"
-      type="error"
-    >
-The file you selected is {{ megabytes }} MBs which exceeds the maximum file size of 6MB.
-    </v-alert>
-            <!-- previewing images works but not pdfs etc.  -->
-  					<!-- <img :src="previewURL" height="150" v-if="previewURL"/> -->
-  					<v-text-field label="Select Receipt" @click='pickFile' v-model='fileName' prepend-icon='attach_file'></v-text-field>
-  					<input
-  						type="file"
-  						style="display: none"
-  						ref="receipt"
-  						accept=fileExts
-  						@change="onFilePicked"
-  					>
-  				</v-flex>
-  			</v-container>
-  </div>
-
+<div>
+  <v-container fluid>
+    <v-flex xs12 class="text-xs-center text-sm-center text-md-center text-lg-center">
+      <v-alert :value="fileTooBig" type="error">
+        The file you selected is {{ megabytes }} MBs which exceeds the maximum file size of 6MB.
+      </v-alert>
+      <!-- previewing images works but not pdfs etc.  -->
+      <!-- <img :src="previewURL" height="150" v-if="previewURL"/> -->
+      <v-text-field label="Select Receipt" @click='pickFile' v-model='fileName' prepend-icon='attach_file'></v-text-field>
+      <input type="file" style="display: none" ref="receipt" accept=fileExts @change="onFilePicked">
+    </v-flex>
+  </v-container>
+</div>
 </template>
 
 <script>
@@ -35,22 +19,14 @@ The file you selected is {{ megabytes }} MBs which exceeds the maximum file size
 function fileTooBig() {
   return this.megabytes > 6;
 }
+
 function megabytes() {
   return this.fileSize / 1000000;
-}
-function wrongFileType() {
-  return (
-    !_.isEmpty(this.fileName) &&
-    (!_.includes(this.fileName, 'image') ||
-      !_.includes(this.fileName, 'pdf') ||
-      !_.includes(this.fileName, 'excel') ||
-      !_.includes(this.fileName, 'msword'))
-  );
 }
 
 export default {
   data: () => ({
-    title: 'Image Upload',
+    title: 'receipt upload',
     dialog: false,
     fileSize: 0,
     fileName: '',
@@ -60,8 +36,7 @@ export default {
   }),
   computed: {
     fileTooBig,
-    megabytes,
-    wrongFileType
+    megabytes
   },
 
   methods: {
@@ -73,14 +48,15 @@ export default {
       const files = e.target.files;
       if (files[0] !== undefined) {
         this.fileName = files[0].name;
-        if (this.fileName.lastIndexOf('.') <= 0) {
+        this.fileSize = files[0].size;
+        if (this.fileName.lastIndexOf('.') <= 0 || this.fileTooBig) {
+          //this stops a memory error with really large files
           return;
         }
         const fr = new FileReader();
         fr.readAsDataURL(files[0]);
         fr.addEventListener('load', () => {
           this.previewURL = fr.result;
-          this.fileSize = files[0].size;
           this.fileBlob = files[0]; // file that can be sent to s3
           this.$emit('fileSelected', this.fileBlob);
         });
