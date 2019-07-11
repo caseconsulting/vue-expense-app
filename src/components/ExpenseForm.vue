00dfaeefd237953f9ff16db1234e97c0d8e55b9d
austinjl <austinlam@Austins-MacBook-Pro.local>
2019-07-11 11:37:56 -0400

    319-I-want-to-filter-the-selectable-expense-type: 319 I want to filter the selectable expense type containing todays date

--- src/components/ExpenseForm.vue
+++ src/components/ExpenseForm.vue
@@ -18,6 +18,7 @@
           label="Employee"
           autocomplete
         ></v-select>
+
         <!--Expense type picker if super-admin -->
         <v-select
           v-if="employeeRole === 'super-admin' && this.$route.path !== '/home'"
