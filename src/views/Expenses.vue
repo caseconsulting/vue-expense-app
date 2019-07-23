e08f04ed597134bd06736a1f402575ccc9d5d9ef
austinjl <alam@consultwithcase.com>
2019-07-23 13:48:54 -0400

    fix-category-expense-error: Fixed category error on expense

--- src/views/Expenses.vue
+++ src/views/Expenses.vue
@@ -173,8 +173,8 @@
                     <p v-if="props.item.url">
                       <b>Url: </b> <a v-if="props.item.url" :href="props.item.url">{{ props.item.url }}</a>
                     </p>
-                    <p v-if="props.item.categories && props.item.categories.length > 0">
-                      <b>Categories: </b>{{ props.item.categories.join(', ') }}
+                    <p v-if="props.item.categories && props.item.categories.length == 1">
+                      <b>Category: </b>{{ props.item.categories }}
                     </p>
                   </div>
                 </v-card-text>
