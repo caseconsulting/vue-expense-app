5a670b48fadf62fb30d5a427301cbb2d3520ddee
austinjl <alam@consultwithcase.com>
2019-08-05 11:51:15 -0400

    731-employee-page-action-icons: removed merge notes

--- src/components/DeleteErrorModal.vue
+++ src/components/DeleteErrorModal.vue
@@ -2,13 +2,9 @@
   <v-layout row justify-center>
     <v-dialog v-model="activate" persistent max-width="350">
       <v-card>
-<<<<<<< HEAD:src/components/ExpenseTypeDeleteModel.vue
-        <v-card-title class="headline">Error: cannot delete expense</v-card-title>
-        <v-card-text>Cannot delete expense type. Expenses for this type exist.</v-card-text>
-=======
         <v-card-title class="headline">Error: cannot delete {{ type }}</v-card-title>
         <v-card-text>Cannot delete {{ type }}. Expenses for this {{ type }} exist.</v-card-text>
->>>>>>> 731-employee-page-action-icons: added action icons to employees page and changed delete error modal to be reusable:src/components/DeleteErrorModal.vue
+
         <v-card-actions>
           <v-spacer></v-spacer>
           <v-btn color="gray darken-1" flat @click.native="emit(`invalid-${type}-delete`)">Ok</v-btn>
