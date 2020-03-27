c12be31d42b48b20ee69ea60555ea25e789a87d8
austinjl <alam@consultwithcase.com>
2020-03-27 17:02:07 -0400

    training-url-page-layout: Change container for training page

--- src/views/TrainingAnalytics.vue
+++ src/views/TrainingAnalytics.vue
@@ -1,114 +1,111 @@
 <template>
-  <v-container style="padding-top: 0px;">
-    <div>
-      <h1 style="text-align: center;">Trending</h1>
-    </div>
+  <v-layout row justify-center>
+    <v-flex xs12>
+      <h1 class="text-center">Trending</h1>
+    </v-flex>
 
     <br />
-    <div class="text-center">
-      <!-- category buttons -->
-      <center>
-        <!-- close button -->
-        <v-tooltip bottom>
-          <template v-slot:activator="{ on }">
-            <v-btn @click="filterByCategory('All')" class="mx-3" fab dark color="red" v-on="on">
-              <v-icon dark>close</v-icon>
-            </v-btn>
-          </template>
-          <span>Close</span>
-        </v-tooltip>
-        <!-- end close button -->
-        <!-- Training button -->
-        <v-tooltip bottom>
-          <template v-slot:activator="{ on }">
-            <v-btn @click="filterByCategory('Training')" class="mx-3 pa-12" fab dark large color="primary" v-on="on">
-              <v-icon dark class="pb-7" size="60px">fitness_center</v-icon>
-            </v-btn>
-          </template>
-          <span>Training</span>
-        </v-tooltip>
-        <!-- end Training button -->
-        <!-- Conference button -->
-        <v-tooltip bottom>
-          <template v-slot:activator="{ on }">
-            <v-btn @click="filterByCategory('Conference')" class="mx-3 pa-12" fab dark large color="pink" v-on="on">
-              <v-icon dark class="pb-7" size="60px">group</v-icon>
-            </v-btn>
-          </template>
-          <span>Conference</span>
-        </v-tooltip>
-        <!-- end Conference button -->
-        <!-- Certifications button -->
-        <v-tooltip bottom>
-          <template v-slot:activator="{ on }">
-            <v-btn
-              @click="filterByCategory('Certifications')"
-              class="mx-3 pa-12"
-              fab
-              dark
-              large
-              color="orange"
-              v-on="on"
-            >
-              <v-icon dark class="pb-7" size="60px">stars</v-icon>
-            </v-btn>
-          </template>
-          <span>Certifications</span>
-        </v-tooltip>
-        <!-- end Certifications button -->
-        <!-- Lodging button -->
-        <v-tooltip bottom>
-          <template v-slot:activator="{ on }">
-            <v-btn @click="filterByCategory('Lodging')" class="mx-3 pa-12" fab dark large color="indigo" v-on="on">
-              <v-icon dark class="pb-7" size="60px">hotel</v-icon>
-            </v-btn>
-          </template>
-          <span>Lodging</span>
-        </v-tooltip>
-        <!-- end Lodging button -->
-        <!-- Travel button -->
-        <v-tooltip bottom>
-          <template v-slot:activator="{ on }">
-            <v-btn @click="filterByCategory('Travel')" class="mx-3 pa-12" fab dark large color="cyan" v-on="on">
-              <v-icon dark class="pb-7" size="60px">airplanemode_active</v-icon>
-            </v-btn>
-          </template>
-          <span>Travel</span>
-        </v-tooltip>
-        <!-- end Travel button -->
-        <!-- Meals button -->
-        <v-tooltip bottom>
-          <template v-slot:activator="{ on }">
-            <v-btn @click="filterByCategory('Meals')" class="mx-3 pa-12" fab dark large color="purple" v-on="on">
-              <v-icon dark class="pb-7" size="60px">restaurant</v-icon>
-            </v-btn>
-          </template>
-          <span>Meals</span>
-        </v-tooltip>
-        <!-- end Meals button -->
-        <a style="padding-right: 89px;"> </a>
-      </center>
-      <!-- end category buttons -->
-      <br />
-      <hr />
-      <div>Category: {{ categoryFilter }}</div>
-      <br />
 
+    <!-- category buttons -->
+    <v-flex xs12 class="text-center">
+      <!-- close button -->
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn @click="filterByCategory('All')" class="mx-3" fab dark color="red" v-on="on">
+            <v-icon dark>close</v-icon>
+          </v-btn>
+        </template>
+        <span>Close</span>
+      </v-tooltip>
+      <!-- end close button -->
+      <!-- Training button -->
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn @click="filterByCategory('Training')" class="mx-3 pa-12" fab dark large color="primary" v-on="on">
+            <v-icon dark class="pb-7" size="60px">fitness_center</v-icon>
+          </v-btn>
+        </template>
+        <span>Training</span>
+      </v-tooltip>
+      <!-- end Training button -->
+      <!-- Conference button -->
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn @click="filterByCategory('Conference')" class="mx-3 pa-12" fab dark large color="pink" v-on="on">
+            <v-icon dark class="pb-7" size="60px">group</v-icon>
+          </v-btn>
+        </template>
+        <span>Conference</span>
+      </v-tooltip>
+      <!-- end Conference button -->
+      <!-- Certifications button -->
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn @click="filterByCategory('Certifications')" class="mx-3 pa-12" fab dark large color="orange" v-on="on">
+            <v-icon dark class="pb-7" size="60px">stars</v-icon>
+          </v-btn>
+        </template>
+        <span>Certifications</span>
+      </v-tooltip>
+      <!-- end Certifications button -->
+      <!-- Lodging button -->
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn @click="filterByCategory('Lodging')" class="mx-3 pa-12" fab dark large color="indigo" v-on="on">
+            <v-icon dark class="pb-7" size="60px">hotel</v-icon>
+          </v-btn>
+        </template>
+        <span>Lodging</span>
+      </v-tooltip>
+      <!-- end Lodging button -->
+      <!-- Travel button -->
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn @click="filterByCategory('Travel')" class="mx-3 pa-12" fab dark large color="cyan" v-on="on">
+            <v-icon dark class="pb-7" size="60px">airplanemode_active</v-icon>
+          </v-btn>
+        </template>
+        <span>Travel</span>
+      </v-tooltip>
+      <!-- end Travel button -->
+      <!-- Meals button -->
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn @click="filterByCategory('Meals')" class="mx-3 pa-12" fab dark large color="purple" v-on="on">
+            <v-icon dark class="pb-7" size="60px">restaurant</v-icon>
+          </v-btn>
+        </template>
+        <span>Meals</span>
+      </v-tooltip>
+      <!-- end Meals button -->
+    </v-flex>
+    <!-- end category buttons -->
+
+    <br />
+    <hr />
+
+    <v-flex xs12 class="text-center">
+      <div>Category: {{ categoryFilter }}</div>
+    </v-flex>
+
+    <br />
+
+    <v-flex xs12>
       <!-- list all url info -->
       <div v-for="url in this.urls" :key="url.id">
-        <v-container class="py-0">
+        <v-container class="py-0 text-center">
           <v-row dense>
             <v-col cols="12">
               <v-card color="#565651" dark :href="url.id" target="_blank">
                 <v-layout wrap class="ma-1">
-                  <v-flex xs12 sm3 md2>
+                  <v-flex xs12 sm3 md2 xl1>
                     <v-avatar class="ma-1" size="100" tile>
                       <img :src="url.display" :class="{ caseImage: url.isCaseLogo }" @error="changeDisplay(url)" />
                     </v-avatar>
                     <h3 v-if="!isEmpty(url.title)">{{ url.publisher }}</h3>
                   </v-flex>
 
-                  <v-flex xs12 sm9 md10>
+                  <v-flex xs12 sm9 md10 xl11>
                     <v-layout column justify-space-between fill-height>
                       <!-- title and description -->
                       <div v-if="!isEmpty(url.title)">
@@ -133,9 +130,10 @@
           </v-row>
         </v-container>
       </div>
-    </div>
-  </v-container>
+    </v-flex>
+  </v-layout>
 </template>
+
 <script>
 import api from '@/shared/api.js';
 import _ from 'lodash';
