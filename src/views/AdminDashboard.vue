b88bb67cf3c92dc3b6a1ed945f5f3a0da996252d
austinjl <alam@consultwithcase.com>
2020-05-18 18:23:46 -0400

    933-Admin-dashboard-pinned-to-right-side-when-screen-is-md: Change breakpoints to center admin dashboard table

--- src/views/AdminDashboard.vue
+++ src/views/AdminDashboard.vue
@@ -1,6 +1,6 @@
 <template>
   <v-layout row wrap>
-    <v-flex offset-lg1 offset-md2 lg9 md10 sm12>
+    <v-flex offset-lg1 offset-md1 md10 sm12>
       <rollup-expense-type-table></rollup-expense-type-table>
     </v-flex>
     <v-flex lg2 md2 sm12>
