92be3494f1f4e46942daea26e4fbb8466a2f12bb
austinjl <alam@consultwithcase.com>
2020-05-22 11:25:19 -0400

    972: Refactor and document BudgetSelectModal.vue

--- src/components/BudgetSelectModal.vue
+++ src/components/BudgetSelectModal.vue
@@ -2,10 +2,15 @@
   <v-layout row justify-center>
     <v-dialog v-model="activate" persistent max-width="400">
       <v-card>
+        <!-- Anniversary Date -->
         <v-toolbar color="#565651" dark>
           <v-toolbar-title>Anniversary Date: {{ moment(hireDate).format('MMMM Do') }}</v-toolbar-title>
         </v-toolbar>
+        <!-- End Anniversary Date -->
+
+        <!-- Buttons -->
         <v-list two-line>
+          <!-- Budget List -->
           <template v-for="(budgetYear, index) in budgetYears">
             <v-list-item :key="budgetYear" ripple @click.native="select(budgetYear)" class="list-hover">
               <v-list-item-content>
@@ -18,6 +23,9 @@
             </v-list-item>
             <v-divider :key="index"></v-divider>
           </template>
+          <!-- End Budget List -->
+
+          <!-- Cancel Button -->
           <template>
             <v-list-item ripple @click.native="emit(`cancel-budget-year`)" class="list-hover">
               <v-list-item-content>
@@ -25,39 +33,62 @@
               </v-list-item-content>
             </v-list-item>
           </template>
+          <!-- End Cancel Button -->
         </v-list>
+        <!-- End Buttons -->
       </v-card>
     </v-dialog>
   </v-layout>
 </template>
 
 <script>
-const Moment = require('moment');
 const IsoFormat = 'YYYY-MM-DD';
+const Moment = require('moment');
 
+/**
+ * Emits a message and data if it exists.
+ *
+ * @param msg - Message to emit
+ * @param data - Data to emit
+ */
+function emit(msg, data) {
+  if (data) {
+    // data exists
+    window.EventBus.$emit(msg, data);
+  } else {
+    // data does not exist
+    window.EventBus.$emit(msg);
+  }
+} // emit
+
+/**
+ * Checks if a given budget year is the same as the current budget year displayed.
+ *
+ * @param budgetYear - int budget year to compare to current
+ * @return boolean - given budget year is equal to current
+ */
+function isCurrent(budgetYear) {
+  let [year] = this.current.split('-');
+  return budgetYear == year;
+} // isCurrent
+
+/**
+ * Set and emit the selected budget year.
+ *
+ * @param budgetYear - int budget year selected
+ */
 function select(budgetYear) {
   let fiscalYear = Moment(this.hireDate, IsoFormat);
   fiscalYear.year(budgetYear);
   this.emit(`selected-budget-year`, fiscalYear);
-}
-
-function isCurrent(budgetYear) {
-  let [year] = this.current.split('-');
-  return budgetYear == year;
-}
+} // select
 
 export default {
-  props: ['budgetYears', 'current', 'hireDate', 'activate'],
+  props: ['activate', 'budgetYears', 'current', 'hireDate'],
   methods: {
-    emit(msg, data) {
-      if (data) {
-        window.EventBus.$emit(msg, data);
-      } else {
-        window.EventBus.$emit(msg);
-      }
-    },
-    select,
-    isCurrent
+    emit,
+    isCurrent,
+    select
   }
 };
 </script>
