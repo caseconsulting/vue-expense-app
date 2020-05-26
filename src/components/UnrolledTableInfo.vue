1ffe7df6194c5936ed0543caaed20cb8e4d9b1ce
austinjl <alam@consultwithcase.com>
2020-05-26 11:49:38 -0400

    972: Refactor and Document UnrolledTableInfo.vue

--- src/components/UnrolledTableInfo.vue
+++ src/components/UnrolledTableInfo.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container fluid style="background: grey;">
     <div>
-      <!-- unreimburse sub-datable -->
+      <!-- Unreimburse Sub-Datable -->
       <v-data-table
         :headers="headers"
         :items="expenses"
@@ -13,18 +13,18 @@
         show-select
         hide-default-footer
       >
-        <!-- remove header check box -->
+        <!-- Remove check box in header-->
         <template v-slot:header.data-table-select></template>
-        <!-- end remove header check box -->
-        <!-- checkbox for items -->
+
+        <!-- Group Checkbox -->
         <template v-slot:item.data-table-select="{ item }">
           <v-checkbox :input-value="item.selected" primary hide-details class="ma-0"></v-checkbox>
         </template>
-        <!-- end checkbox for items -->
-        <!-- rows in datatable -->
+
+        <!-- Rows in datatable -->
         <template v-slot:item="{ item }">
           <tr @click="expenseClicked(item)">
-            <!-- checkbox for individual expense -->
+            <!-- Expense Check Box -->
             <td style="width: 1px;" :class="{ failed: item.failed }">
               <v-checkbox
                 :input-value="item.selected"
@@ -38,14 +38,20 @@
               >
               </v-checkbox>
             </td>
+
+            <!-- Cost -->
             <td id="money-team" :class="{ failed: item.failed }">{{ item.cost | moneyValue }}</td>
+
+            <!-- Purchase Date -->
             <td :class="{ failed: item.failed }">{{ item.purchaseDate | dateFormat }}</td>
+
+            <!-- Description -->
             <td :class="{ failed: item.failed }">{{ item.description | descripFormat }}</td>
           </tr>
         </template>
-        <!-- end rows in datatable -->
+        <!-- End rows in datatable -->
       </v-data-table>
-      <!-- end unreimburse sub-datable -->
+      <!-- End Unreimburse Sub-Datable -->
     </div>
   </v-container>
 </template>
@@ -53,47 +59,39 @@
 <script>
 import moment from 'moment';
 
-/* methods */
+// |--------------------------------------------------|
+// |                                                  |
+// |                     METHODS                      |
+// |                                                  |
+// |--------------------------------------------------|
 
 /*
- * Emit an event to parent that an expense was selected
- */
-function expenseSelected(selectedExpense) {
-  window.EventBus.$emit('selectExpense', selectedExpense);
-}
-
-/*
- * Emit an event to parent that an expense was selected
+ * Emit an event to parent that an expense was clicked
+ *
+ * @param clickedExpense - expense clicked
  */
 function expenseClicked(clickedExpense) {
   window.EventBus.$emit('expenseClicked', clickedExpense);
-}
+} // expenseClicked
+
+/*
+ * Emit an event to parent that an expense was selected.
+ *
+ * @param selectExpense - selected expense
+ */
+function expenseSelected(selectedExpense) {
+  window.EventBus.$emit('selectExpense', selectedExpense);
+} // expenseSelected
+
+// |--------------------------------------------------|
+// |                                                  |
+// |                      EXPORT                      |
+// |                                                  |
+// |--------------------------------------------------|
 
 export default {
-  filters: {
-    moneyValue: (value) => {
-      return `${new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'USD',
-        minimumFractionDigits: 2,
-        maximumFractionDigits: 2
-      }).format(value)}`;
-    },
-    dateFormat: (value) => {
-      if (value) {
-        return moment(value).format('MMM Do, YYYY');
-      } else {
-        return '';
-      }
-    },
-    descripFormat: (val) => {
-      return val.length > 250 ? val.substring(0, 250) + '...' : val;
-    }
-  },
   data() {
     return {
-      sortBy: 'purchaseDate',
-      sortDesc: false,
       headers: [
         {
           text: 'Cost',
@@ -110,14 +108,39 @@ export default {
           value: 'description',
           align: 'center'
         }
-      ]
+      ],
+      sortBy: 'purchaseDate',
+      sortDesc: false
     };
   },
-  props: ['expenses'],
+  filters: {
+    dateFormat: (value) => {
+      if (value) {
+        // date exists
+        return moment(value).format('MMM Do, YYYY');
+      } else {
+        // date does not exist
+        return '';
+      }
+    },
+    descripFormat: (val) => {
+      // split strings that exceed 250 characters with eclipses
+      return val.length > 250 ? val.substring(0, 250) + '...' : val;
+    },
+    moneyValue: (value) => {
+      return `${new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+      }).format(value)}`;
+    }
+  },
   methods: {
-    expenseSelected,
-    expenseClicked
-  }
+    expenseClicked,
+    expenseSelected
+  },
+  props: ['expenses'] // list of expenses
 };
 </script>
 
