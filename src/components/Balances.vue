01949763cbc43029eee8e9109a4d66b079afc911
zzahid <zzahid@consultwithcase.com>
2020-06-18 18:43:01 -0400

    149-show-t-training-balance: simplified code and displayed all available balances

--- src/components/Balances.vue
+++ src/components/Balances.vue
@@ -5,45 +5,15 @@
         <h4 class="white--text">Balances</h4>
       </v-card-title>
       <v-card-text class="px-7 pt-5 pb-1 black--text">
-        <!-- <v-row v-for="balance in balanceData" :key="balance.id">
-          {{ balance.name }}:
-          <v-spacer></v-spacer>
-          <p>{{ this.trainingBalance }}</p>
-        </v-row> -->
-        <v-row>
-          Training:
-          <v-spacer></v-spacer>
-          <p>{{ this.trainingBalance }}</p>
+        <!-- If no avaible balances dislay message -->
+        <v-row v-if="balanceData == 0" justify="center">
+          <p>No available balances</p>
         </v-row>
-        <v-row>
-          Holiday:
+        <!-- Loop through and display all balances -->
+        <v-row v-for="balance in this.keysBalance" :key="balance">
+          <p>{{ balance }}:</p>
           <v-spacer></v-spacer>
-          <p>{{ this.holidayBalance }}</p>
-        </v-row>
-        <v-row>
-          Case Connections:
-          <v-spacer></v-spacer>
-          <p>{{ this.caseConnectBalance }}</p>
-        </v-row>
-        <v-row>
-          Case Cares:
-          <v-spacer></v-spacer>
-          <p>{{ this.caseCareBalance }}</p>
-        </v-row>
-        <v-row>
-          Vacation:
-          <v-spacer></v-spacer>
-          <p>{{ this.caseCareBalance }}</p>
-        </v-row>
-        <v-row>
-          COVID-19 PTO:
-          <v-spacer></v-spacer>
-          <p>{{ this.covidPTO }}</p>
-        </v-row>
-        <v-row>
-          Sabbatical:
-          <v-spacer></v-spacer>
-          <p>{{ this.sabbaticalBalance }}</p>
+          <p>${{ balanceData[balance] }}</p>
         </v-row>
       </v-card-text>
     </v-card>
@@ -65,13 +35,7 @@ async function created() {
   this.ptoBalances = await api.getPTOBalances(this.employee.employeeNumber); // call api
   this.ptoBalances = this.ptoBalances.results.users[this.employee.employeeNumber];
   this.balanceData = this.ptoBalances['pto_balances'];
-  this.trainingBalance = this.balanceData['Training'];
-  this.holidayBalance = this.balanceData['Holiday'];
-  this.caseConnectBalance = this.balanceData['Case Connections'];
-  this.caseCareBalance = this.balanceData['Case Cares'];
-  this.vacationBalance = this.balanceData['PTO'];
-  this.covidPTO = this.balanceData['COVID-19 PTO'];
-  this.sabbaticalBalance = this.balancedData['Sabbatical'];
+  this.keysBalance = Object.keys(this.balanceData);
 } // created
 // |--------------------------------------------------|
 // |                                                  |
@@ -84,13 +48,8 @@ export default {
   data() {
     return {
       ptoBalances: [],
-      trainingBalance: 0,
-      holidayBalance: 0,
-      caseConnectBalance: 0,
-      caseCareBalance: 0,
-      vacationBalance: 0,
-      covidPTO: 0,
-      sabbaticalBalance: 0
+      balanceData: [],
+      keysBalance: []
     };
   },
   props: ['employee']
