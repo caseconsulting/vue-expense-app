a2e1b97bb00e7c788c091a290c5b0f1d5d168e16
zzahid <zzahid@consultwithcase.com>
2020-06-22 11:18:45 -0400

    1038-loading-bar-on-activity-feed: added loading bar to activity feed

--- src/components/ActivityFeed.vue
+++ src/components/ActivityFeed.vue
@@ -6,25 +6,40 @@
       </v-card-title>
     </v-card>
     <v-card class="overflow-y-auto" max-height="376px">
+      <div v-if="this.loadingBar" class="pb-4">
+        <v-progress-linear :indeterminate="true"></v-progress-linear>
+      </div>
       <!-- timeline -->
-      <v-timeline dense>
-        <v-timeline-item color="#bc3825" v-for="event in events" :key="event.name">
-          <!-- <template v-slot:opposite>
+      <div v-else>
+        <v-timeline dense>
+          <v-timeline-item color="#bc3825" v-for="event in events" :key="event.name">
+            <!-- <template v-slot:opposite>
             <icon :name="event.icon"></icon>
-          </template> -->
-          <template v-slot:icon v-if="event.icon">
-            <icon class="white--text" :name="event.icon"></icon>
-          </template>
-          <h3>{{ event.date }}</h3>
-          <div>{{ event.text }}</div>
-        </v-timeline-item>
-      </v-timeline>
+            </template>-->
+            <template v-slot:icon v-if="event.icon">
+              <icon class="white--text" :name="event.icon"></icon>
+            </template>
+            <h3>{{ event.date }}</h3>
+            <div>{{ event.text }}</div>
+          </v-timeline-item>
+        </v-timeline>
+      </div>
     </v-card>
   </v-flex>
 </template>
 
 <script>
+async function mounted() {
+  this.loadingBar = true;
+  this.loadingBar = false;
+}
 export default {
+  mounted,
+  data() {
+    return {
+      loadingBar: false
+    };
+  },
   props: ['events']
 };
 </script>
