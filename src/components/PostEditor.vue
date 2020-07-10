a1c36c3ef807e364002b5fe6ea22c23f6165da2e
pturriagol <pturriagol@consultwithcase.com>
2020-07-10 09:22:29 -0400

    1130-set-up-blog-page-in-app: added the post editor and did force stuff check with cadre if that was bad

--- src/components/PostEditor.vue
+++ src/components/PostEditor.vue
@@ -1,6 +1,20 @@
 <template>
-  <p>post editor</p>
+  <ckeditor :editor="editor"></ckeditor>
 </template>
 <script>
-export default {};
+import CKEditor from '@ckeditor/ckeditor5-vue';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+export default {
+  components: {
+    // Use the <ckeditor> component in this view.
+    ckeditor: CKEditor.component
+  },
+  data() {
+    return {
+      editor: ClassicEditor
+
+      // ...
+    };
+  }
+};
 </script>
