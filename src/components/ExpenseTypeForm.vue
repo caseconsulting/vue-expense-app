12fe462bd0bf405fb5904f54aceba7035eb1c7d5
rlitscher <rlitscher@consultwithcase.com>
2020-07-17 14:00:27 -0400

    change-categories-to-JSON-objects: remove checkbox during submitting

--- src/components/ExpenseTypeForm.vue
+++ src/components/ExpenseTypeForm.vue
@@ -37,12 +37,7 @@
 
         <v-layout row wrap>
           <v-flex v-for="(category, index) in model.categories" :key="index" xs6>
-            <v-checkbox
-              v-if="!submitting"
-              light
-              :label="category.name"
-              v-model="category.showOnFeed"
-            ></v-checkbox>
+            <v-checkbox v-if="!submitting" light :label="category.name" v-model="category.showOnFeed"></v-checkbox>
           </v-flex>
         </v-layout>
         <br />
