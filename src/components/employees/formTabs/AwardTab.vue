057b9a823d1c2e2bfe455369af0a5c7dbe7c9e4b
rlitscher <rlitscher@consultwithcase.com>
2020-08-12 12:36:07 -0400

    1168: add files for awards tabs

---
+++ src/components/employees/formTabs/AwardTab.vue
@@ -0,0 +1,145 @@
+<template>
+  <div>
+    <!-- Loop awards -->
+    <div
+      v-for="(award, index) in model.awards"
+      style="border: 1px solid grey;"
+      class="pt-3 pb-1 px-5"
+      :key="'award: ' + award.name + index"
+    >
+      <v-combobox
+        v-model="award.name"
+        :rules="requiredRules"
+        label="Award"
+        data-vv-name="Award"
+        append-outer-icon="delete"
+        @click:append-outer="deleteAward(index)"
+      >
+      </v-combobox>
+
+      <v-row>
+        <v-col cols="12" sm="6" md="12" lg="6" class="pt-0">
+          <!-- Received Date -->
+          <v-menu
+            v-model="award.showReceivedMenu"
+            :close-on-content-click="false"
+            transition="scale-transition"
+            offset-y
+            max-width="290px"
+            min-width="290px"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-text-field
+                :value="formatDate(award.dateReceived)"
+                label="Date Received"
+                prepend-icon="event_available"
+                :rules="dateRules"
+                readonly
+                v-bind="attrs"
+                v-on="on"
+              ></v-text-field>
+            </template>
+            <v-date-picker
+              v-model="award.dateReceived"
+              no-title
+              @input="award.showReceivedMenu = false"
+            ></v-date-picker>
+          </v-menu>
+          <!-- End Received Date -->
+        </v-col>
+      </v-row>
+    </div>
+    <!-- Add award button -->
+    <div class="pt-4" align="center">
+      <v-btn @click="addAward()"><v-icon class="pr-1">add</v-icon>Award</v-btn>
+    </div>
+  </div>
+</template>
+
+<script>
+import api from '@/shared/api.js';
+// |--------------------------------------------------|
+// |                                                  |
+// |                 LIFECYCLE HOOKS                  |
+// |                                                  |
+// |--------------------------------------------------|
+
+async function created() {
+  this.employees = await api.getItems(api.EMPLOYEES); // get all employees
+} // created
+
+// |--------------------------------------------------|
+// |                                                  |
+// |                     METHODS                      |
+// |                                                  |
+// |--------------------------------------------------|
+
+/**
+ * Add an award to the form.
+ */
+function addAward() {
+  this.model.awards.push({
+    name: null,
+    dateReceived: null,
+    expirationDate: null,
+    showReceivedMenu: false,
+    showExpirationMenu: false
+  });
+} // addAward
+
+/**
+ * delete an award from the form
+ */
+function deleteAward(index) {
+  this.model.awards.splice(index, 1);
+} // deleteAward
+
+function formatDate(date) {
+  if (!date) {
+    return null;
+  }
+  const [year, month, day] = date.split('-');
+  return `${month}/${day}/${year}`;
+} // formatDate
+
+/**
+ * Parse a date to isoformat (YYYY-MM-DD).
+ *
+ * @param Date = date to parse
+ * @return Date - date in isoformat
+ */
+function parseDate(date) {
+  if (!date) {
+    return null;
+  }
+  const [month, day, year] = date.split('/');
+  return `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`; // parseDate
+}
+
+export default {
+  created,
+  data() {
+    return {
+      dateOptionalRules: [
+        (v) => {
+          return v ? /^\d{1,2}\/\d{1,2}\/\d{4}$/.test(v) || 'Date must be valid. Format: MM/DD/YYYY' : true;
+        }
+      ], // rules for optional dates
+      dateRules: [
+        (v) => !!v || 'Date required',
+        (v) => (!!v && /^\d{1,2}\/\d{1,2}\/\d{4}$/.test(v)) || 'Date must be valid. Format: MM/DD/YYYY'
+      ], // rules for date
+      requiredRules: [
+        (v) => !!v || 'This field is required. You must enter information or delete the field if possible'
+      ] // rules for required fields
+    };
+  },
+  methods: {
+    addAward,
+    deleteAward,
+    formatDate,
+    parseDate
+  },
+  props: ['model']
+};
+</script>
