0ae24230a1d87a4de24697660daa880fb82221d8
rlitscher <rlitscher@consultwithcase.com>
2020-08-12 20:36:18 -0400

    1169: fix lines

--- src/components/employees/infoTabs/CustomerOrgTab.vue
+++ src/components/employees/infoTabs/CustomerOrgTab.vue
@@ -5,7 +5,7 @@
         <p><b>Customer Organization Experience: </b>{{ exp.name }}</p>
         <p><b>Years of Experience: </b>{{ exp.years }}</p>
         <p><b>Current: </b>{{ exp.current | current }}</p>
-        <hr v-if="index < model.customerOrgExp.length - 1" class="pb-3" />
+        <hr v-if="index < model.customerOrgExp.length - 1" class="mb-3" />
       </div>
     </div>
     <p v-else>No Customer Organization Experience Information</p>
