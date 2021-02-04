e2c68503f8ed4ef89208551a02fe0f8887942e2d
mGilbert15 <gilberme@dukes.jmu.edu>
2021-02-04 09:34:42 -0500

    2029-change-the-toggle-on-time-card-to-be-a-hover-tooltip: added tooltip to switch on tsheets component

--- src/components/TSheetsData.vue
+++ src/components/TSheetsData.vue
@@ -4,7 +4,16 @@
       <v-card-title class="header_style">
         <h3>TSheets Data</h3>
         <v-spacer></v-spacer>
-        <v-switch v-model="showMinutes" dense hide-details color="gray" class="my-0 py-0"></v-switch>
+        <!--Switch between minutes and hours-->
+        <v-tooltip top nudge-top="10">
+          <template v-slot:activator="{ on, attrs }">
+            <div v-bind="attrs" v-on="on">
+              <v-switch v-model="showMinutes" dense hide-details color="gray" class="my-0 py-0"></v-switch>
+            </div>
+          </template>
+          <span>{{ tooltipText }}</span>
+        </v-tooltip>
+        <!--End of Switch-->
       </v-card-title>
       <v-card-text class="pt-5 pb-0 black--text">
         <monthly-charges :passedEmployee="employee" :showMinutes="showMinutes"></monthly-charges>
@@ -19,10 +28,28 @@
 import MonthlyCharges from '@/components/MonthlyCharges.vue';
 import Balances from '@/components/Balances.vue';
 
+// |--------------------------------------------------|
+// |                                                  |
+// |                     COMPUTED                     |
+// |                                                  |
+// |--------------------------------------------------|
+
+/**
+ * Calculates the tooltip text to display on v-switch based on value of showMinutes.
+ *
+ * @return String - the tooltip text to display based on if showMinutes is true
+ */
+function tooltipText() {
+  return this.showMinutes ? 'Hours <- Minutes' : 'Hours -> Minutes';
+} //tooltipText
+
 export default {
   components: {
     Balances,
     MonthlyCharges
+  },
+  computed: {
+    tooltipText
   },
   data() {
     return {
