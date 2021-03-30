18d8728851ecb00a8b1c569a9fcf10597e412632
pturriagol <pturriagol@consultwithcase.com>
2021-03-30 11:31:48 -0700

    just-removing-unused-code-really-quickly: removed commented out code

--- unitTests/homepageTests.js
+++ unitTests/homepageTests.js
@@ -18,13 +18,6 @@ describe('Testing the homepage', () => {
             .click('#passwordNext > div > button > div.VfPpkd-RLmnJb')
             .waitForElementVisible('#app > div > header > div > div.v-toolbar__title > h1')
             .assert.urlEquals('http://localhost:8080/reimbursements');
-
-            // browser
-            //     .perform(() => {
-            //         console.info('uhhhh');
-            //      })
-            //     .pause(10000000);
-            
     });
 
     test('checking to see if you have to navigate every time', browser => {
