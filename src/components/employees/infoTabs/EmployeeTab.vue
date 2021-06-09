371e8d167bc7404f7cfd25b4714325f1e617b987
cmartin <cmartin@consultwithcase.com>
2021-06-09 16:40:58 -0400

    2288-show-nickname-on-the-info-tab: show nickname field on the employee tab

--- src/components/employees/infoTabs/EmployeeTab.vue
+++ src/components/employees/infoTabs/EmployeeTab.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <p><b>Full Name:</b> {{ fullName }}</p>
+    <p v-if="!isEmpty(this.model.nickname)"><b>Nickname:</b> {{ this.model.nickname }}</p>
     <p><b>Employee Number:</b> {{ this.model.employeeNumber }}</p>
     <p><b>Email:</b> {{ this.model.email }}</p>
     <p v-if="!isEmpty(this.model.prime)"><b>Prime:</b> {{ this.model.prime }}</p>
