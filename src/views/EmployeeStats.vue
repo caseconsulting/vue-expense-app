04b8889ce35ef5828b839b8be24a89123958ddf7
sjosi <sjosi@consultwithcase.com>
2021-06-21 14:15:43 -0400

    2349-remove-google-charts: removed all google charts data

--- src/views/EmployeeStats.vue
+++ src/views/EmployeeStats.vue
@@ -2,7 +2,6 @@
   <v-container>
     <h1>Employee Statistics Dashboard</h1>
     <job-experience-histogram :options="optionSet" :chart-data="dataSet"></job-experience-histogram>
-    <div id="google-job-exp-hist-chart"></div>
 
     <div>
       <v-container flex>
@@ -118,39 +117,6 @@ function drawJobExpHistGraph() {
   this.optionSet = options;
 } // drawJobExpHistGraph
 
-function googleJobExpHistGraph() {
-  var dataArray = [['Year']];
-  for (var i = 0; i < this.jobExperienceHist.length; i++) {
-    dataArray.push([this.jobExperienceHist[i]]);
-  }
-  // eslint-disable-next-line no-undef
-  var data = google.visualization.arrayToDataTable(dataArray);
-  var options = {
-    title: 'Number of Years of Job Experience for Employees',
-    legend: { position: 'none' },
-    histogram: {
-      bucketSize: 5,
-      hideBucketItems: true
-      //lastBucketPercentile: 5
-    },
-    hAxis: {
-      title: 'Years of Experience'
-    },
-    vAxis: {
-      title: 'Quantity of Employees'
-    }
-  };
-
-  // eslint-disable-next-line no-undef
-  var chart = new google.visualization.Histogram(document.getElementById('google-job-exp-hist-chart'));
-  chart.draw(data, options);
-}
-
-function stringToDate(dateAsString) {
-  var date = moment(dateAsString);
-  return date;
-} //stringToDate
-
 async function fillData() {
   //Get data
   //Put into dictionary where key is kinda tech and value is quantity
@@ -233,68 +199,17 @@ async function fillData() {
     title: {
       display: true,
       text: 'Types of Technologies used by Employees'
-    }
+    },
+    responsive: true,
+    maintainAspectRatio: false
   };
-
   this.techChartDataReceived = true;
 }
 
-async function googleChart() {
-  let employees = await api.getItems(api.EMPLOYEES);
-  let technologies = {};
-
-  employees.forEach((employee) => {
-    if (employee.technologies) {
-      employee.technologies.forEach((currTech) => {
-        if (!technologies[currTech.name]) {
-          technologies[currTech.name] = 1;
-        } else {
-          technologies[currTech.name] += 1;
-        }
-      });
-    }
-  });
-
-  //We now sort the entries
-  let technologyPairs = Object.entries(technologies);
-  technologyPairs = technologyPairs.sort((a, b) => {
-    return b[1] - a[1];
-  });
-
-  technologyPairs = technologyPairs.slice(0, 5);
-
-  let colors = [
-    'color: pink; stroke-color: red; stroke-width: 1',
-    'color: #b6ecfc; stroke-color: #59bbf0; stroke-width: 1',
-    'color: #f6fab1; stroke-color: #c5c969; stroke-width: 1',
-    'color: #cdf2b6; stroke-color: #649447; stroke-width: 1',
-    'color: #d5b6f2; stroke-color: #a759f0; stroke-width: 1'
-  ];
-
-  for (let i = 0; i < technologyPairs.length; i++) {
-    technologyPairs[i].push(colors[i]);
-  }
-
-  technologyPairs.unshift(['Technology', 'Quantity', { role: 'style' }]);
-
-  /* eslint-disable-next-line no-undef */
-  let data = google.visualization.arrayToDataTable(technologyPairs);
-
-  //Set the chart data
-  let options = {
-    title: 'Types of technogolies employees use',
-    width: 750,
-    height: 500,
-    backgroundColor: 'transparent',
-    legend: 'none'
-  };
-
-  /* eslint-disable-next-line no-undef */
-  var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
-  chart.draw(data, options);
-
-  this.techChartDataReceived = true;
-}
+function stringToDate(dateAsString) {
+  var date = moment(dateAsString);
+  return date;
+} //stringToDate
 
 export default {
   components: {
@@ -306,8 +221,6 @@ export default {
       techChartData: null,
       techChartOptions: null,
       techChartDataReceived: false,
-      techGoogleChartData: null,
-      techGoogleChartOption: null,
       dataSet: [],
       optionSet: [],
       employees: [],
@@ -317,10 +230,8 @@ export default {
   },
   methods: {
     fillData,
-    googleChart,
     calculateTimeDifference,
     drawJobExpHistGraph,
-    googleJobExpHistGraph,
     jobExperienceData,
     stringToDate
   },
@@ -329,26 +240,16 @@ export default {
   },
   created: async function () {
     // eslint-disable-next-line no-undef
-    google.charts.load('current', { packages: ['corechart'] });
     this.employees = await api.getItems(api.EMPLOYEES);
     await this.jobExperienceData();
     this.drawJobExpHistGraph();
-    // eslint-disable-next-line no-undef
-    google.charts.setOnLoadCallback(this.googleJobExpHistGraph);
-    // eslint-disable-next-line no-undef
-    google.charts.setOnLoadCallback(this.googleChart);
   }
 };
 </script>
 
 <style scoped>
-#google-job-exp-hist-chart {
-  width: 900px;
-  height: 500px;
-}
-
 .techChart {
   height: 35%;
-  width: 50%;
+  width: 100%;
 }
 </style>
