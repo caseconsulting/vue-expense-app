d21c49e748f8c06f66a29fc1cfccb149b757fa87
thimler <thimler@consultwithcase.com>
2021-07-08 16:27:07 -0400

    2461-Fix-the-error-when-selecting-the-customer-org-tab-on-mobile-on-the-stats-page: Moved IC chart moved to customer org

--- src/components/charts/chartTabs/CertificationsChartTab.vue
+++ src/components/charts/chartTabs/CertificationsChartTab.vue
@@ -2,9 +2,6 @@
   <v-container>
     <v-row justify="center">
       <v-col md="6" sm="12" class="mt-4">
-        <i-c-table />
-      </v-col>
-      <v-col md="6" sm="12" class="mt-4">
         <certification-chart />
       </v-col>
     </v-row>
@@ -12,10 +9,9 @@
 </template>
 
 <script>
-import ICTable from '../customCharts/IcTable.vue';
 import CertificationChart from '../customCharts/CertificationChart.vue';
 
 export default {
-  components: { CertificationChart, ICTable }
+  components: { CertificationChart }
 };
 </script>
--- src/components/charts/chartTabs/CustomerOrgChartTab.vue
+++ src/components/charts/chartTabs/CustomerOrgChartTab.vue
@@ -2,16 +2,19 @@
   <v-container>
     <v-row justify="center">
       <v-col md="6" sm="12" class="mt-4">
+        <i-c-table />
+      </v-col>
+      <v-col md="6" sm="12" class="mt-4">
         <customer-org-chart />
       </v-col>
     </v-row>
   </v-container>
 </template>
-
 <script>
 import CustomerOrgChart from '../customCharts/CustomerOrgChart.vue';
+import ICTable from '../customCharts/IcTable.vue';
 
 export default {
-  components: { CustomerOrgChart }
+  components: { CustomerOrgChart, ICTable }
 };
 </script>
