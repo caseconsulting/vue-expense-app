a2004049ea0d6c67673a287d6992e55cc4eb63e3
cmartin <cmartin@consultwithcase.com>
2021-07-13 16:42:46 -0400

    1677-notify-user-of-badge-expiration: change labeling of modal

--- src/components/modals/BadgeExpirationModal.vue
+++ src/components/modals/BadgeExpirationModal.vue
@@ -2,8 +2,10 @@
   <v-row justify="center">
     <v-dialog v-model="dialog" persistent max-width="350">
       <v-card>
-        <v-card-title v-if="badgesExpiring.length == 1" class="headline"> Your Badge is Expiring </v-card-title>
-        <v-card-title v-else class="headline"> Your Badges are expiring </v-card-title>
+        <v-card-title v-if="badgesExpiring.length == 1" class="headline">
+          Clearance Badge Expiration Date Coming Up
+        </v-card-title>
+        <v-card-title v-else class="headline"> Clearance Badge Expiration </v-card-title>
         <v-card-text class="text--red">
           <ul>
             <li v-for="time in timesUntilExp" :key="time">You have a badge expiring in {{ time }} days</li>
@@ -20,6 +22,9 @@
 
 <script>
 export default {
+  created() {
+    this.badgesExpiring.sort((a, b) => a - b);
+  },
   data() {
     return {
       dialog: this.badgeExpiring,
