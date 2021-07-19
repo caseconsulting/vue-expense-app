939405315fe98232035b2e55046310ef0cb9376f
thimler <thimler@consultwithcase.com>
2021-07-19 14:27:36 -0400

    2554-Add-an-interface-for-employees-to-upload-resume: More styling changes

--- src/components/modals/ResumeParser.vue
+++ src/components/modals/ResumeParser.vue
@@ -3,9 +3,11 @@
     <v-card>
       <v-card-title class="header_style">Upload Resume</v-card-title>
       <v-card-text class="pa-5">
-        <div class="pa-3">
-          <v-row class="text-center">
-            <h3>Drag your PDF here</h3>
+        <v-container fluid>
+          <v-row>
+            <v-col class="text-center">
+              <h1>Add Your Resume Here</h1>
+            </v-col>
           </v-row>
           <v-row>
             <v-col cols="10" class="pr-0"><v-file-input label="Resume"></v-file-input></v-col>
@@ -13,19 +15,32 @@
               <v-btn color="green" outlined>Submit</v-btn>
             </v-col>
           </v-row>
-        </div>
-        <v-row class="text-center">
-          <v-col cols="6">
-            <v-card flat outlined>
-              <v-card-title>Current</v-card-title>
-            </v-card>
-          </v-col>
-          <v-col cols="6">
-            <v-card flat outlined>
-              <v-card-title>New</v-card-title>
-            </v-card>
-          </v-col>
-        </v-row>
+          <v-row class="text-center">
+            <v-col>
+              <h1>Pending Changes</h1>
+            </v-col>
+          </v-row>
+          <v-row class="text-center">
+            <v-col cols="5">
+              <h3>Currently on the Form</h3>
+            </v-col>
+            <v-col cols="5">
+              <h3>New Content</h3>
+            </v-col>
+          </v-row>
+          <v-row class="text-center">
+            <v-col cols="5">
+              <v-text-field disabled label="Main"> </v-text-field>
+            </v-col>
+            <v-col cols="5">
+              <v-text-field label="Main"> </v-text-field>
+            </v-col>
+            <v-col cols="2" class="pt-7">
+              <v-icon large left color="green">done</v-icon>
+              <v-icon large right color="red">close</v-icon>
+            </v-col>
+          </v-row>
+        </v-container>
       </v-card-text>
     </v-card>
   </v-dialog>
