32980b36682de6a21fd7150ed6aaaa99d0cbb963
thimler <thimler@consultwithcase.com>
2021-07-19 13:58:39 -0400

    2554-Add-an-interface-for-employees-to-upload-resume: Added styling to the upload modal

--- src/components/modals/ResumeParser.vue
+++ src/components/modals/ResumeParser.vue
@@ -3,10 +3,28 @@
     <v-card>
       <v-card-title class="header_style">Upload Resume</v-card-title>
       <v-card-text class="pa-5">
-        <v-row> Drag your PDF here </v-row>
-        <v-row>
-          <v-col>Current</v-col>
-          <v-col>New</v-col>
+        <div class="pa-3">
+          <v-row class="text-center">
+            <h3>Drag your PDF here</h3>
+          </v-row>
+          <v-row>
+            <v-col cols="10" class="pr-0"><v-file-input label="Resume"></v-file-input></v-col>
+            <v-col cols="2" class="text-center">
+              <v-btn color="green" outlined>Submit</v-btn>
+            </v-col>
+          </v-row>
+        </div>
+        <v-row class="text-center">
+          <v-col cols="6">
+            <v-card flat outlined>
+              <v-card-title>Current</v-card-title>
+            </v-card>
+          </v-col>
+          <v-col cols="6">
+            <v-card flat outlined>
+              <v-card-title>New</v-card-title>
+            </v-card>
+          </v-col>
         </v-row>
       </v-card-text>
     </v-card>
