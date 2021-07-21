bf6a29ff04781ca2d94a3d313a4b330df0b53ec2
rvellanki <rvellanki@consultwithcase.com>
2021-07-21 16:35:39 -0400

    2530-Current-address-should-only-be-visible-to-admins: Only admins can view everyones address and everyone else can view their own address

--- src/components/modals/ResumeParser.vue
+++ src/components/modals/ResumeParser.vue
@@ -74,6 +74,7 @@ async function submit() {
     let techComprehend = this.resumeObject.filter((entity) => {
       return entity.Type === 'TITLE';
     });
+    console.log(techComprehend);
 
     this.loading = false;
   }
