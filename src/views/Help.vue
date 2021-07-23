fae5c6b43f6858f208627f4c26fac248eeeca90b
litschre <litschre@dukes.jmu.edu>
2021-07-23 16:03:06 -0400

    2539: update help page

--- src/views/Help.vue
+++ src/views/Help.vue
@@ -14,19 +14,19 @@
       <v-expansion-panels accordion v-model="panel">
         <v-expansion-panel v-for="section in sections" :key="section[0]">
           <!-- Header -->
-          <v-expansion-panel-header
-            :id="section[0]"
-            v-if="section[1] == role || role == 'admin' || section[1] == 'user'"
-          >
+          <v-expansion-panel-header :id="section[0]" v-if="canView(section[1])">
             {{ section[0] }}
           </v-expansion-panel-header>
 
           <!-- Content -->
-          <v-expansion-panel-content v-if="section[1] == role || role == 'admin' || section[1] == 'user'">
+          <v-expansion-panel-content v-if="canView(section[1])">
             <v-card>
               <v-card-text class="grey lighten-3">
                 <li v-for="(ques, index) in section" :key="ques.title">
-                  <div v-if="ques.title" style="padding-bottom: 10px">
+                  <div
+                    v-if="ques.title && (!ques.employeeRole || canView(ques.employeeRole))"
+                    style="padding-bottom: 10px"
+                  >
                     <body style="font-style: italic" :id="section[0] + '-' + index">
                       <icon name="space-shuttle" /> {{ ques.title }}
                     </body>
@@ -63,6 +63,19 @@ async function created() {
   if (route == 'help3') {
     this.panel = 3;
   }
+} // created
+
+// |--------------------------------------------------|
+// |                                                  |
+// |                     METHODS                      |
+// |                                                  |
+// |--------------------------------------------------|
+
+/**
+ * Determine if userr can view.
+ */
+function canView(qRole) {
+  return qRole == this.role || this.role == 'admin' || qRole == 'user';
 } // created
 
 // |--------------------------------------------------|
@@ -179,14 +192,12 @@ export default {
           {
             title: "Why can't I delete an expense?",
             body: 'An expense cannot be deleted if it has been reimbursed or you are editing an expense.'
-          }
-        ],
-        myEpensesAdmin: [
-          'My Expenses (admin only)',
+          },
           {
             title: 'How to back-out of an expense',
             body:
-              'Admins may create expenses with a negative dollar amount. Negative expenses can be used as a short-cut way to unreimburse an expense that has already been reimbursed in a previous anniversary year and was overdrafted. All expenses to be unreimbursed in the current anniversary year however, should be unreimbursed using the unriembursed button on the expense page. '
+              'Admins may create expenses with a negative dollar amount. Negative expenses can be used as a short-cut way to unreimburse an expense that has already been reimbursed in a previous anniversary year and was overdrafted. All expenses to be unreimbursed in the current anniversary year however, should be unreimbursed using the unriembursed button on the expense page. ',
+            employeeRole: 'admin'
           }
         ],
         expenseTypes: [
@@ -201,46 +212,48 @@ export default {
             title: 'What is the overdraft flag?',
             body:
               'This allows an expense type to be charged up to twice the defined amount. Subtracting the overage from next years budget.'
-          }
-        ],
-        expenseTypesAdmin: [
-          'Expense Types (admin only)',
-          'admin',
+          },
           {
             title: 'What are categories?',
             body:
-              'Admins may create up to 10 different categories for employees to choose from when submitting and expense. Categories are unique to each expense type and users will be required to select one category if the expense type has them. '
+              'Admins may create up to 10 different categories for employees to choose from when submitting and expense. Categories are unique to each expense type and users will be required to select one category if the expense type has them. ',
+            employeeRole: 'admin'
           },
           {
             title: "Why can't I delete an expense type?",
             body:
-              'An expense type cannot be deleted if there are expenses for that expense type or you are editing an expense type.'
+              'An expense type cannot be deleted if there are expenses for that expense type or you are editing an expense type.',
+            employeeRole: 'admin'
           },
           {
             title: 'What are the employee access options for expense types?',
             body:
-              'There are 4 available employee access options to choose from, Full-time, Part-time, Intern, and Custom. The Full-time access option allow the expense type to be viewed and accessed by all employees that are full time. The Part-time option enables the expense type to be used for those who are part-time and have a percentage less than 100%. The Intern option allows the expense type to be viewed to those that have the Intern role. Finally, the Custom option allows you to pick one or more individuals to view the expense type regardless of their role.'
+              'There are 4 available employee access options to choose from, Full-time, Part-time, Intern, and Custom. The Full-time access option allow the expense type to be viewed and accessed by all employees that are full time. The Part-time option enables the expense type to be used for those who are part-time and have a percentage less than 100%. The Intern option allows the expense type to be viewed to those that have the Intern role. Finally, the Custom option allows you to pick one or more individuals to view the expense type regardless of their role.',
+            employeeRole: 'admin'
           },
           {
             title: 'What does the "Should the expense be pro-rated?" switch mean?',
             body:
-              'By toggling this switch, it would mean that all part-time employees that can access the expense type would have a percentage of the budget amount based on their part-time percentage.'
+              'By toggling this switch, it would mean that all part-time employees that can access the expense type would have a percentage of the budget amount based on their part-time percentage.',
+            employeeRole: 'admin'
           },
           {
             title: 'What does the "Does this expense type have a recipient?" switch mean?',
             body:
-              'This would mean that upon creating an expense for this expense type, the user would have to assign someone else within the company to receive the amount provided within the expense.'
+              'This would mean that upon creating an expense for this expense type, the user would have to assign someone else within the company to receive the amount provided within the expense.',
+            employeeRole: 'admin'
           },
-
           {
             title: 'What does the "Have this expense type show on the company feed?" switch mean?',
             body:
-              'When a user creates an expense and after being reimbursed, it will display info about the expense on the activity feed in the Home page. This feature can be toggled off by the user when creating an expense or by the person reimbursing the expense.'
+              'When a user creates an expense and after being reimbursed, it will display info about the expense on the activity feed in the Home page. This feature can be toggled off by the user when creating an expense or by the person reimbursing the expense.',
+            employeeRole: 'admin'
           },
           {
             title: 'What does the "Require a url for this expense?" switch mean?',
             body:
-              'When a user creates an expense, by default entering a url is optional. However, by enabling this feature, this would make entering a url mandatory.'
+              'When a user creates an expense, by default entering a url is optional. However, by enabling this feature, this would make entering a url mandatory.',
+            employeeRole: 'admin'
           }
         ],
         reimbursements: [
@@ -260,26 +273,25 @@ export default {
             body: "An employee's ID number will be a user's hire order. This number also corresponds to TSheets data."
           },
           {
-            title: 'What are the different employee roles?',
-            body:
-              'There are currently 4 employee roles (user, admin, manager, intern). Users main function is to be able to submit expenses. Admins have all functionality. Managers have the same access as a user but can also edit any employee data. Interns do not have access to any budgets by default.'
-          }
-        ],
-        employeesAdmin: [
-          'Employees (admin only)',
+            title: 'What is the manager role for?',
+            body: 'Managers have the same access as a user but can also edit or view any employee data. They should have the same access as admins on the employee pages.',
+            employeeRole: 'manager'
+          },
           {
             title: 'What capabilities do Admins have?',
             body:
-              "Admins may view reimbursements, reimburse expenses, and create expense types, view each employee's budget home view, create new employees, and create expenses for other employees"
+              "Admins may view reimbursements, reimburse expenses, and create expense types, view each employee's budget home view, create new employees, and create expenses for other employees",
+            employeeRole: 'admin'
           },
-          'admin',
           {
             title: 'What does it mean to mark an employee inactive?',
-            body: "This will freeze the user's ability to submit new expenses."
+            body: "This will freeze the user's ability to submit new expenses.",
+            employeeRole: 'admin'
           },
           {
             title: "Why can't I delete an employee?",
-            body: 'An employee cannot be deleted if the employee has expenses or you are editing an employee.'
+            body: 'An employee cannot be deleted if the employee has expenses or you are editing an employee.',
+            employeeRole: 'admin'
           }
         ],
         knownIssues: [
@@ -292,6 +304,9 @@ export default {
         ]
       } // faq sections
     };
+  },
+  methods: {
+    canView
   }
 };
 </script>
