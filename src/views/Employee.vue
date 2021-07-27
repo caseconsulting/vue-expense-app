5a7974195d9f09d58fcf66e0f08a3b0dc2c2a0ed
rvellanki <rvellanki@consultwithcase.com>
2021-07-27 10:51:20 -0400

    2619-Relocate-Delete-Resume-button: Removes Delete Resume button while editing the form

--- src/views/Employee.vue
+++ src/views/Employee.vue
@@ -16,6 +16,7 @@
         >
         <v-btn
           class="white--text ma-2"
+          v-if="!editing"
           color="#bc3825"
           @click="deleteResume"
           :disabled="!hasResume"
