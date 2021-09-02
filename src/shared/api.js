a251e4ecf8a79f6b97e889121a32f32113ba4fee
zramsis <zramsis@consultwithcase.com>
2021-09-02 14:28:35 -0400

    2846-promises-to-async: api changed the rest of the promises to async, also made all the functions that use execute async

--- src/shared/api.js
+++ src/shared/api.js
@@ -71,8 +71,8 @@ async function getTechSkills(tech) {
   return techNames;
 }
 
-function getAllActiveEmployeeBudgets(id) {
-  return execute('get', `/${UTILITY}/getAllActiveEmployeeBudgets/${id}`);
+async function getAllActiveEmployeeBudgets(id) {
+  return await execute('get', `/${UTILITY}/getAllActiveEmployeeBudgets/${id}`);
 }
 
 /**
@@ -80,8 +80,8 @@ function getAllActiveEmployeeBudgets(id) {
  *
  * @return object - Employee Basecamp avatar data
  */
-function getBasecampAvatars() {
-  return execute('get', `/${BASECAMP}/getBasecampAvatars`);
+async function getBasecampAvatars() {
+  return await execute('get', `/${BASECAMP}/getBasecampAvatars`);
 } // getBasecampAvatars
 
 /**
@@ -89,24 +89,24 @@ function getBasecampAvatars() {
  *
  * @return object - Basecamp Campfire data
  */
-function getBasecampCampfires() {
-  return execute('get', `/${BASECAMP}/getBasecampCampfires`);
+async function getBasecampCampfires() {
+  return await execute('get', `/${BASECAMP}/getBasecampCampfires`);
 } // getBasecampCampfires
 
-function getEmployeeBudget(id, expenseTypeId, date) {
-  return execute('get', `/${UTILITY}/getEmployeeBudget/${id}/${expenseTypeId}/${date}`);
+async function getEmployeeBudget(id, expenseTypeId, date) {
+  return await execute('get', `/${UTILITY}/getEmployeeBudget/${id}/${expenseTypeId}/${date}`);
 }
 
-function getFiscalDateViewBudgets(id, fiscalDateView) {
-  return execute('get', `/${UTILITY}/getFiscalDateViewBudgets/${id}/${fiscalDateView}`);
+async function getFiscalDateViewBudgets(id, fiscalDateView) {
+  return await execute('get', `/${UTILITY}/getFiscalDateViewBudgets/${id}/${fiscalDateView}`);
 }
 
-function getEmployeeBudgets(id) {
-  return execute('get', `/budgets/employee/${id}`);
+async function getEmployeeBudgets(id) {
+  return await execute('get', `/budgets/employee/${id}`);
 }
 
-function getAllEvents() {
-  return execute('get', `/${UTILITY}/getAllEvents`);
+async function getAllEvents() {
+  return await execute('get', `/${UTILITY}/getAllEvents`);
 }
 
 // function getBudgetsByDate(id, date) {
@@ -117,282 +117,175 @@ function getAllEvents() {
 //   return execute('get', `/${UTILITY}/getEmployeeBudgets/${id}/${date}/${expenseTypeId}`);
 // }
 
-function getAudits(type, startDate, endDate) {
-  return execute('get', `/${AUDIT}/${type}/${startDate}/${endDate}`);
+async function getAudits(type, startDate, endDate) {
+  return await execute('get', `/${AUDIT}/${type}/${startDate}/${endDate}`);
 }
 
-function getItems(type) {
-  return execute('get', `/${type}`);
+async function getItems(type) {
+  return await execute('get', `/${type}`);
 }
 
-function getItem(type, id) {
-  return execute('get', `/${type}/${id}`);
+async function getItem(type, id) {
+  return await execute('get', `/${type}/${id}`);
 }
 
-function getAllEmployeeExpenses(id) {
-  return execute('get', `/${UTILITY}/getAllEmployeeExpenses/${id}`);
+async function getAllEmployeeExpenses(id) {
+  return await execute('get', `/${UTILITY}/getAllEmployeeExpenses/${id}`);
 }
 
-function getAllExpenseTypeExpenses(id) {
-  return execute('get', `/${UTILITY}/getAllExpenseTypeExpenses/${id}`);
+async function getAllExpenseTypeExpenses(id) {
+  return await execute('get', `/${UTILITY}/getAllExpenseTypeExpenses/${id}`);
 }
 
-function getURLInfo(id, category) {
-  return execute('get', `/${TRAINING_URLS}/'${id}'/${category}`);
+async function getURLInfo(id, category) {
+  return await execute('get', `/${TRAINING_URLS}/'${id}'/${category}`);
 }
 
-function createItem(type, data) {
-  return execute('post', `/${type}`, data);
+async function createItem(type, data) {
+  return await execute('post', `/${type}`, data);
 }
 
-function updateItem(type, data) {
-  return execute('put', `/${type}`, data);
+async function updateItem(type, data) {
+  return await execute('put', `/${type}`, data);
 }
 
-function deleteItem(type, id) {
-  return execute('delete', `/${type}/${id}`);
+async function deleteItem(type, id) {
+  return await execute('delete', `/${type}/${id}`);
 }
 
-function getAllAggregateExpenses() {
-  return execute('get', `/${UTILITY}/getAllAggregateExpenses`);
+async function getAllAggregateExpenses() {
+  return await execute('get', `/${UTILITY}/getAllAggregateExpenses`);
 }
 
-function getAllExpenses() {
-  return execute('get', `/${UTILITY}/getAllExpenses`);
+async function getAllExpenses() {
+  return await execute('get', `/${UTILITY}/getAllExpenses`);
 }
 
-function getRole() {
-  return execute('get', 'info/role');
+async function getRole() {
+  return await execute('get', 'info/role');
 }
-function getUser() {
-  return execute('get', 'info/me');
-}
-
-function getAttachment(employeeId, expenseId) {
-  return execute('get', `attachment/${employeeId}/${expenseId}`);
+async function getUser() {
+  return await execute('get', 'info/me');
 }
 
-function getBlogFile(authorId, blogId) {
-  return execute('get', `${BLOG_FILE}/${authorId}/${blogId}`);
+async function getAttachment(employeeId, expenseId) {
+  return await execute('get', `attachment/${employeeId}/${expenseId}`);
 }
 
-function getPictureFile(authorId, blogId, mainPicture) {
-  return execute('get', `${BLOG_FILE}/${authorId}/${blogId}/${mainPicture}`);
+async function getBlogFile(authorId, blogId) {
+  return await execute('get', `${BLOG_FILE}/${authorId}/${blogId}`);
 }
 
-function getLocation(locationQuery) {
-  return execute('get', `${GOOGLE_MAPS}/getLocation/${locationQuery}`);
+async function getPictureFile(authorId, blogId, mainPicture) {
+  return await execute('get', `${BLOG_FILE}/${authorId}/${blogId}/${mainPicture}`);
 }
 
-function getZipCode(addressId) {
-  return execute('get', `${GOOGLE_MAPS}/getZipCode/${addressId}`);
+async function getLocation(locationQuery) {
+  return await execute('get', `${GOOGLE_MAPS}/getLocation/${locationQuery}`);
+}
+
+async function getZipCode(addressId) {
+  return await execute('get', `${GOOGLE_MAPS}/getZipCode/${addressId}`);
 }
 
 async function deleteResume(employeeId) {
-  // inject the accessToken for each request
-  let accessToken = getAccessToken();
-  return client({
-    method: 'delete',
-    url: `${RESUME}/${employeeId}`,
-    headers: {
-      Authorization: `Bearer ${accessToken}`
-    }
-  })
-    .then((response) => {
-      return response.data;
-    })
-    .catch(() => {
-      return null;
-    });
+  return await execute('delete', `${RESUME}/${employeeId}`);
 }
 
 async function extractText(employeeId, file) {
   let formData = new FormData();
   formData.append('receipt', file);
 
-  // inject the accessToken for each request
-  let accessToken = getAccessToken();
-
-  return client({
-    method: 'put',
-    url: `/attachment/${employeeId}/${file.name}`,
-    data: formData,
-    headers: {
-      Authorization: `Bearer ${accessToken}`
-    }
-  })
-    .then((response) => {
-      return response.data;
-    })
-    .catch((err) => {
-      return err;
-    });
+  return await execute('put', `/attachment/${employeeId}/${file.name}`, formData);
 }
 
 async function extractResumeText(employeeId, file) {
   let formData = new FormData();
   formData.append('resume', file);
 
-  // inject the accessToken for each request
-  let accessToken = getAccessToken();
-
-  return client({
-    method: 'put',
-    url: `/${RESUME}/${employeeId}`,
-    data: formData,
-    headers: {
-      Authorization: `Bearer ${accessToken}`
-    }
-  })
-    .then((response) => {
-      return response.data;
-    })
-    .catch((err) => {
-      return err;
-    });
+  return await execute('put', `/${RESUME}/${employeeId}`, formData);
 }
 
 async function getResume(employeeId) {
   // inject the accessToken for each request
   let accessToken = getAccessToken();
-  return client({
-    method: 'get',
-    url: `${RESUME}/${employeeId}`,
-    headers: {
-      Authorization: `Bearer ${accessToken}`
-    }
-  })
-    .then((response) => {
-      return response.data;
-    })
-    .catch(() => {
-      return null;
+
+  try {
+    let c = await client({
+      method: 'get',
+      url: `${RESUME}/${employeeId}`,
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      }
     });
+    return c.data;
+  } catch (err) {
+    return null;
+  }
 }
 
 async function createAttachment(expense, file) {
   let formData = new FormData();
   formData.append('receipt', file);
 
-  // inject the accessToken for each request
-  let accessToken = getAccessToken();
-  return client({
-    method: 'post',
-    url: `/attachment/${expense.employeeId}/${expense.id}`,
-    data: formData,
-    headers: {
-      Authorization: `Bearer ${accessToken}`
-    }
-  })
-    .then((response) => {
-      return response.data;
-    })
-    .catch((err) => {
-      return err;
-    });
+  return await execute('post', `/attachment/${expense.employeeId}/${expense.id}`, formData);
 }
 
 async function createBlogFile(blogPost, file, fileName) {
   let formData = new FormData();
   formData.append('blogFile', file, fileName);
 
-  // inject the accessToken for each request
-  let accessToken = getAccessToken();
-  return client({
-    method: 'post',
-    url: `/${BLOG_FILE}/${blogPost.authorId}/${blogPost.id}`,
-    data: formData,
-    headers: {
-      Authorization: `Bearer ${accessToken}`
-    }
-  })
-    .then((response) => {
-      return response.data;
-    })
-    .catch((err) => {
-      return err;
-    });
+  return await execute('post', `/${BLOG_FILE}/${blogPost.authorId}/${blogPost.id}`, formData);
 }
 
-function deleteAttachment(expense) {
-  return execute('delete', `attachment/${expense.employeeId}/${expense.id}/${expense.receipt}`);
+async function deleteAttachment(expense) {
+  return await execute('delete', `attachment/${expense.employeeId}/${expense.id}/${expense.receipt}`);
 }
 
-function deleteBlogFile(blogPost) {
-  return execute(
+async function deleteBlogFile(blogPost) {
+  return await execute(
     'delete',
     `${BLOG_FILE}/${blogPost.authorId}/${blogPost.id}/${blogPost.fileName}/${blogPost.mainPicture}`
   );
 }
 
 //functions for QuickBooks time
-function getPTOBalances(employeeNumber) {
-  return execute('get', `/${QUICK_BOOKS_TIME}/getPTOBalances/${employeeNumber}`);
+async function getPTOBalances(employeeNumber) {
+  return await execute('get', `/${QUICK_BOOKS_TIME}/getPTOBalances/${employeeNumber}`);
 }
 
-function getMonthlyHours(employeeNumber) {
-  return execute('get', `/${QUICK_BOOKS_TIME}/getMonthlyHours/${employeeNumber}`);
+async function getMonthlyHours(employeeNumber) {
+  return await execute('get', `/${QUICK_BOOKS_TIME}/getMonthlyHours/${employeeNumber}`);
 }
-function getTwitterToken() {
-  return execute('get', `/${TWITTER}/getTwitterToken`);
+async function getTwitterToken() {
+  return await execute('get', `/${TWITTER}/getTwitterToken`);
 }
-function getCaseTimeline() {
-  return execute('get', `${TWITTER}/getCaseTimeline`);
+async function getCaseTimeline() {
+  return await execute('get', `${TWITTER}/getCaseTimeline`);
 }
-function getFeedEvents() {
-  return execute('get', `/${BASECAMP}/getFeedEvents`);
+async function getFeedEvents() {
+  return await execute('get', `/${BASECAMP}/getFeedEvents`);
 }
-function getModerationLabel(img) {
-  return execute('post', `${BLOG_ATTACHMENT}/getModerationLabel/${img}`);
-}
-
-function getKeyPhrases(data) {
-  return execute('post', `${BLOG_ATTACHMENT}/getKeyPhrases`, data);
+async function getModerationLabel(img) {
+  return await execute('post', `${BLOG_ATTACHMENT}/getModerationLabel/${img}`);
 }
 
-function uploadResume(employeeId, file) {
+async function getKeyPhrases(data) {
+  return await execute('post', `${BLOG_ATTACHMENT}/getKeyPhrases`, data);
+}
+
+async function uploadResume(employeeId, file) {
   let formData = new FormData();
   formData.append('resume', file);
 
-  // inject the accessToken for each request
-  let accessToken = getAccessToken();
-
-  return client({
-    method: 'post',
-    url: `/${RESUME}/${employeeId}`,
-    data: formData,
-    headers: {
-      Authorization: `Bearer ${accessToken}`
-    }
-  })
-    .then((response) => {
-      return response.data;
-    })
-    .catch((err) => {
-      return err;
-    });
+  return await execute('post', `/${RESUME}/${employeeId}`, formData);
 }
 
 async function uploadBlogAttachment(file) {
   let formData = new FormData();
   formData.append('image', file);
 
-  // inject the accessToken for each request
-  let accessToken = getAccessToken();
-  return client({
-    method: 'post',
-    url: `${BLOG_ATTACHMENT}/uploadBlogAttachmentToS3/${file.name}`,
-    data: formData,
-    headers: {
-      Authorization: `Bearer ${accessToken}`
-    }
-  })
-    .then((response) => {
-      return response.data;
-    })
-    .catch((err) => {
-      return err;
-    });
+  return await execute('post', `${BLOG_ATTACHMENT}/uploadBlogAttachmentToS3/${file.name}`, formData);
 }
 
 /**
@@ -400,39 +293,32 @@ async function uploadBlogAttachment(file) {
  * @returns a list of colleges that match that query
  */
 async function getColleges(inputValue) {
-  return execute('get', `/${HIPPO_LAB}/getColleges/${inputValue}`)
-    .then((response) => {
-      for (let i = 0; i < response.length; i++) {
-        if (response[i] === 'Virginia Polytechnic Institute and State University') {
-          response[i] = 'Virginia Polytechnic Institute and State University (Virginia Tech)';
-        } else if (response[i] === 'University of Mississippi') {
-          response[i] = 'University of Mississippi (Ole Miss)';
-        } else if (response[i] === 'United States Military Academy') {
-          response[i] = 'United States Military Academy (West Point)';
-        } else if (response[i] === 'Northern Virginia Community College') {
-          response[i] = 'Northern Virginia Community College (NoVa)';
-        } else if (response[i] === 'Georgia Institute of Technology') {
-          response[i] = 'Georgia Institute of Technology (Georgia Tech)';
-        } else if (response[i] === 'Florida Institute of Technology') {
-          response[i] = 'Florida Institute of Technology (Florida Tech)';
-        } else if (response[i] === 'City University of New York') {
-          response[i] = 'City University of New York (City Tech)';
-        } else if (response[i] === 'California Institute of Technology') {
-          response[i] = 'California Institute of Technology (Caltech)';
-        }
-      }
-      return response;
-    })
-    .catch(() => {
-      return [];
-    });
-  // let list = await execute('get', `http://universities.hipolabs.com/search?name=${inputValue}`);
+  try {
+    let response = await execute('get', `/${HIPPO_LAB}/getColleges/${inputValue}`);
 
-  // let finalColleges = [];
-  // for (let i = 0; i < list.length; i++) {
-  //   finalColleges.push(list[i].name);
-  // }
-  //return finalColleges;
+    for (let i = 0; i < response.length; i++) {
+      if (response[i] === 'Virginia Polytechnic Institute and State University') {
+        response[i] = 'Virginia Polytechnic Institute and State University (Virginia Tech)';
+      } else if (response[i] === 'University of Mississippi') {
+        response[i] = 'University of Mississippi (Ole Miss)';
+      } else if (response[i] === 'United States Military Academy') {
+        response[i] = 'United States Military Academy (West Point)';
+      } else if (response[i] === 'Northern Virginia Community College') {
+        response[i] = 'Northern Virginia Community College (NoVa)';
+      } else if (response[i] === 'Georgia Institute of Technology') {
+        response[i] = 'Georgia Institute of Technology (Georgia Tech)';
+      } else if (response[i] === 'Florida Institute of Technology') {
+        response[i] = 'Florida Institute of Technology (Florida Tech)';
+      } else if (response[i] === 'City University of New York') {
+        response[i] = 'City University of New York (City Tech)';
+      } else if (response[i] === 'California Institute of Technology') {
+        response[i] = 'California Institute of Technology (Caltech)';
+      }
+    }
+    return response;
+  } catch (err) {
+    return [];
+  }
 }
 
 export default {
