8e165c39046de11d6feee496429a15e93fb9abf4
Michael Frank <mfrankwork@gmail.com>
2022-02-19 16:49:40 -0500

    updates: Upgrade to eslint 8, eslint-plugin-vue 8, @vue/eslint-config-prettier 7. Switch to @babel/eslint-parser. Remove @vue/cli-plugin-eslint 4, which does not work with eslint 8.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: '@babel/eslint-parser',
     ecmaVersion: 9,
     sourceType: 'module'
   },
@@ -11,11 +11,13 @@ module.exports = {
     jest: true,
     node: true
   },
-  extends: ['eslint:recommended', 'plugin:vue/essential', '@vue/prettier', 'plugin:prettier/recommended'],
+  extends: ['eslint:recommended', 'plugin:vue/essential', '@vue/eslint-config-prettier'],
   rules: {
     // allow console during development
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // allow component names to be a single word
+    'vue/multi-word-component-names': 'off'
   }
 };
