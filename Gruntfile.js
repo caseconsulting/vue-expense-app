7b23ce83ff1c27b79336029a4eea298baacbbd9f
Michael Frank <mfrankwork@gmail.com>
2022-05-04 18:33:36 -0400

    3618-grunt-shell: Switch from grunt-aws-s3 to grunt-shell and AWS CLI commands

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,58 +1,64 @@
-module.exports = function (grunt) {
-  grunt.loadNpmTasks('grunt-aws-s3');
+const _ = require('lodash');
+const fs = require('fs');
+
+// Returns bucket name for specified stage
+function _getBucketName(stage) {
+  return stage === 'prod' ? `case-consulting-expense-app-${stage}` : `case-expense-app-${stage}`;
+}
+
+// Returns true if path references a file
+function _isFile(path) {
+  return fs.statSync(`dist/${path}`).isFile();
+}
+
+module.exports = (grunt) => {
+  grunt.loadNpmTasks('grunt-shell');
   grunt.initConfig({
-    aws_s3: {
-      options: {
-        access: 'private',
-        params: {
-          CacheControl: 'max-age=600, s-maxage=300'
-        },
-        differential: true,
-        region: 'us-east-1'
-      },
-      dev: {
-        options: {
-          bucket: 'case-consulting-expense-app-dev'
-        },
-        files: [
-          {
-            expand: true,
-            cwd: 'dist/',
-            src: ['**'],
-            dest: '/'
-          }
-        ]
-      },
-      test: {
-        options: {
-          bucket: 'case-consulting-expense-app-test'
-        },
-        files: [
-          {
-            expand: true,
-            cwd: 'dist/',
-            src: ['**'],
-            dest: '/'
-          }
-        ]
-      },
-      prod: {
-        options: {
-          bucket: 'case-consulting-expense-app-prod',
-          awsProfile: 'prod'
-        },
-        files: [
-          {
-            expand: true,
-            cwd: 'dist/',
-            src: ['**'],
-            dest: '/'
-          }
-        ]
+    shell: {
+      deploy: {
+        command: (stage) => {
+          // Get S3 bucket name
+          const bucket = _getBucketName(stage);
+
+          // Define source and destination paths
+          let paths = {
+            'appleTouchIcon.png': 'appleTouchIcon.png',
+            css: 'css',
+            'favicon.ico': 'favicon.ico',
+            img: 'img',
+            'index.html': 'index.html',
+            js: 'js',
+            'navlogo.png': 'navlogo.png'
+          };
+
+          // Add remove commands
+          let commands = [];
+          commands.push(`aws s3 rm s3://${bucket}/ --recursive`);
+
+          // Add copy commands
+          const recursive = '--recursive --exclude "*.map"';
+          const security = '--acl private';
+          const cacheControl = stage === 'dev' ? 'max-age=600,s-maxage=300' : 'max-age=600,s-maxage=300';
+          const metadata = `--metadata-directive REPLACE --cache-control ${cacheControl}`;
+          _.each(paths, (source, destination) => {
+            commands.push(
+              _.compact([
+                `aws s3 cp dist/${source} s3://${bucket}/${destination}`,
+                _isFile(source) ? '' : recursive,
+                security,
+                metadata
+              ]).join(' ')
+            );
+          });
+
+          // Return one really long command
+          return commands.join(' && ');
+        }
       }
     }
   });
-  grunt.registerTask('deploy:dev', 'aws_s3:dev');
-  grunt.registerTask('deploy:test', 'aws_s3:test');
-  grunt.registerTask('deploy:prod', 'aws_s3:prod');
+
+  grunt.registerTask('deploy:dev', 'shell:deploy:dev');
+  grunt.registerTask('deploy:test', 'shell:deploy:test');
+  grunt.registerTask('deploy:prod', 'shell:deploy:prod');
 };
