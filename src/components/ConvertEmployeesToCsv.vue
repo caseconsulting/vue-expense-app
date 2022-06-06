6707b05565ed45c48cad57f96ede90fcf2d96011
jhendricks <jhendricks@consultwithcase.com>
2022-06-06 17:37:52 -0400

    3684-add-concentrations/minors-to-employees-csv: add concentrations and minors to csv

--- src/components/ConvertEmployeesToCsv.vue
+++ src/components/ConvertEmployeesToCsv.vue
@@ -351,6 +351,28 @@ function getEducation(edu) {
           str += major;
         });
 
+        if (!_.isEmpty(degree.concentrations)) {
+          str += ' (Concentrations: ';
+          degree.concentrations.forEach((concentration, i) => {
+            if (i != 0) {
+              str += ', ';
+            }
+            str += concentration;
+          });
+          str += ')';
+        }
+
+        if (!_.isEmpty(degree.minors)) {
+          str += ' (Minors: ';
+          degree.minors.forEach((minor, i) => {
+            if (i != 0) {
+              str += ', ';
+            }
+            str += minor;
+          });
+          str += ')';
+        }
+
         str += ' - ' + degree.completionDate;
         result.push(str); // push each degree individually
       });
