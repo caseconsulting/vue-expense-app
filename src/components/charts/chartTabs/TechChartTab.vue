6ff1db718f8f543ac1310bbfbb2d1c8c3d0cfb0b
jhendricks <jhendricks@consultwithcase.com>
2022-06-16 16:15:10 -0400

    2883-add-tech-word-cloud: get wordcloud showing

--- src/components/charts/chartTabs/TechChartTab.vue
+++ src/components/charts/chartTabs/TechChartTab.vue
@@ -1,6 +1,16 @@
 <template>
   <v-container fluid>
     <v-row justify="center">
+      <v-col xl="6" lg="8" sm="12">
+        <vue-word-cloud
+          style="height: 280px; width: 640px"
+          :words="words"
+          :color="([, weight]) => (weight > 10 ? 'red' : weight > 5 ? 'blue' : 'grey')"
+          font-family="Roboto"
+        />
+      </v-col>
+    </v-row>
+    <v-row justify="center">
       <v-col xl="6" lg="8" sm="12" class="mt-4">
         <tech-bar-chart></tech-bar-chart>
       </v-col>
@@ -8,8 +18,12 @@
   </v-container>
 </template>
 
+<script type="application/javascript" src="https://unpkg.com/vue"></script>
+<script type="application/javascript" src="https://unpkg.com/vuewordcloud"></script>
+
 <script>
 import TechBarChart from '../customCharts/TechBarChart.vue';
+import VueWordCloud from 'vuewordcloud';
 
 // |--------------------------------------------------|
 // |                                                  |
@@ -18,6 +32,16 @@ import TechBarChart from '../customCharts/TechBarChart.vue';
 // |--------------------------------------------------|
 
 export default {
-  components: { TechBarChart }
+  components: { TechBarChart, [VueWordCloud.name]: VueWordCloud },
+  data() {
+    return {
+      words: [
+        ['romance', 19],
+        ['horror', 3],
+        ['fantasy', 7],
+        ['adventure', 3]
+      ]
+    };
+  }
 };
 </script>
