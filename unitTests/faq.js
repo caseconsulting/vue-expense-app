2233d9fe33383a2a16fd06b960d1df64ac232f6a
logburn <logburn@consultwithcase.com>
2022-06-20 14:50:25 -0400

    3765-faq: made sure faq worked

--- unitTests/faq.js
+++ unitTests/faq.js
@@ -1,20 +1,23 @@
 describe('testing links', () => {
   before((browser) => {
+    browser.url('http://localhost:8080');
+    browser.windowMaximize('current'); // maximize window
     browser
-      .url('http://localhost:8080')
-      .windowMaximize('current') // maximize window
-      .waitForElementVisible('#custom-button-color') // wait for login button to load
+      .waitForElementVisible('#custom-button-color', 30000) // wait for login button to load
+      .pause(500)
       .click('#custom-button-color') // click that button
-      .assert.visible('img.auth0-lock-header-logo') // check that the case logo is there in auth0
-      .assert.visible('div.auth0-lock-content') // check that the auth0 login button is visible
+      .waitForElementVisible('img.auth0-lock-header-logo', 30000) // check that the case logo is there in auth0
+      .waitForElementVisible('div.auth0-lock-content', 30000) // check that the auth0 login button is visible
       .click('div.auth0-lock-social-button-text') // click the button
-      .waitForElementVisible('input#identifierId') // wait for email box to be visible
+      .waitForElementVisible('input#identifierId', 30000) // wait for email box to be visible
+      .pause(500)
       .setValue('input#identifierId', process.env.VUE_APP_AUTH0_EMAIL) // input your email
       .keys(browser.Keys.ENTER) // click continue button
-      .waitForElementVisible('#password') // wait for password field to load
+      .waitForElementVisible('#password', 30000) // wait for password field to load
+      .pause(500)
       .setValue('input[type="password"]', process.env.VUE_APP_AUTH0_PASSWORD) // input your password
       .keys(browser.Keys.ENTER) // click continue button
-      .waitForElementVisible('h1.d-inline'); // wait for header text 'Case Portal'
+      .waitForElementVisible('h1.d-inline', 30000); // wait for header text 'Case Portal'
   });
 
   this.tags = ['faq'];
