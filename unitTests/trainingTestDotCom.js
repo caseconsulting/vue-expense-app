ba5523fc3d621105783c1dff1e61a12919e9dda8
logburn <logburn@consultwithcase.com>
2022-06-20 13:23:29 -0400

    3771-training-page: fixed test

--- unitTests/trainingTestDotCom.js
+++ unitTests/trainingTestDotCom.js
@@ -8,25 +8,25 @@ describe('testing sign-in', () => {
 
   it('Test redirect to Oauth Signin and then app', (browser) => {
     browser
-      .waitForElementVisible('#custom-button-color') // wait for login button to load
+      .waitForElementVisible('#custom-button-color', 30000) // wait for login button to load
       .click('#custom-button-color') // click that button
       .assert.visible('img.auth0-lock-header-logo') // check that the case logo is there in auth0
       .assert.visible('div.auth0-lock-content') // check that the auth0 login button is visible
       .click('div.auth0-lock-social-button-text') // click the button
-      .waitForElementVisible('input#identifierId') // wait for email box to be visible
+      .waitForElementVisible('input#identifierId', 30000) // wait for email box to be visible
       .setValue('input#identifierId', process.env.VUE_APP_AUTH0_EMAIL) // input your email
       .keys(browser.Keys.ENTER) // click continue button
-      .waitForElementVisible('#password') // wait for password field to load
+      .waitForElementVisible('#password', 30000) // wait for password field to load
       .setValue('input[type="password"]', process.env.VUE_APP_AUTH0_PASSWORD) // input your password
-      .keys(browser.Keys.ENTER) // click continue button
-      .waitForElementVisible('h1.d-inline'); // wait for header text 'Case Portal'
+      .keys(browser.Keys.ENTER) // click login button
+      .waitForElementVisible('h1.d-inline', 30000); // wait for header text 'Case Portal'
   });
 
   it('Test navigate to Training', (browser) => {
     browser
-      .waitForElementVisible('#fire')
-      .click('#fire')
+      .waitForElementVisible('#mdi-fire')
+      .click('#mdi-fire')
       .useXpath()
-      .waitForElementVisible("//*[contains(text(),'https://test.com')]");
+      .waitForElementVisible("//*[contains(text(),'https://vt.edu')]");
   });
 });
