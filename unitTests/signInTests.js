94e14084824ed4e2aad6f2743973d6ae69057cdd
jhendricks <jhendricks@consultwithcase.com>
2022-06-20 15:06:42 -0400

    3769-pass-signInTests: make sign in section consistant

--- unitTests/signInTests.js
+++ unitTests/signInTests.js
@@ -1,36 +1,23 @@
 describe('testing sign-in', () => {
   before((browser) => {
-    browser.url('http://localhost:8080');
-    browser.windowMaximize('current'); // maximize window
-  });
-
-  this.tags = ['sign-in'];
-
-  it('Test load of Oauth sign in page and its elements', (browser) => {
-    browser.assert
-      .visible('.case_logo') // main case logo
-      .assert.visible('#custom-button-color') // button to log in
-      .assert.visible('h1.d-inline') // text for case
-      .assert.visible('h1.my-4') // case portal text in middle of page
-      .assert.visible('img.logo-bar'); // Logo on the top bar
-  });
-
-  it('Test redirect to Oauth Signin and then app', (browser) => {
     browser
+      .url('http://localhost:8080')
+      .windowMaximize('current') // maximize window
       .waitForElementVisible('#custom-button-color', 30000) // wait for login button to load
       .click('#custom-button-color') // click that button
-      .assert.visible('img.auth0-lock-header-logo') // check that the case logo is there in auth0
-      .assert.visible('div.auth0-lock-content') // check that the auth0 login button is visible
-      .pause(1000)
+      .waitForElementVisible('img.auth0-lock-header-logo') // check that the case logo is there in auth0
+      .waitForElementVisible('div.auth0-lock-content') // check that the auth0 login button is visible
       .click('div.auth0-lock-social-button-text') // click the button
       .waitForElementVisible('input#identifierId', 30000) // wait for email box to be visible
       .setValue('input#identifierId', process.env.VUE_APP_AUTH0_EMAIL) // input your email
       .keys(browser.Keys.ENTER) // click continue button
       .waitForElementVisible('#password', 30000) // wait for password field to load
       .setValue('input[type="password"]', process.env.VUE_APP_AUTH0_PASSWORD) // input your password
-      .keys(browser.Keys.ENTER) // click login button
+      .keys(browser.Keys.ENTER) // click continue button
       .waitForElementVisible('h1.d-inline', 30000); // wait for header text 'Case Portal'
   });
+
+  this.tags = ['sign-in'];
 
   it('Test home page loaded after sign-in for users', (browser) => {
     browser
