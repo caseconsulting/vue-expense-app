048e707633139c5b5fcd65dddddedf5c05f2f42e
jhendricks <jhendricks@consultwithcase.com>
2022-06-20 13:58:27 -0400

    3769-pass-signInTests: pass tests

--- unitTests/signInTests.js
+++ unitTests/signInTests.js
@@ -17,23 +17,24 @@ describe('testing sign-in', () => {
 
   it('Test redirect to Oauth Signin and then app', (browser) => {
     browser
-      .waitForElementVisible('#custom-button-color', 40000) // wait for login button to load
+      .waitForElementVisible('#custom-button-color', 30000) // wait for login button to load
       .click('#custom-button-color') // click that button
       .assert.visible('img.auth0-lock-header-logo') // check that the case logo is there in auth0
       .assert.visible('div.auth0-lock-content') // check that the auth0 login button is visible
+      .pause(1000)
       .click('div.auth0-lock-social-button-text') // click the button
-      .waitForElementVisible('input#identifierId') // wait for email box to be visible
+      .waitForElementVisible('input#identifierId', 30000) // wait for email box to be visible
       .setValue('input#identifierId', process.env.VUE_APP_AUTH0_EMAIL) // input your email
       .keys(browser.Keys.ENTER) // click continue button
-      .waitForElementVisible('#password') // wait for password field to load
+      .waitForElementVisible('#password', 30000) // wait for password field to load
       .setValue('input[type="password"]', process.env.VUE_APP_AUTH0_PASSWORD) // input your password
       .keys(browser.Keys.ENTER) // click login button
-      .waitForElementVisible('h1.d-inline'); // wait for header text 'Case Portal'
+      .waitForElementVisible('h1.d-inline', 30000); // wait for header text 'Case Portal'
   });
 
   it('Test home page loaded after sign-in for users', (browser) => {
     browser
-      .waitForElementVisible('#home-greeting', 40000)
+      .waitForElementVisible('#home-greeting', 30000)
       .assert.urlEquals('http://localhost:8080/home')
       .assert.visible('#home-greeting')
       .assert.visible('#t-sheets-data')
