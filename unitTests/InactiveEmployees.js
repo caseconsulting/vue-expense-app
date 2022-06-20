9b1119348c3f14356d49fcd69d20b84cdff5e5fa
logburn <logburn@consultwithcase.com>
2022-06-20 16:16:40 -0400

    3766-inactiveEmployees-test: inactiveemployee passing

--- unitTests/InactiveEmployees.js
+++ unitTests/InactiveEmployees.js
@@ -1,39 +1,41 @@
 describe('testing sign-in', () => {
-  before((browser) => {
-    browser.url('http://localhost:8080');
-    browser.windowMaximize('current'); // maximize window
+  describe('testing sign-in', () => {
+    before((browser) => {
+      browser.url('http://localhost:8080');
+      browser.windowMaximize('current'); // maximize window
+      browser
+        .waitForElementVisible('#custom-button-color', 30000) // wait for login button to load
+        .pause(500)
+        .click('#custom-button-color') // click that button
+        .waitForElementVisible('img.auth0-lock-header-logo', 30000) // check that the case logo is there in auth0
+        .waitForElementVisible('div.auth0-lock-content', 30000) // check that the auth0 login button is visible
+        .click('div.auth0-lock-social-button-text') // click the button
+        .waitForElementVisible('input#identifierId', 30000) // wait for email box to be visible
+        .pause(500)
+        .setValue('input#identifierId', process.env.VUE_APP_AUTH0_EMAIL) // input your email
+        .keys(browser.Keys.ENTER) // click continue button
+        .waitForElementVisible('#password', 30000) // wait for password field to load
+        .pause(500)
+        .setValue('input[type="password"]', process.env.VUE_APP_AUTH0_PASSWORD) // input your password
+        .keys(browser.Keys.ENTER) // click continue button
+        .waitForElementVisible('h1.d-inline', 30000); // wait for header text 'Case Portal'
+    });
   });
 
   this.tags = ['inactive'];
 
-  it('Test redirect to Oauth Signin and then app', (browser) => {
-    browser
-      .waitForElementVisible('#custom-button-color') // wait for login button to load
-      .click('#custom-button-color') // click that button
-      .assert.visible('img.auth0-lock-header-logo') // check that the case logo is there in auth0
-      .assert.visible('div.auth0-lock-content') // check that the auth0 login button is visible
-      .click('div.auth0-lock-social-button-text') // click the button
-      .waitForElementVisible('input#identifierId') // wait for email box to be visible
-      .setValue('input#identifierId', process.env.VUE_APP_AUTH0_EMAIL) // input your email
-      .keys(browser.Keys.ENTER) // click continue button
-      .waitForElementVisible('#password') // wait for password field to load
-      .setValue('input[type="password"]', process.env.VUE_APP_AUTH0_PASSWORD) // input your password
-      .keys(browser.Keys.ENTER) // click continue button
-      .waitForElementVisible('h1.d-inline'); // wait for header text 'Case Portal'
-  });
-
   it('Test clicking the inactive button and verifying the employees are red text', (browser) => {
     browser
-      .waitForElementVisible('#users')
-      .click('#users')
-      .waitForElementVisible('#employeesSearch')
+      .waitForElementVisible('#mdi-account-group', 30000)
+      .click('#mdi-account-group')
+      .waitForElementVisible('#employeesSearch', 30000)
       .click('#employeesSearch')
-      .waitForElementVisible('#inactive')
+      .waitForElementVisible('#inactive', 30000)
       .click('#inactive') //selects inactive
-      .waitForElementVisible('#part')
+      .waitForElementVisible('#part', 30000)
       .click('#part') //deselects part time
-      .waitForElementVisible('#full')
+      .waitForElementVisible('#full', 30000)
       .click('#full') //deselects full time
-      .waitForElementVisible('.text-start p.inactiveStyle'); //checks if the class used for inactive employees is present
+      .waitForElementVisible('.text-start p.inactiveStyle', 30000); //checks if the class used for inactive employees is present
   });
 });
