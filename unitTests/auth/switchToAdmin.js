3ac92e4baa222d79af55be3dfe5a01998df7cbb2
jhendricks <jhendricks@consultwithcase.com>
2022-06-28 13:25:11 -0400

    POR-1775-add-switch-role-tests: modularize and adjust tests

--- unitTests/auth/switchToAdmin.js
+++ unitTests/auth/switchToAdmin.js
@@ -1,41 +1,27 @@
-describe('testing logout from home page', () => {
+const utils = require('../utils/utils.js');
+const vars = require('../utils/constants.js');
+
+describe('Login and confirm home greeting', () => {
   before((browser) => {
-    browser
-      .url('http://localhost:8080')
-      .windowMaximize('current') // maximize window
-      .waitForElementVisible('#custom-button-color', 30000) // wait for login button to load
-      .pause(500)
-      .click('#custom-button-color') // click that button
-      .waitForElementVisible('img.auth0-lock-header-logo') // check that the case logo is there in auth0
-      .waitForElementVisible('div.auth0-lock-content') // check that the auth0 login button is visible
-      .pause(500)
-      .click('div.auth0-lock-social-button-text') // click the button
-      .waitForElementVisible('input#identifierId', 30000) // wait for email box to be visible
-      .pause(500)
-      .setValue('input#identifierId', process.env.VUE_APP_AUTH0_EMAIL) // input your email
-      .keys(browser.Keys.ENTER) // click continue button
-      .waitForElementVisible('#password', 30000) // wait for password field to load
-      .pause(500)
-      .setValue('input[type="password"]', process.env.VUE_APP_AUTH0_PASSWORD) // input your password
-      .keys(browser.Keys.ENTER) // click continue button
-      .waitForElementVisible('h1.d-inline', 30000); // wait for header text 'Case Portal'
+    utils.commonBefore(browser);
+    browser.waitForElementVisible('#home-greeting', vars.WAIT_TIME); // look for Hello message
   });
+});
 
-  this.tags = ['switchToAdmin'];
+this.tags = ['switchToAdmin'];
 
-  it('Clicking switch to admin', (browser) => {
-    browser
-      .waitForElementVisible('#profile', 30000)
-      .click('#profile')
-      .waitForElementVisible('#switchRoleBtn', 30000)
-      .click('#switchRoleBtn')
-      .waitForElementVisible('.headline', 30000)
-      .execute('document.getElementById("adminSelector").click()')
-      .waitForElementVisible('#confirmSwitch', 30000)
-      .click('#confirmSwitch');
-  });
+it('Clicking switch to admin', (browser) => {
+  browser
+    .waitForElementVisible('#profile', vars.WAIT_TIME)
+    .click('#profile')
+    .waitForElementVisible('#switchRoleBtn', vars.WAIT_TIME)
+    .click('#switchRoleBtn')
+    .waitForElementVisible('.headline', vars.WAIT_TIME)
+    .execute('document.getElementById("adminSelector").click()')
+    .waitForElementVisible('#confirmSwitch', vars.WAIT_TIME)
+    .click('#confirmSwitch');
+});
 
-  it('Verify user home page', (browser) => {
-    browser.waitForElementVisible('#filterEmployee', 30000); // look for unreimbursed filters
-  });
+it('Verify user home page', (browser) => {
+  browser.waitForElementVisible('#filterEmployee', vars.WAIT_TIME); // look for unreimbursed filters
 });
