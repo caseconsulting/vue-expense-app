720b30c05ec3fcc18db7ee74f658930a2779967f
jhendricks <jhendricks@consultwithcase.com>
2022-07-06 09:32:49 -0400

    POR-1814-update-reports-dropdowns-based-on-employee-selection: refresh all fields and employee list when changing data types

--- src/components/EmployeeContractTable.vue
+++ src/components/EmployeeContractTable.vue
@@ -717,7 +717,15 @@ function watchDataType() {
         this.buildContractsColumn();
     }
   }
+
+  // resets the dropdowns and employees list
   this.dataTypeSearch = null;
+  this.search = null;
+  this.contract = null;
+  this.prime = null;
+  this.requestedDate = null;
+  this.filteredEmployees = this.employeesInfo;
+
   this.populateDataTypeDropDowns();
 } // watchDataType
 
