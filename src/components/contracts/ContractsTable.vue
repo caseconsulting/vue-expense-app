c5ee2a30e1451d68fe0fd6e286fcd8d6442135bc
despinoza17 <despi17@vt.edu>
2023-01-09 09:45:04 -0500

    1991: Added in edit rows in contracts data table

--- src/components/contracts/ContractsTable.vue
+++ src/components/contracts/ContractsTable.vue
@@ -10,6 +10,31 @@
           :headers="contractHeaders"
           :items="$store.getters.contracts"
         >
+          <template v-slot:[`item.contractName`]="{ item }">
+            <v-text-field v-if="editingItemID == item.id" v-model="item.contractName"></v-text-field>
+            <span v-else>{{ item.contractName }}</span>
+          </template>
+
+          <template v-slot:[`item.primeName`]="{ item }">
+            <v-text-field v-if="editingItemID == item.id" v-model="item.primeName"></v-text-field>
+            <span v-else>{{ item.primeName }}</span>
+          </template>
+
+          <template v-slot:[`item.costType`]="{ item }">
+            <v-text-field v-if="editingItemID == item.id" v-model="item.costType"></v-text-field>
+            <span v-else>{{ item.costType }}</span>
+          </template>
+
+          <template v-slot:[`item.popStartDate`]="{ item }">
+            <v-text-field v-if="editingItemID == item.id" v-model="item.popStartDate"></v-text-field>
+            <span v-else>{{ item.popStartDate }}</span>
+          </template>
+
+          <template v-slot:[`item.popEndDate`]="{ item }"
+            ><v-text-field v-if="editingItemID == item.id" v-model="item.popEndDate"></v-text-field>
+            <span v-else>{{ item.popEndDate }}</span>
+          </template>
+
           <!-- Expanded Row Slot -->
           <template v-slot:expanded-item="{ item }"
             ><td :colspan="contractHeaders.length" class="pa-0">
@@ -42,36 +67,80 @@
             </td>
           </template>
           <!-- Actions Slot -->
-          <template v-slot:[`item.actions`]="{}">
-            <!-- Add Project -->
-            <v-tooltip top>
-              <template v-slot:activator="{ on }">
-                <v-btn icon text v-on="on">
-                  <v-icon class="case-gray">mdi-file-document-plus</v-icon>
-                </v-btn>
-              </template>
-              <span>Add Project</span>
-            </v-tooltip>
+          <template v-slot:[`item.actions`]="{ item }">
+            <div v-if="editingItemID != item.id">
+              <!-- Add Project -->
+              <v-tooltip top>
+                <template v-slot:activator="{ on }">
+                  <v-btn :disabled="editingItemID != null" icon text v-on="on">
+                    <v-icon class="case-gray">mdi-file-document-plus</v-icon>
+                  </v-btn>
+                </template>
+                <span>Add Project</span>
+              </v-tooltip>
 
-            <!-- Edit Contract -->
-            <v-tooltip top>
-              <template v-slot:activator="{ on }">
-                <v-btn icon text v-on="on">
-                  <v-icon class="case-gray">edit</v-icon>
-                </v-btn>
-              </template>
-              <span>Edit</span>
-            </v-tooltip>
+              <!-- Edit Contract -->
+              <v-tooltip top>
+                <template v-slot:activator="{ on }">
+                  <v-btn
+                    icon
+                    text
+                    :disabled="editingItemID != null"
+                    v-on="on"
+                    @click="
+                      () => {
+                        editingItemID = item.id;
+                      }
+                    "
+                  >
+                    <v-icon class="case-gray">edit</v-icon>
+                  </v-btn>
+                </template>
+                <span>Edit</span>
+              </v-tooltip>
 
-            <!-- Delete Contract -->
-            <v-tooltip top>
-              <template v-slot:activator="{ on }">
-                <v-btn icon text v-on="on">
-                  <v-icon class="case-gray">delete</v-icon>
-                </v-btn>
-              </template>
-              <span>Delete</span>
-            </v-tooltip>
+              <!-- Delete Contract -->
+              <v-tooltip top>
+                <template v-slot:activator="{ on }">
+                  <v-btn :disabled="editingItemID != null" icon text v-on="on">
+                    <v-icon class="case-gray">delete</v-icon>
+                  </v-btn>
+                </template>
+                <span>Delete</span>
+              </v-tooltip>
+            </div>
+
+            <!-- IS EDITING ROW -->
+            <div v-else>
+              <!-- Save Contract -->
+              <v-tooltip top>
+                <template v-slot:activator="{ on }">
+                  <v-btn icon text v-on="on">
+                    <v-icon class="case-gray">save</v-icon>
+                  </v-btn>
+                </template>
+                <span>Save</span>
+              </v-tooltip>
+
+              <!-- Cancel Contract -->
+              <v-tooltip top>
+                <template v-slot:activator="{ on }">
+                  <v-btn
+                    icon
+                    text
+                    @click="
+                      () => {
+                        editingItemID = null;
+                      }
+                    "
+                    v-on="on"
+                  >
+                    <v-icon class="case-gray">cancel</v-icon>
+                  </v-btn>
+                </template>
+                <span>Cancel</span>
+              </v-tooltip>
+            </div>
           </template>
         </v-data-table>
       </v-container>
@@ -118,6 +187,12 @@ function clickedRow(contractObj) {
 // |--------------------------------------------------|
 
 export default {
+  beforeDestroy,
+  watch: {
+    editingItemID: function (val) {
+      console.log(val);
+    }
+  },
   created,
   methods: {
     clickedRow,
@@ -125,6 +200,7 @@ export default {
   },
   data() {
     return {
+      editingItemID: null,
       loading: false,
       expanded: [],
       projectHeaders: [
