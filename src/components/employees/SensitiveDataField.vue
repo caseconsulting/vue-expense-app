bf75f0dfcc1a39db77d0173fdba1d2ead95c74c4
despinoza17 <despi17@vt.edu>
2023-03-09 15:41:13 -0500

    POR-2054: Created sensitive data field component
    POR-2054: Modified sensitive data field component

---
+++ src/components/employees/SensitiveDataField.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="field-container">
+    <p @click="$emit('click')">
+      <b>{{ label }}:</b> {{ show ? value : '*****' }}
+    </p>
+    <a
+      class="show font-weight-bold text-caption"
+      @click="
+        () => {
+          show = !show;
+        }
+      "
+      >{{ show ? 'Hide' : 'Show' }}</a
+    >
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      show: false
+    };
+  },
+  props: ['label', 'value']
+};
+</script>
+<style scoped>
+.field-container {
+  position: relative;
+}
+
+.show {
+  position: absolute;
+  top: 16px;
+}
+</style>
