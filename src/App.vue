3fa9a43c4b4527d7efe5be5148e7c1db0fc1b1ed
Chad Martin <martincc@dukes.jmu.edu>
2023-08-03 12:42:21 -0400

    POR-2276: change version number and update basecamp notes link

--- src/App.vue
+++ src/App.vue
@@ -125,7 +125,7 @@
                 id="P"
                 class="black--text"
                 target="_blank"
-                href="https://3.basecamp.com/3097063/buckets/4708396/documents/6321360262"
+                href="https://3.basecamp.com/3097063/buckets/4708396/documents/6414892698"
                 ><strong>Version</strong> {{ version }}</a
               >
             </template>
