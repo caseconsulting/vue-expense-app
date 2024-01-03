84196d5f43ed3648b30d41bdbfb32d755024184b
logburn <21106956+logburn@users.noreply.github.com>
2024-01-03 14:28:40 -0500

    POR-2463: removed key refresh from PTO widget

--- src/views/Employee.vue
+++ src/views/Employee.vue
@@ -61,7 +61,7 @@
       <v-row class="pt-0">
         <!-- QuickBooks Time and Budgets-->
         <v-col v-if="displayQuickBooksTimeAndBalances" cols="12" md="5" lg="4" class="pt-0">
-          <quick-books-time-data :employee="model" :key="refreshKey" class="mb-4"></quick-books-time-data>
+          <quick-books-time-data :employee="model" class="mb-4"></quick-books-time-data>
           <available-budgets
             class="mb-4"
             v-if="model.id && !loading"
