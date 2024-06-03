3ec28b24b16284319ff1062200db96682b594bdb
Chad Martin <martincc@dukes.jmu.edu>
2024-06-03 13:03:23 -0400

    POR-2607: fix copy to clipboard button breaking on contact employees modal

--- src/components/shared/ContactEmployeesModal.vue
+++ src/components/shared/ContactEmployeesModal.vue
@@ -156,7 +156,7 @@ function customFilter(_, queryText, item) {
  * Copyies the list of employee emails to the user's clipboard.
  */
 async function copyEmailList() {
-  let list = this.getList();
+  let list = getList();
   await navigator.clipboard.writeText(list);
   copied.value = true;
 } // copyEmailList
@@ -165,7 +165,7 @@ async function copyEmailList() {
  * Generate the list of emails and separate by comma for the mail service.
  */
 function emailEmployees() {
-  let list = this.getList();
+  let list = getList();
   window.open(list, '_blank');
 } // emailEmployees
 
