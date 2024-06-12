c12eab4753b907520fe2519cf9ac44ce7e453e2a
Dylan Crotty <dcrotty@Dylans-MacBook-Air.local>
2024-06-12 16:13:11 -0400

    POR-2614-Tag-filtering-in-a-seperate-component-to-be-used-in-multiple-components: refactored ReportsCertifications.vue to use TagsFilter.vue

--- src/components/reports/ReportsCertifications.vue
+++ src/components/reports/ReportsCertifications.vue
@@ -33,34 +33,7 @@
             @click:clear="certificationSearch = null"
           ></v-autocomplete>
         </v-col>
-        <v-col v-if="userRoleIsAdmin() || userRoleIsManager()" cols="6" xl="3" lg="3" md="3" sm="6" class="my-0 py-0">
-          <v-autocomplete
-            clearable
-            label="Filter by Tag (click to flip)"
-            variant="underlined"
-            v-model="selectedTags"
-            :items="tags"
-            multiple
-            item-title="tagName"
-            item-value="id"
-            @update:model-value="refreshDropdownItems()"
-            return-object
-          >
-            <template v-slot:selection="{ item }">
-              <v-chip
-                size="small"
-                closable
-                @click.stop
-                @click="negateTag(item.raw)"
-                @click:close="removeTag(item.raw)"
-                :color="chipColor(item.raw.id)"
-              >
-                {{ tagFlip.includes(item.raw.id) ? 'NOT ' : '' }}
-                {{ item.raw.tagName }}
-              </v-chip>
-            </template>
-          </v-autocomplete>
-        </v-col>
+        <tags-filter v-model="tagsInfo" @update:modelValue="refreshDropdownItems()"></tags-filter>
         <v-col cols="6" xl="3" lg="3" md="3" sm="6" class="my-0 py-0">
           <v-checkbox v-model="showInactiveEmployees" label="Show Inactive Users"></v-checkbox>
         </v-col>
@@ -108,13 +81,15 @@
 
 <script setup>
 import _ from 'lodash';
-import { userRoleIsAdmin, userRoleIsManager } from '@/utils/utils';
 import { getActive, getFullName, populateEmployeesDropdown } from './reports-utils';
 import { getTodaysDate, isSameOrBefore } from '@/shared/dateUtils';
 import { employeeFilter } from '@/shared/filterUtils';
 import { onMounted, ref, inject, watch } from 'vue';
 import { useStore } from 'vuex';
 import { useRouter } from 'vue-router';
+import { selectedTagsHasEmployee } from '@/shared/employeeUtils';
+import TagsFilter from '@/components/shared/TagsFilter.vue';
+
 const store = useStore();
 const emitter = inject('emitter');
 const router = useRouter();
@@ -125,10 +100,12 @@ const filteredEmployees = ref([]);
 const certificationSearch = ref(null);
 const certifications = ref([]);
 const search = ref(null); // query text for datatable search field
-const selectedTags = ref([]);
 const showInactiveEmployees = ref(false);
-const tagFlip = ref([]);
-const tags = ref([]);
+const tagsInfo = ref({
+  selected: [],
+  flipped: []
+});
+
 // datatable headers
 const headers = ref([
   {
@@ -160,7 +137,6 @@ const headers = ref([
  */
 onMounted(() => {
   employeesInfo.value = getActive(store.getters.employees); // default to filtered
-  tags.value = store.getters.tags;
   filteredEmployees.value = employeesInfo.value; // one.value is shown
   populateDropdowns(employeesInfo.value);
   buildCertificationsColumns();
@@ -204,16 +180,6 @@ function buildCertificationsColumns() {
 } // buildCertificationsColumns
 
 /**
- * Returns the color that at tag filter chip should be
- *
- * @param id ID of the tag item
- *
- */
-function chipColor(id) {
-  return tagFlip.value.includes(id) ? 'red' : 'gray';
-} // chipColor
-
-/**
  * handles click event of the employee table entry
  *
  * @param item - the employee
@@ -221,19 +187,6 @@ function chipColor(id) {
 function handleClick(_, { item }) {
   router.push(`/employee/${item.employeeNumber}`);
 } //handleClick
-
-/**
- * negates a tag
- */
-function negateTag(item) {
-  // try to find the id in the tagFlip array, if it is there then remove it else add it
-  const index = tagFlip.value.indexOf(item.id);
-  if (index >= 0) {
-    tagFlip.value.splice(index, 1);
-  } else {
-    tagFlip.value.push(item.id);
-  }
-} // negateTag
 
 /**
  * Populates all certifications in the search dropdown.
@@ -279,26 +232,14 @@ function refreshDropdownItems() {
   if (certificationSearch.value) {
     searchCertifications();
   }
-  if (selectedTags.value.length > 0) {
+  if (tagsInfo.value.selected.length > 0) {
     filteredEmployees.value = _.filter(filteredEmployees.value, (employee) => {
-      return selectedTagsHasEmployee(employee);
+      return selectedTagsHasEmployee(employee.id, tagsInfo);
     });
   }
 
   populateDropdowns(filteredEmployees.value);
 } // refreshDropdownItems
-
-/**
- * Removes an item from the tag filters's active filters
- *
- * @param item - The filter to remove
- */
-function removeTag(item) {
-  const selIndex = selectedTags.value.findIndex((t) => t.id === item.id);
-  if (selIndex >= 0) {
-    selectedTags.value.splice(selIndex, 1);
-  }
-} // remove
 
 /**
  * Filters employees on the data table by the certification entered by the user.
@@ -312,24 +253,6 @@ function searchCertifications() {
     }
   });
 } // searchCertifications
-
-/**
- * helper function: return true if any selected tag has employee listed under it.
- *
- * @param e - the employee
- * @return true if the employee has a tag selected in filters
- */
-function selectedTagsHasEmployee(e) {
-  let inTag, tagFlipped;
-  for (let i = 0; i < selectedTags.value.length; i++) {
-    inTag = selectedTags.value[i].employees.includes(e.id);
-    tagFlipped = tagFlip.value.includes(selectedTags.value[i].id);
-    if (inTag != tagFlipped) {
-      return true;
-    }
-  }
-  return false;
-} // selectedTagsHasEmployee
 
 /**
  * Emit new data for tab.value
@@ -354,34 +277,6 @@ watch(showInactiveEmployees, () => {
   employeesInfo.value = store.getters.employees;
   if (!showInactiveEmployees.value) employeesInfo.value = getActive(employeesInfo.value);
   populateDropdowns(employeesInfo.value);
-  refreshDropdownItems();
-});
-
-/**
- * In the case that the page has been force reloaded (and the store cleared)
- * watcher.value will be activated when the store is populated again.
- */
-watch(tagFlip, () => {
-  refreshDropdownItems();
-});
-
-/**
- * Remove items from tagFlip array when they are removed from the selected
- * tags
- */
-watch(selectedTags, () => {
-  let negatedTagRemoved = true;
-  // use normal for loop to have the index
-  for (let i = 0; i < tagFlip.value.length; i++) {
-    // try to find the current tag in the selectedTags
-    _.forEach(selectedTags.value, (t) => {
-      if (t.id === tagFlip.value[i]) negatedTagRemoved = false;
-    });
-    // if it isn't there, remove it from tagFlip too
-    if (negatedTagRemoved) {
-      tagFlip.value.splice(i, 1);
-    }
-  }
   refreshDropdownItems();
 });
 </script>
