6c57cb1e849ec5dd5c434c9671c7ac00317d854e
Dylan Crotty <dcrotty@consultwithcase.com>
2024-06-20 17:29:37 -0400

    POR-2641-migrate-/shared-folder-to-composition-api: migrated UserAvatar.vue to compositions

--- src/components/shared/UserAvatar.vue
+++ src/components/shared/UserAvatar.vue
@@ -5,52 +5,61 @@
   </v-avatar>
 </template>
 
-<script>
+<script setup>
+import { computed } from 'vue';
+
+// |--------------------------------------------------|
+// |                                                  |
+// |                       SETUP                      |
+// |                                                  |
+// |--------------------------------------------------|
+
+const props = defineProps({
+  employee: {
+    type: Object,
+    required: true
+  },
+  image: {
+    type: String,
+    default: ''
+  },
+  color: {
+    type: String,
+    default: undefined
+  },
+  size: {
+    type: [String, Number],
+    default: '40'
+  },
+  alt: {
+    type: String,
+    default: undefined
+  }
+});
+
+// |--------------------------------------------------|
+// |                                                  |
+// |                     COMPUTED                     |
+// |                                                  |
+// |--------------------------------------------------|
+
 /**
  * Returns the employee name
  */
-function empName() {
-  if (!this.employee) return 'a CASE employee';
-  return `${this.employee.nickname || this.employee.firstName} ${this.employee.lastName}`;
-} // empName
+
+const empName = computed(() => {
+  if (!props.employee) return 'a CASE employee';
+  return `${props.employee.nickname || props.employee.firstName} ${props.employee.lastName}`;
+}); // empName
 
 /**
  * Returns just the initials employee
  */
-function initials() {
-  if (!this.employee) return '';
+const initials = computed(() => {
+  if (!props.employee) return '';
   return (
-    (this.employee.nickname || this.employee.firstName).charAt(0).toUpperCase() +
-    this.employee.lastName.charAt(0).toUpperCase()
+    (props.employee.nickname || props.employee.firstName).charAt(0).toUpperCase() +
+    props.employee.lastName.charAt(0).toUpperCase()
   );
-} // initials
-
-export default {
-  computed: {
-    empName,
-    initials
-  },
-  props: {
-    employee: {
-      type: Object,
-      required: true
-    },
-    image: {
-      type: String,
-      default: ''
-    },
-    color: {
-      type: String,
-      default: undefined
-    },
-    size: {
-      type: [String, Number],
-      default: '40'
-    },
-    alt: {
-      type: String,
-      default: undefined
-    }
-  }
-};
+}); // initials
 </script>
