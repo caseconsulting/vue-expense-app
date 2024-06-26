99b4d9328591edcceb7c42c9f07c66b9daf213c6
Luke Ogburn <21106956+logburn@users.noreply.github.com>
2024-06-26 11:21:16 -0400

    POR-2656: only use PTO as nonbillable in timesheet export (#229)
    * saving
    
    * POR-2656: remove testing code
    
    * POR-2656: flip CYK tag
    
    * POR-2656: exclude only PTO for all employees
    
    * POR-2656: fix updatePeriodDefault bug

--- src/components/employees/csv/ExportEmployeeData.vue
+++ src/components/employees/csv/ExportEmployeeData.vue
@@ -322,13 +322,7 @@ function updatePeriodDefault() {
 /**
  * exportType watcher
  */
-watch(
-  exportType,
-  () => {
-    updatePeriodDefault;
-  },
-  { deep: true }
-);
+watch(exportType, updatePeriodDefault, { deep: true });
 </script>
 
 <style scoped>
