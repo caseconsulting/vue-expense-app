d4fd38378f3f564611cffc9bfc2f003730a35189
madelinegburns <mburns@consultwithcase.com>
2024-06-27 12:11:39 -0400

    POR-2684:Added the Education Modal (#238)
    Co-authored-by: Madeline Burns <madelineburns@Madelines-Laptop.local>

--- src/components/employee-beta/EducationInfoCard.vue
+++ src/components/employee-beta/EducationInfoCard.vue
@@ -1,84 +1,93 @@
 <template>
-  <v-card>
-    <v-card-title class="d-flex align-center justify-space-between beta_header_style">
-      <h3>Education</h3>
-      <v-btn v-if="isAdmin || isUser" density="comfortable" variant="text" icon="" class="mx-1">
-        <v-tooltip activator="parent" location="top"> Edit Education </v-tooltip>
-        <v-icon id="edit" color="white"> mdi-pencil </v-icon>
-      </v-btn>
-    </v-card-title>
-    <v-card-text v-if="isEmpty(filteredList)">
-      <p>No Education to display</p>
-    </v-card-text>
-    <v-card-text v-else>
-      <v-list-item v-for="(edu, index) in filteredList" :key="edu + index">
-        <!-- START UNIVERSITY SECTION -->
-        <div class="ml-4" v-if="edu.type === 'university'">
-          <v-list-item-title class="d-flex align-center">
-            <v-icon class="mx-3">mdi-school-outline</v-icon>
-            <p class="mt-3">
-              <b>{{ edu.name }}</b>
-            </p>
-            <p
-              class="ma-2 px-6"
-              style="color: #828282"
-              v-if="edu.degrees.length === 1 && edu.degrees[0].completionDate"
+  <div class="eduTab">
+    <v-card>
+      <v-card-title class="d-flex align-center justify-space-between beta_header_style">
+        <h3>Education</h3>
+        <v-btn v-if="isAdmin || isUser" density="comfortable" variant="text" icon="" class="mx-1">
+          <v-tooltip activator="parent" location="top"> Edit Education </v-tooltip>
+          <v-icon id="edit" color="white"> mdi-pencil </v-icon>
+        </v-btn>
+      </v-card-title>
+      <v-card-text v-if="isEmpty(showableList)">
+        <p>No Education to display</p>
+      </v-card-text>
+      <v-card-text v-else>
+        <v-list-item v-for="(edu, index) in showableList" :key="edu + index">
+          <!-- START UNIVERSITY SECTION -->
+          <div class="ml-4" v-if="edu.type === 'university'">
+            <v-list-item-title class="d-flex align-center">
+              <v-icon class="mx-3">mdi-school-outline</v-icon>
+              <p class="mt-3">
+                <b>{{ edu.name }}</b>
+              </p>
+              <p
+                class="ma-2 px-6"
+                style="color: #828282"
+                v-if="edu.degrees.length === 1 && edu.degrees[0].completionDate"
+              >
+                {{ monthYearFormatBETA(edu.degrees[0].completionDate) }}
+              </p>
+            </v-list-item-title>
+            <div
+              class="ml-12"
+              style="color: #828282; font-size: 15px"
+              v-for="(degree, index) in edu.degrees"
+              :key="degree + index"
             >
-              {{ monthYearFormatBETA(edu.degrees[0].completionDate) }}
-            </p>
-          </v-list-item-title>
-          <div
-            class="ml-12"
-            style="color: #828282; font-size: 15px"
-            v-for="(degree, index) in edu.degrees"
-            :key="degree + index"
-          >
-            <p><b>Degree: </b>{{ degree.degreeType }}</p>
-            <!-- <v-divider v-if="index < filteredList.length - 1" class="mb-3"></v-divider> -->
-            <div v-for="(major, majorIndex) in degree.majors" :key="major + majorIndex">
-              <p><b>Major(s): </b> {{ major }}</p>
-              <p v-if="degree.minors.length > 1"><b>Minor(s): </b>{{ getMinors(degree) }}</p>
-              <p v-else-if="degree.minors.length === 1"><b>Minor(s): </b>{{ degree.minors[0] }}</p>
+              <p><b>Degree: </b>{{ degree.degreeType }}</p>
+              <div v-for="(major, majorIndex) in degree.majors" :key="major + majorIndex">
+                <p><b>Major(s): </b> {{ major }}</p>
+                <p v-if="degree.minors.length > 1"><b>Minor(s): </b>{{ getMinors(degree) }}</p>
+                <p v-else-if="degree.minors.length === 1"><b>Minor(s): </b>{{ degree.minors[0] }}</p>
+              </div>
             </div>
           </div>
-        </div>
-        <!-- END UNIVERSITY SECTION -->
+          <!-- END UNIVERSITY SECTION -->
 
-        <!-- START MILITARY SECTION -->
-        <div class="ml-4" v-if="edu.type === 'military'">
-          <v-list-item-title class="d-flex align-center">
-            <v-icon class="mx-3">mdi-school-outline</v-icon>
-            <p class="mt-3">
-              <b>{{ edu.branch }} (Military Service)</b>
-            </p>
-            <p class="ma-2 px-6" style="color: #828282" v-if="edu.completeDate">
-              {{ monthYearFormatBETA(edu.startDate) }} - {{ monthYearFormatBETA(edu.completeDate) }}
-            </p>
-            <p v-else>{{ monthYearFormatBETA(edu.startDate) }} - Present</p>
-          </v-list-item-title>
-        </div>
-        <!-- END MILITARY SECTION -->
+          <!-- START MILITARY SECTION -->
+          <div class="ml-4" v-if="edu.type === 'military'">
+            <v-list-item-title class="d-flex align-center">
+              <v-icon class="mx-3">mdi-school-outline</v-icon>
+              <p class="mt-3">
+                <b>{{ edu.branch }} (Military Service)</b>
+              </p>
+              <p class="ma-2 px-6" style="color: #828282" v-if="edu.completeDate">
+                {{ monthYearFormatBETA(edu.startDate) }} - {{ monthYearFormatBETA(edu.completeDate) }}
+              </p>
+              <p v-else>{{ monthYearFormatBETA(edu.startDate) }} - Present</p>
+            </v-list-item-title>
+          </div>
+          <!-- END MILITARY SECTION -->
 
-        <div class="ml-4" v-if="edu.type === 'highSchool'">
-          <v-list-item-title class="d-flex align-center">
-            <v-icon class="mx-3">mdi-school-outline</v-icon>
-            <p class="mt-3">
-              <b>{{ edu.name }}</b>
-            </p>
-            <p class="ma-2 px-6" style="color: #828282" v-if="edu.gradDate">
-              {{ monthYearFormatBETA(edu.gradDate) }}
-            </p>
-          </v-list-item-title>
+          <!-- START HIGHSCHOOL SECTION -->
+          <div class="ml-4" v-if="edu.type === 'highSchool'">
+            <v-list-item-title class="d-flex align-center">
+              <v-icon class="mx-3">mdi-school-outline</v-icon>
+              <p class="mt-3">
+                <b>{{ edu.name }}</b>
+              </p>
+              <p class="ma-2 px-6" style="color: #828282" v-if="edu.gradDate">
+                {{ monthYearFormatBETA(edu.gradDate) }}
+              </p>
+            </v-list-item-title>
+          </div>
+          <!-- END HIGHSCHOOL SECTION -->
+        </v-list-item>
+        <div v-if="!isEmpty(showableList) && Math.ceil(totalList.length / 2) != 1" class="text-center">
+          <v-card-actions>
+            <v-btn block @click="dialog = true">Click to see more</v-btn>
+          </v-card-actions>
         </div>
-      </v-list-item>
-    </v-card-text>
-  </v-card>
+      </v-card-text>
+      <education-modal :model="model" :dialog="dialog"></education-modal>
+    </v-card>
+  </div>
 </template>
 
 <script setup>
 import { onBeforeMount, ref } from 'vue';
-import { isEmpty } from '@/utils/utils';
-import { monthYearFormatBETA } from '../../utils/utils';
+import { monthYearFormatBETA, isEmpty } from '../../utils/utils';
+import EducationModal from './modals/EducationModal.vue';
 
 // |--------------------------------------------------|
 // |                                                  |
@@ -86,7 +95,9 @@ import { monthYearFormatBETA } from '../../utils/utils';
 // |                                                  |
 // |--------------------------------------------------|
 
-const filteredList = ref([]);
+const dialog = ref(false);
+const showableList = ref([]);
+const totalList = ref([]);
 const props = defineProps(['model', 'isAdmin', 'isUser']);
 
 // |--------------------------------------------------|
@@ -100,10 +111,9 @@ const props = defineProps(['model', 'isAdmin', 'isUser']);
  */
 onBeforeMount(() => {
   if (!isEmpty(props.model.education)) {
-    filteredList.value = props.model.education.slice(0, 4);
+    showableList.value = props.model.education.slice(0, 2); // only shows 2 educations at a time
+    totalList.value = props.model.education; // gets all education
   }
-
-  console.log(filteredList.value);
 }); // created
 
 // |--------------------------------------------------|
