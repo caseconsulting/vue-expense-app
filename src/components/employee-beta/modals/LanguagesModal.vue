e1a78532c29b2704abb413bfa187133ce48b1c0f
dcrotty1 <dcrotty@consultwithcase.com>
2024-06-27 14:31:29 -0400

    Dcrotty por 2576 beta profile por 2692 technology skills language modal (#248)
    * add tech and skills modal
    
    * add languages modal
    
    * fixed Click to See More button on languages card always being visible
    
    * created technologiesModal.vue
    
    * added technologiesModal filters
    
    * fixed filtering bug
    
    ---------
    
    Co-authored-by: Sammy Fischer <sfischer@consultwithcase.com>

---
+++ src/components/employee-beta/modals/LanguagesModal.vue
@@ -0,0 +1,79 @@
+<template>
+  <base-info-modal title="Foreign Languages">
+    <v-card-text>
+      <div v-if="!isEmpty(model.languages)">
+        <!-- Loop Languages -->
+        <div v-for="(languages, index) in visibleItems" :key="languages.name + index">
+          <v-row>
+            <v-col>
+              <p><b>Language: </b>{{ languages.name }}</p>
+            </v-col>
+          </v-row>
+          <p class="pl-10"><b>Level of Fluency: </b>{{ languages.proficiency }}</p>
+          <hr v-if="index < filteredList.length - 1" class="mb-3" />
+        </div>
+        <!-- End Loop Languages -->
+      </div>
+      <!-- Employee does not have Language Experience -->
+      <p v-else>No Foreign Language Information</p>
+      <!-- Pagination -->
+      <div v-if="pages != 1" class="text-center">
+        <v-pagination v-model="currentPage" :length="pages" total-visible="8"></v-pagination>
+      </div>
+    </v-card-text>
+  </base-info-modal>
+</template>
+
+<script setup>
+import { computed, ref, watch } from 'vue';
+import { isEmpty, sortLanguagesByProficiency } from '@/utils/utils';
+import BaseInfoModal from './BaseInfoModal.vue';
+
+// |--------------------------------------------------|
+// |                                                  |
+// |                      SETUP                       |
+// |                                                  |
+// |--------------------------------------------------|
+
+const ITEMS_PER_PAGE = 8;
+
+const props = defineProps(['model']);
+
+const currentPage = ref(1);
+const filteredList = ref(sortLanguagesByProficiency(props.model.languages));
+
+// |--------------------------------------------------|
+// |                                                  |
+// |                     COMPUTED                     |
+// |                                                  |
+// |--------------------------------------------------|
+
+/**
+ * Items visible on the current page
+ */
+const visibleItems = computed(() => {
+  const startIndex = (currentPage.value - 1) * ITEMS_PER_PAGE;
+  const endIndex = startIndex + ITEMS_PER_PAGE;
+  return filteredList.value.slice(startIndex, endIndex);
+});
+
+/**
+ * The number of pages
+ */
+const pages = computed(() => Math.ceil(filteredList.value.length / ITEMS_PER_PAGE));
+
+// |--------------------------------------------------|
+// |                                                  |
+// |                     WATCHERS                     |
+// |                                                  |
+// |--------------------------------------------------|
+
+// updates the filtered list only when languages change
+watch(
+  () => props.model.languages,
+  () => {
+    filteredList.value = sortLanguagesByProficiency(props.model.languages);
+  },
+  { deep: true }
+);
+</script>
---
+++ src/components/employee-beta/modals/TechnologiesModal.vue
@@ -0,0 +1,128 @@
+<template>
+  <base-info-modal title="Technologies and Skills">
+    <v-card-text>
+      <!-- Employee has Technology Experience -->
+      <div v-if="!isEmpty(model.technologies)">
+        <!--Tech Filters -->
+        <div class="mb-3">
+          <fieldset class="filter_border">
+            <legend class="legend_style">Sort By</legend>
+            <v-col cols="12">
+              <v-btn-toggle color="primary" v-model="filters" density="compact" multiple>
+                <v-btn value="filter1current">
+                  <v-tooltip activator="parent" location="top">Current</v-tooltip>
+                  <v-icon size="x-large">mdi-check{{ filters.includes('filter1current') ? '-bold' : '' }}</v-icon>
+                </v-btn>
+
+                <v-btn value="filter2years">
+                  <v-tooltip activator="parent" location="top">Years of Experience</v-tooltip>
+                  <v-icon size="x-large"
+                    >mdi-calendar-multiple{{ filters.includes('filter2years') ? '-check' : '' }}</v-icon
+                  >
+                </v-btn>
+
+                <v-btn value="filter3name">
+                  <v-tooltip activator="parent" location="top">Alphabetical</v-tooltip>
+                  <v-icon size="x-large">mdi-sort-alphabetical-descending-variant</v-icon>
+                </v-btn>
+              </v-btn-toggle>
+            </v-col>
+          </fieldset>
+        </div>
+        <!-- End of Sort Filters -->
+        <v-list>
+          <!-- Loop Technologies -->
+          <v-list-item v-for="(technology, index) in filteredList" :key="technology.name + index">
+            <v-list-item-title class="d-flex align-center pb-4">
+              <div class="mx-3">
+                <span v-if="technology.current">
+                  <v-icon>mdi-check</v-icon>
+                  <v-tooltip activator="parent" location="left">Current Skill</v-tooltip>
+                </span>
+              </div>
+              <p class="ma-3">
+                <b>{{ technology.name }}</b>
+              </p>
+            </v-list-item-title>
+            <v-row class="pl-10">
+              <p><b>Experience: </b>{{ Number(technology.years).toFixed(1) }} years</p>
+            </v-row>
+            <hr v-if="index < filteredList.length - 1" class="my-3" />
+          </v-list-item>
+          <!-- End Loop Technologies -->
+        </v-list>
+        <div
+          v-if="!isEmpty(model.technologies) && Math.ceil(model.technologies.length / ITEMS_PER_PAGE) != 1"
+          class="text-center"
+        >
+          <v-pagination
+            v-model="page"
+            :length="Math.ceil(model.technologies.length / ITEMS_PER_PAGE)"
+            :total-visible="8"
+          ></v-pagination>
+        </div>
+      </div>
+      <!-- Employee does not have Technology Experience -->
+      <p v-else>No Technologies or Skills Information</p>
+    </v-card-text>
+  </base-info-modal>
+</template>
+
+<script setup>
+import _ from 'lodash';
+import { ref, computed } from 'vue';
+import { isEmpty } from '@/utils/utils';
+import BaseInfoModal from '@/components/employee-beta/modals/BaseInfoModal.vue';
+
+// |--------------------------------------------------|
+// |                                                  |
+// |                      SETUP                       |
+// |                                                  |
+// |--------------------------------------------------|
+
+const ITEMS_PER_PAGE = 8;
+const iteratees = {
+  filter1current: (obj) => !obj.current,
+  filter2years: (obj) => -obj.years,
+  filter3name: (obj) => obj.name
+};
+
+const props = defineProps(['model']);
+const page = ref(1);
+const filters = ref(['filter1current', 'filter2years']);
+
+// |--------------------------------------------------|
+// |                                                  |
+// |                     COMPUTED                     |
+// |                                                  |
+// |--------------------------------------------------|
+
+const filteredList = computed(() => {
+  const startIndex = (page.value - 1) * ITEMS_PER_PAGE; //each page contains 5 technologies entries
+  const endIndex = startIndex + ITEMS_PER_PAGE;
+  if (!isEmpty(props.model.technologies)) {
+    return sortByFilters(props.model.technologies, filters.value).slice(startIndex, endIndex);
+  }
+  return [];
+});
+
+// |--------------------------------------------------|
+// |                                                  |
+// |                     METHODS                      |
+// |                                                  |
+// |--------------------------------------------------|
+
+/**
+ * Helper function to sort technologies by filters
+ * @param {Array} technologies - List of technologies
+ * @param {Array} filters - List of applied filters
+ * @return filteredList - A list of technologies sorted by filters
+ */
+function sortByFilters(technologies, filters) {
+  const sorted = _.sortBy(
+    technologies,
+    filters.sort().map((filter) => iteratees[filter]) //sort filters so order by current, years, name
+  );
+  return sorted;
+}
+</script>
