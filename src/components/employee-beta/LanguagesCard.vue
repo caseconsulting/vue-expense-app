03ebce1168de0e2340b3af0253b78fae651825d1
Madeline Burns <madelineburns@Madelines-Laptop.local>
2024-06-27 15:06:22 -0400

    POR-2732 - Add beta header styling to all cards

--- src/components/employee-beta/LanguagesCard.vue
+++ src/components/employee-beta/LanguagesCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="infoTab">
     <v-card>
-      <v-card-title class="d-flex align-center justify-space-between header_style">
+      <v-card-title class="d-flex align-center justify-space-between beta_header_style">
         <h3 class="text-white px-2">Foreign Languages</h3>
         <v-btn v-if="isAdmin || isUser" density="comfortable" variant="text" icon="">
           <v-tooltip activator="parent" location="top"> Edit Profile </v-tooltip>
