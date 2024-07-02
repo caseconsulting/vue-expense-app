4afe980c1bfb52a796f633d5b4d33e2bb868b22c
Sammy Fischer <sfischer@consultwithcase.com>
2024-07-02 12:29:00 -0400

    removed duplicate code in awards components

--- src/components/employee-beta/lists/AwardsList.vue
+++ src/components/employee-beta/lists/AwardsList.vue
@@ -1,19 +1,14 @@
 <template>
   <v-list>
-    <v-list-item v-for="(certification, index) in list" :key="certification.name + index">
+    <v-list-item v-for="(awards, index) in list" :key="awards.name + index">
       <v-list-item-title class="d-flex align-center pb-4">
-        <v-icon class="mx-3">mdi-star-check</v-icon>
+        <v-icon class="mx-3">mdi-trophy-award</v-icon>
         <p class="mt-3">
-          <b>{{ certification.name }}</b>
+          <b>{{ awards.name }}</b>
         </p>
       </v-list-item-title>
-      <v-row class="ml-12 gray-text">
-        <p><b>Date Received: </b>{{ monthDayYearFormat(certification.dateReceived) }}</p>
-      </v-row>
       <v-row class="ml-12 mb-1 gray-text">
-        <p v-if="certification.expirationDate">
-          <b>Expiration Date: </b>{{ monthDayYearFormat(certification.expirationDate) }}
-        </p>
+        <p><b>Date Received: </b>{{ monthYearFormat(awards.dateReceived) }}</p>
       </v-row>
       <v-row no-gutters class="mx-5">
         <v-divider v-if="index < list.length - 1" />
@@ -23,11 +18,11 @@
 </template>
 
 <script setup>
-import { monthDayYearFormat } from '@/utils/utils';
+import { monthYearFormat } from '@/utils/utils';
 
 // |--------------------------------------------------|
 // |                                                  |
-// |                       SETUP                      |
+// |                      SETUP                       |
 // |                                                  |
 // |--------------------------------------------------|
 
