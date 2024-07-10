8086f8962c6f216b2f5f7738a305f95d8753e263
Sammy Fischer <sfischer@consultwithcase.com>
2024-07-10 14:44:54 -0400

    POR-2752-session-timeout-breaks-portal: reworked auto logout functionality

--- src/main.js
+++ src/main.js
@@ -11,7 +11,8 @@ import UserAvatar from '@/components/shared/UserAvatar';
 
 import App from './App.vue';
 
-const emitter = mitt();
+export const emitter = mitt();
+
 const app = createApp(App).use(router).use(store).use(vuetify);
 app.component('UserAvatar', UserAvatar);
 app.provide('emitter', emitter);
