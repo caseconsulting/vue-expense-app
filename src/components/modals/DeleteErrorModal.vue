b16d308ab52cec68f3e09c598dd7c90767fe2392
Dylan Crotty <dcrotty@consultwithcase.com>
2024-07-11 14:59:16 -0400

    POR-2791-invalid-delete-prevents-further-actions: fixed bug and reformatted error modeling

--- src/components/modals/DeleteErrorModal.vue
+++ src/components/modals/DeleteErrorModal.vue
@@ -1,21 +1,12 @@
 <template>
   <div>
-    <v-dialog v-model="activate" persistent max-width="350">
+    <v-dialog v-model="activate" @click:outside="closeModal()" max-width="350">
       <v-card>
         <v-card-text class="font-weight-medium text-h6">Error: cannot delete {{ type }}</v-card-text>
         <v-card-text>Cannot delete {{ type }}. Expenses for this {{ type }} exist.</v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn
-            color="gray darken-1"
-            variant="text"
-            @click="
-              emit(`invalid-${type}-delete`);
-              activate = false;
-            "
-          >
-            Ok
-          </v-btn>
+          <v-btn color="gray darken-1" variant="text" @click="closeModal()"> Ok </v-btn>
           <v-spacer></v-spacer>
         </v-card-actions>
       </v-card>
@@ -24,7 +15,7 @@
 </template>
 
 <script setup>
-import { inject, onBeforeUnmount, ref, watch } from 'vue';
+import { inject } from 'vue';
 
 // |--------------------------------------------------|
 // |                                                  |
@@ -32,49 +23,11 @@ import { inject, onBeforeUnmount, ref, watch } from 'vue';
 // |                                                  |
 // |--------------------------------------------------|
 
+const activate = defineModel();
 const props = defineProps([
-  'toggleDeleteErrorModal', // dialog activator
   'type' // type of object being deleted
 ]);
 const emitter = inject('emitter');
-
-const activate = ref(false); // dialog activator
-
-// |--------------------------------------------------|
-// |                                                  |
-// |                 LIFECYCLE HOOKS                  |
-// |                                                  |
-// |--------------------------------------------------|
-
-// The watcher above and this emitter kind of do the same thing, but the watcher didn't update when it should've in
-// some cases. To fix this, I used this event instead (fired in ExpenseTypes.vue), but I didn't delete the watcher to
-// avoid breaking anything else.
-// this and the watcher that sets `activate` to true do the same thing, except the watcher broke under certain cases.
-// to avoid more issues, I've left it there
-emitter.on('delete-expense-type-error-show', () => {
-  activate.value = true;
-});
-
-/**
- * Before Unmount lifecycle hook - removes event listeners
- */
-onBeforeUnmount(() => {
-  emitter.off('delete-expense-type-error-show');
-});
-
-// |--------------------------------------------------|
-// |                                                  |
-// |                    WATCHERS                      |
-// |                                                  |
-// |--------------------------------------------------|
-
-// Watcher for toggleDeleteErrorModal
-watch(
-  () => props.toggleDeleteErrorModal,
-  () => {
-    activate.value = props.toggleDeleteErrorModal;
-  }
-);
 
 // |--------------------------------------------------|
 // |                                                  |
@@ -83,11 +36,10 @@ watch(
 // |--------------------------------------------------|
 
 /**
- * Emits a message.
- *
- * @param msg - Message to emit
+ * Closes modal and emits invalid delete event
  */
-function emit(msg) {
-  emitter.emit(msg);
-} // emit
+function closeModal() {
+  emitter.emit(`invalid-${props.type}-delete`);
+  activate.value = false;
+}
 </script>
