a6d4e079e76c2f75296e0b9af1fded26b6a7408f
sammyfischer <sfischer@consultwithcase.com>
2024-07-11 15:57:44 -0400

    POR 2696 - clearance modal (#305)
    * created clearance modal component
    
    * POR 2681 - create resume component (#279)
    
    * added non-functional resume buttons to the employee page
    
    * added resume upload, download, and delete buttons to employee profile
    
    * made resume card render with props from the edit model
    
    * Dcrotty por 2576 beta profile por 2687 tab navigation for different sections (#288)
    
    * created tabs for each card group
    
    * formatting the tabs
    
    * added employee profile header
    
    * added employee profile header !
    
    * POR 2737 - better sorting for certifications and awards (#289)
    
    * added sortedByDateReceieved function and implemented into certifications card and modal
    
    * added sorting to awards
    
    * fixed import errors
    
    * POR 2688 - added quickbook timecard data to employee profile (#292)
    
    * added quickbooks time data card
    
    * refactored EmployeeInfoCard.vue and formatted emmployee page
    
    * fixed styling on header and hireDate values bug
    
    * 😫
    
    * Dcrotty por 2576 beta profile por 2689 available budgets (#293)
    
    * added available budgets to the employee profile
    
    * added budgets chart to employee budgets
    
    * fixed comment
    
    * make all dates show
    
    * added search employee functionality for admins (#307)
    
    ---------
    
    Co-authored-by: dcrotty1 <dcrotty@consultwithcase.com>
    Co-authored-by: madelinegburns <mburns@consultwithcase.com>
    Co-authored-by: Madeline Burns <madelineburns@Madelines-Laptop.local>

---
+++ src/components/employee-beta/lists/ClearanceList.vue
@@ -0,0 +1,172 @@
+<template>
+  <v-list>
+    <v-list-item v-for="(clearance, index) in list" :key="clearance + index">
+      <v-row no-gutters>
+        <v-card-text class="pb-0">
+          <p v-if="!isEmpty(getClearanceType(clearance))" class="clearance-type-text">
+            <b>{{ getClearanceType(clearance) }}</b>
+          </p>
+          <div class="gray-text ml-6">
+            <p v-if="!isEmpty(getSubmissionDate(clearance))"><b>Submission: </b>{{ getSubmissionDate(clearance) }}</p>
+            <p v-if="!isEmpty(getGrantedDate(clearance))"><b>Granted: </b>{{ getGrantedDate(clearance) }}</p>
+          </div>
+          <p v-if="!isEmpty(getBadgeNumber(clearance))" class="gray-text ml-6">
+            <b>Badge Number: </b>{{ getBadgeNumber(clearance) }}
+          </p>
+          <p v-if="!isEmpty(getBadgeExpirationDate(clearance))" class="gray-text ml-6">
+            <b>Expiration: </b>{{ getBadgeExpirationDate(clearance) }}
+          </p>
+          <p v-if="!isEmpty(getAdjudicationDates(clearance))" class="gray-text ml-6">
+            <b>Adjudication Dates: </b>{{ getAdjudicationDates(clearance) }}
+          </p>
+          <p v-if="!isEmpty(getBiDates(clearance))" class="gray-text ml-6">
+            <b>Bi Dates: </b>{{ getBiDates(clearance) }}
+          </p>
+          <p v-if="!isEmpty(getPolyDates(clearance))" class="gray-text ml-6">
+            <b>Poly Dates: </b>{{ getPolyDates(clearance) }}
+          </p>
+        </v-card-text>
+      </v-row>
+      <v-row no-gutters class="mx-5">
+        <v-divider v-if="index < list.length - 1" />
+      </v-row>
+    </v-list-item>
+  </v-list>
+</template>
+
+<script setup>
+import { isEmpty, monthDayYearFormat } from '@/utils/utils';
+
+// |--------------------------------------------------|
+// |                                                  |
+// |                       SETUP                      |
+// |                                                  |
+// |--------------------------------------------------|
+
+defineProps({
+  list: {
+    type: Array,
+    required: true
+  }
+});
+
+// |--------------------------------------------------|
+// |                                                  |
+// |                     METHODS                      |
+// |                                                  |
+// |--------------------------------------------------|
+
+/**
+ * Returns the type of clearance
+ *
+ * @param {*} clearance The clearance object
+ * @return String - clearance type
+ */
+function getClearanceType(clearance) {
+  return clearance.awaitingClearance ? clearance.type + ' - Awaiting Clearance' : clearance.type;
+}
+
+/**
+ * Returns clearance submission date
+ *
+ * @param {*} clearance The clearance object
+ * @return String - submission date
+ */
+function getSubmissionDate(clearance) {
+  return monthDayYearFormat(clearance.submissionDate);
+}
+
+/**
+ * Returns clearance granted date
+ *
+ * @param {*} clearance The clearance object
+ * @return String - granted date
+ */
+function getGrantedDate(clearance) {
+  if (!clearance.awaitingClearance) return monthDayYearFormat(clearance.grantedDate);
+  else return null;
+}
+
+/**
+ * Returns employee badge number
+ *
+ * @param {*} clearance The clearance object
+ * @return String - badge number
+ */
+function getBadgeNumber(clearance) {
+  if (!clearance.awaitingClearance) return clearance.badgeNum;
+  else return null;
+}
+
+/**
+ * Returns badge expiration date
+ *
+ * @param {*} clearance The clearance object
+ * @return String - expiration date
+ */
+function getBadgeExpirationDate(clearance) {
+  if (!clearance.awaitingClearance) return monthDayYearFormat(clearance.badgeExpirationDate);
+  else return null;
+}
+
+/**
+ * Returns clearance adjudication dates
+ *
+ * @param {*} clearance The clearance object
+ * @return String - adjudication dates
+ */
+function getAdjudicationDates(clearance) {
+  let dates = null;
+  if (!isEmpty(clearance.adjudicationDates)) {
+    clearance.adjudicationDates.forEach((adjudicationDate) => {
+      if (isEmpty(dates)) dates = monthDayYearFormat(adjudicationDate);
+      else dates = dates + '; ' + monthDayYearFormat(adjudicationDate);
+    });
+  }
+  return dates;
+}
+
+/**
+ * Returns clearance bi dates
+ *
+ * @param {*} clearance The clearance object
+ * @return String - bi dates
+ */
+function getBiDates(clearance) {
+  let dates = null;
+  if (!isEmpty(clearance.biDates)) {
+    clearance.biDates.forEach((biDate) => {
+      if (isEmpty(dates)) dates = monthDayYearFormat(biDate);
+      else dates = dates + '; ' + monthDayYearFormat(biDate);
+    });
+  }
+  return dates;
+}
+
+/**
+ * Returns clearance poly dates
+ *
+ * @param {*} clearance The clearance object
+ * @return String - poly dates
+ */
+function getPolyDates(clearance) {
+  let dates = null;
+  if (!isEmpty(clearance.adjudicationDates)) {
+    clearance.polyDates.forEach((polyDate) => {
+      if (isEmpty(dates)) dates = monthDayYearFormat(polyDate);
+      else dates = dates + '; ' + monthDayYearFormat(polyDate);
+    });
+  }
+  return dates;
+}
+</script>
+
+<style scoped>
+.clearance-type-text {
+  font-size: 16px;
+}
+
+.gray-text {
+  color: #828282;
+}
+</style>
