d48bd6126d281cc00610e90cdde825b4ea4cec04
madelinegburns <mburns@consultwithcase.com>
2024-07-15 12:21:52 -0400

    POR-2802-console-error-in-resume-parser: fixed console error (#328)
    Co-authored-by: Madeline Burns <madelineburns@Madelines-Laptop.local>

--- src/components/modals/ResumeParser.vue
+++ src/components/modals/ResumeParser.vue
@@ -1114,7 +1114,7 @@ function clearForm() {
   this.totalChanges = 0;
 
   if (this.$refs.submit !== undefined) {
-    this.$refs.submit.reset();
+    this.$refs.submit.reset;
   }
   this.extractResume = false;
 } // clearForm
