15c0e8430d4eddbaeb653730683b1e1b30785c4a
madelinegburns <mburns@consultwithcase.com>
2024-07-16 14:42:53 -0400

    POR-2735: start and end dates for contracts (#352)
    * POR-2735:Added start and end dates for contracts
    
    * POR-2735:Fixed styling
    
    ---------
    
    Co-authored-by: Madeline Burns <madelineburns@Madelines-Laptop.local>
    Co-authored-by: Madeline Burns <madelineburns@MadelinesLaptop.lan>

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -148,6 +148,17 @@ export function monthYearFormat(date) {
 } // monthYearFormat
 
 /**
+ * Returns a date formated as 'MMM YYYY' (Aug 2020).
+ *
+ * @param date - date to format in string form
+ * @return String - date formated
+ */
+export function monthYearFormatFromString(stringDate) {
+  let pieces = stringDate.split('/');
+  return MONTHS[parseInt(pieces[0])] + ' ' + pieces[1];
+} // monthYearFormat
+
+/**
  * Returns a date formated as 'MMM, YYYY' (Aug, 2020).
  *
  * @param date - date to format
@@ -612,6 +623,21 @@ export const COUNTRIES = [
   'Zimbabwe'
 ]; //countries list
 
+export const MONTHS = {
+  1: 'Jan',
+  2: 'Feb',
+  3: 'Mar',
+  4: 'Apr',
+  5: 'May',
+  6: 'Jun',
+  7: 'Jul',
+  8: 'Aug',
+  9: 'Sep',
+  10: 'Oct',
+  11: 'Nov',
+  12: 'Dec'
+};
+
 export default {
   asyncForEach,
   formatNumber,
@@ -635,5 +661,6 @@ export default {
   userRoleIsManager,
   userRoleIsUser,
   userRoleIsIntern,
-  COUNTRIES
+  COUNTRIES,
+  MONTHS
 };
