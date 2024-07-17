e2b704eef939dee24229907b0179e217db0b1ad6
Sammy Fischer <sfischer@consultwithcase.com>
2024-07-17 17:11:41 -0400

    created PrivateButon component

---
+++ src/components/employee-beta/PrivateButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <v-tooltip activator="parent" location="top" :text="tooltipText"></v-tooltip>
+    <v-btn :icon="buttonIcon" variant="text" @click="secured = !secured"></v-btn>
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+const secured = defineModel();
+
+const buttonIcon = computed(() => (secured.value ? 'mdi-shield' : 'mdi-shield-outline'));
+const tooltipText = computed(() =>
+  secured.value ? 'Click to show other emlployees' : 'Click to hide from other employees'
+);
+</script>
