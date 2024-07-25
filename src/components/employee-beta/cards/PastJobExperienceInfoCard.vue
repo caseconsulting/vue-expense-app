ae413d994a9b251d35f3833c68a5b9c65d2e36da
madelinegburns <mburns@consultwithcase.com>
2024-07-25 14:19:59 -0400

    POR-2850: Added IC exp to Job Exp card and modal (#408)
    Co-authored-by: Madeline Burns <madelineburns@Madelines-Laptop.local>

--- src/components/employee-beta/cards/PastJobExperienceInfoCard.vue
+++ src/components/employee-beta/cards/PastJobExperienceInfoCard.vue
@@ -1,6 +1,13 @@
 <template>
   <base-card title="Past Experience">
-    <v-card-text v-if="isEmpty(pageList)" class="mt-6" style="text-align: center">
+    <v-card-text>
+      <p class="ic-text mt-3 ml-7"><b>Experience in IC:</b></p>
+      <p class="ic-text gray-text ml-3" align="right">{{ icExperience }}</p>
+    </v-card-text>
+    <v-row class="mx-12">
+      <v-divider class="mt-2" />
+    </v-row>
+    <v-card-text v-if="isEmpty(pageList)" class="mt-3" style="text-align: center; font-size: medium">
       <p>No Past Job Experience Information</p>
     </v-card-text>
     <v-card-text v-else>
@@ -10,7 +17,7 @@
           <v-btn @click="toggleJobExpModal()">Click To See More</v-btn>
         </v-card-actions>
       </div>
-      <past-job-experience-modal v-model="toggleModal" :model="model"></past-job-experience-modal>
+      <past-job-experience-modal v-model="toggleModal" :model="model" :onModal="onModal"></past-job-experience-modal>
     </v-card-text>
   </base-card>
 </template>
@@ -22,10 +29,13 @@ import { onBeforeMount, ref } from 'vue';
 import BaseCard from './BaseCard.vue';
 import PastExperienceList from '../lists/PastExperienceList.vue';
 import PastJobExperienceModal from '../modals/PastJobExperienceModal.vue';
+import { difference, format, getTodaysDate, isBefore, maximum, minimum } from '../../../shared/dateUtils';
+import { computed } from 'vue';
 
 const props = defineProps(['model']);
 
 const filterCompanies = ref(_.cloneDeep(props.model.companies));
+const onModal = ref(false);
 const pageList = ref([]);
 const toggleModal = ref(false);
 
@@ -46,11 +56,85 @@ onBeforeMount(() => {
 
 // |--------------------------------------------------|
 // |                                                  |
+// |                     COMPUTED                     |
+// |                                                  |
+// |--------------------------------------------------|
+
+const icExperience = computed(() => {
+  // get values from input, convert to array, and then sort them
+  let givenRanges = _.mapValues(props.model.icTimeFrames, 'range');
+  givenRanges = Object.values(givenRanges);
+  const durations = givenRanges
+    .sort((a, b) => {
+      // array has text in format YYYY-MM, so reformat to YYYYMM so
+      // that it can be sorted as a regular int
+      format(a[0], 'YYYY-MM', 'YYYYMM') - format(b[0], 'YYYY-MM', 'YYYYMM');
+    })
+    .reverse();
+  let ranges = [];
+  let previousVal, firstStart, lastEnd;
+  // combine any dates that overlap, keep separate ones that don't
+  durations.forEach((d) => {
+    previousVal = ranges[ranges.length - 1];
+    if (ranges.length != 0 && isBefore(d[0], previousVal[1])) {
+      // overlap combination
+      firstStart = minimum(previousVal[0], d[0]);
+      lastEnd = maximum([previousVal[1], d[1]]);
+      ranges[ranges.length - 1] = [firstStart, lastEnd];
+    } else {
+      // no overlap
+      ranges.push(d);
+    }
+  });
+
+  let totalDurationMonths = 0; // total months
+  // loop each reach to get total duration in months
+  _.forEach(ranges, (range) => {
+    let start = format(range[0], null, 'YYYY-MM');
+    let end = range.length > 1 ? format(range[1], null, 'YYYY-MM') : getTodaysDate('YYYY-MM');
+    let duration = difference(end, start, 'months') + 1; // calculate range duration
+    totalDurationMonths += Math.max(duration, 0); // remove negative values
+  });
+  // set year output text
+  let totalYearOutput = _.floor(totalDurationMonths / 12);
+  if (totalYearOutput < 1) {
+    totalYearOutput = '';
+  } else if (totalYearOutput == 1) {
+    totalYearOutput += ' Year';
+  } else {
+    totalYearOutput += ' Years';
+  }
+  // set month output text
+  let totalMonthOutput = totalDurationMonths % 12;
+  if (totalMonthOutput < 1) {
+    totalMonthOutput = totalYearOutput.length > 0 ? '' : 'None';
+  } else if (totalMonthOutput == 1) {
+    totalMonthOutput = totalYearOutput.length > 0 ? ` and ${totalMonthOutput} Month` : `${totalMonthOutput} Month`;
+  } else {
+    totalMonthOutput = totalYearOutput.length > 0 ? ` and ${totalMonthOutput} Months` : `${totalMonthOutput} Months`;
+  }
+  return `${totalYearOutput}${totalMonthOutput}`;
+}); // icExperience
+
+// |--------------------------------------------------|
+// |                                                  |
 // |                     METHODS                      |
 // |                                                  |
 // |--------------------------------------------------|
 
 function toggleJobExpModal() {
   toggleModal.value = !toggleModal.value;
+  onModal.value = !onModal.value;
 }
 </script>
+
+<style scoped>
+.gray-text {
+  color: #828282;
+}
+
+.ic-text {
+  display: inline;
+  font-size: medium;
+}
+</style>
