9c9b6e7633f254c20145f01b2a2e60ceb616e731
dcrotty1 <dcrotty@consultwithcase.com>
2024-07-25 14:20:31 -0400

    added view all buttons to all cards (#407)

--- src/components/employee-beta/cards/AwardsCard.vue
+++ src/components/employee-beta/cards/AwardsCard.vue
@@ -11,7 +11,7 @@
         <!-- Employee does not have Awards -->
         <p v-else class="mt-6 ml-6">No Award Information</p>
         <!-- Pagination -->
-        <div v-if="!isEmpty(model.awards) && Math.ceil(model.awards.length / 5) != 1" class="text-center">
+        <div v-if="!isEmpty(model.awards)" class="text-center">
           <v-card-actions class="d-flex justify-center">
             <v-btn @click="toggleAwardsModal()">View all {{ model.awards.length }} awards </v-btn>
           </v-card-actions>
