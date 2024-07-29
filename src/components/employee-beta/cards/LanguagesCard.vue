46af7836e4fb81a5c54753b46741be6d53cc2457
Dylan Crotty <dcrotty@consultwithcase.com>
2024-07-29 17:32:28 -0400

    fixed bug

--- src/components/employee-beta/cards/LanguagesCard.vue
+++ src/components/employee-beta/cards/LanguagesCard.vue
@@ -3,13 +3,13 @@
     <base-card title="Foreign Languages">
       <v-card-text class="px-2 pt-0 pb-0 text-black">
         <!-- Employee has entered languages-->
-        <languages-list v-if="!isEmpty(languages)" :list="filteredList"></languages-list>
+        <languages-list v-if="!isEmpty(model.languages)" :list="filteredList"></languages-list>
         <!-- Employee does not have Language Experience -->
         <p v-else class="mt-6 ml-6">No Foreign Language Information</p>
-        <div v-if="!isEmpty(languages)" class="text-center">
+        <div v-if="!isEmpty(model.languages)" class="text-center">
           <v-card-actions class="d-flex justify-center">
             <v-btn variant="flat" color="#F3F3F3" @click="activateModal()"
-              >View All {{ languages.length }} Languages</v-btn
+              >View All {{ model.languages.length }} Languages</v-btn
             >
           </v-card-actions>
         </div>
@@ -33,7 +33,6 @@ import LanguagesModal from '../modals/LanguagesModal.vue';
 // |--------------------------------------------------|
 
 const props = defineProps(['model']);
-const languages = ref(props.model.languages ? props.model.languages : []);
 const toggleModal = ref(false);
 
 // |--------------------------------------------------|
@@ -45,8 +44,8 @@ const toggleModal = ref(false);
 const filteredList = computed(() => {
   const startIndex = 0; //each page contains 5 certification entries
   const endIndex = 5;
-  if (!isEmpty(languages.value)) {
-    return sortLanguagesByProficiency(languages.value).slice(startIndex, endIndex);
+  if (!isEmpty(props.model.languages)) {
+    return sortLanguagesByProficiency(props.model.languages).slice(startIndex, endIndex);
   }
   return [];
 });
