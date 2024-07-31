8d39978fbc2048197f0ed2bf9a63e780b98eb3ca
Max D <max@Maxs-MacBook-Air.local>
2024-07-31 10:40:25 -0400

    dropdown opens up when you click magnifying glass

--- src/components/employee-beta/cards/ContractInfoCard.vue
+++ src/components/employee-beta/cards/ContractInfoCard.vue
@@ -14,10 +14,10 @@
           </p>
         </div>
         <div v-else class="text-align: center">
-          <p><b>No active contracts</b></p>
+          <p>No active contracts</p>
         </div>
       </v-card-text>
-      <v-card-text class="text-align: center" v-else> No contract Information </v-card-text>
+      <v-card-text class="mt-6 text-align: center" v-else> No contract Information </v-card-text>
       <v-card-actions class="justify-center">
         <v-btn variant="flat" color="#F3F3F3" @click="open">Click to see more</v-btn>
       </v-card-actions>
