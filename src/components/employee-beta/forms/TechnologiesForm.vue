8da651cd7596fce8a83d11925a2cd70d58ee0a65
Max D <max@Maxs-MacBook-Air.local>
2024-08-01 10:26:14 -0400

    removed one of the add buttons for tech and skills and languages when empty list

--- src/components/employee-beta/forms/TechnologiesForm.vue
+++ src/components/employee-beta/forms/TechnologiesForm.vue
@@ -1,6 +1,6 @@
 <template>
   <v-form ref="form" validate-on="lazy">
-    <v-row><h3>Tech & Skills</h3></v-row>
+    <v-row class="mt-2"><h3>Tech & Skills</h3></v-row>
     <v-row>
       <v-col class="d-flex justify-center">
         <v-btn prepend-icon="mdi-plus" @click="addTechnology()">Add Tech/Skill</v-btn>
@@ -75,7 +75,7 @@
         </v-row>
       </v-col>
     </v-row>
-    <v-row>
+    <v-row v-if="technologies.length != 0">
       <v-col class="d-flex justify-center">
         <v-btn prepend-icon="mdi-plus" @click="addTechnology(false)">Add Tech/Skill</v-btn>
       </v-col>
