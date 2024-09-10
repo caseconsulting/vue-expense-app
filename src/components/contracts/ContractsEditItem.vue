6b82805b3157ffe2196897006c02050e10f8b77b
Chad Martin <martincc@dukes.jmu.edu>
2024-09-10 10:51:17 -0400

    POR-3006: remove loading for contracts power edit

--- src/components/contracts/ContractsEditItem.vue
+++ src/components/contracts/ContractsEditItem.vue
@@ -39,14 +39,7 @@
       :label="props.header.title"
       variant="underlined"
     ></v-text-field>
-    <v-btn
-      density="comfortable"
-      @click="saveItem()"
-      :disabled="!valid || loading"
-      :loading="loading"
-      icon
-      variant="text"
-    >
+    <v-btn density="comfortable" @click="saveItem()" :disabled="!valid" icon variant="text">
       <v-tooltip activator="parent">Save {{ props.header.title }}</v-tooltip>
       <v-icon>mdi-content-save</v-icon>
     </v-btn>
@@ -68,7 +61,6 @@ const store = useStore();
 const model = ref(props.item[props.header.key]);
 const header = ref(props.header);
 const item = ref(props.item);
-const loading = ref(false);
 const valid = ref(false);
 
 header.value.editing = true;
@@ -99,24 +91,20 @@ function getDropdownItems() {
 
 async function saveItem() {
   if (valid.value) {
-    loading.value = true;
     let key = props.header.key;
-    console.log(item.value);
     if (item.value.contractId) {
       // update contract
       item.value[key] = model.value;
-      await api.updateAttribute(api.CONTRACTS, { id: item.value.id, [`${key}`]: item.value[key] }, key);
+      api.updateAttribute(api.CONTRACTS, { id: item.value.id, [`${key}`]: item.value[key] }, key);
     } else {
       // update projects field
       let contract = _find(store.getters.contracts, (c) => _find(c.projects, (p) => p.id === item.value.id));
       let pIndex = _findIndex(contract.projects, (p) => p.id === item.value.id);
       item.value[key] = model.value;
       contract.projects[pIndex] = item.value;
-      await api.updateAttribute(api.CONTRACTS, { id: contract.id, projects: contract.projects }, 'projects');
+      api.updateAttribute(api.CONTRACTS, { id: contract.id, projects: contract.projects }, 'projects');
     }
-    //store.dispatch('setContracts', { contracts: [...store.getters.contracts] });
     emitter.emit('saved-contract-item');
-    loading.value = false;
   }
 }
 </script>
