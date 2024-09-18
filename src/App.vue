ea7834348e3c81eae4f4c5ef665e5542440ae2c6
Chad Martin <martincc@dukes.jmu.edu>
2024-09-18 11:11:06 -0400

    upgrade package version and change release notes link

--- src/App.vue
+++ src/App.vue
@@ -131,7 +131,7 @@
             id="P"
             class="text-black"
             target="_blank"
-            href="https://3.basecamp.com/3097063/buckets/4708396/documents/7711838514"
+            href="https://3.basecamp.com/3097063/buckets/4708396/documents/7828405304"
           >
             <v-tooltip activator="parent" location="top">View Release Notes</v-tooltip>
             <strong>Version</strong> {{ version }}
