15e16a62b6f98ef79159e07bee002d046e411cea
rlitscher <rlitscher@consultwithcase.com>
2025-05-19 15:09:57 -0400

    show rank

--- src/components/shared/timesheets/Leader.vue
+++ src/components/shared/timesheets/Leader.vue
@@ -1,9 +1,11 @@
 <template>
   <div v-if="leader" class="d-flex align-center">
-    <v-icon size="x-large" class="me-3" :class="iconClass">mdi-trophy</v-icon>
+    <v-icon size="x-large" :class="iconClass">mdi-trophy</v-icon>
     <span class="py-1">
+      <b v-if="(leader?.rank || 0) > 3" class="me-1">#{{ leader?.rank }}</b>
       <user-avatar :employee="leader" :image="leader?.avatar" :size="45" />
-      <span class="ms-1"> #{{ leader?.rank }} {{ leader?.nickname || leader?.firstName }}</span>
+      <v-tooltip activator="parent" location="top">{{ leader.firstName }} {{ leader.lastName }}</v-tooltip>
+      <span class="ms-1">{{ leader?.nickname || leader?.firstName }}</span>
       <span class="ms-1">({{ Math.trunc(leader?.billableHours) }})</span>
     </span>
   </div>
