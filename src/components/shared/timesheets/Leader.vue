a4849e6e97089ca39245f7810dc7ce0231430372
rlitscher <rlitscher@consultwithcase.com>
2025-05-19 16:44:12 -0400

    update tooltip

--- src/components/shared/timesheets/Leader.vue
+++ src/components/shared/timesheets/Leader.vue
@@ -4,7 +4,7 @@
     <span class="py-1">
       <b v-if="(leader?.rank || 0) > 3" class="me-1">#{{ leader?.rank }}</b>
       <user-avatar :employee="leader" :image="leader?.avatar" :size="45" />
-      <v-tooltip activator="parent" location="top">{{ leader.firstName }} {{ leader.lastName }}</v-tooltip>
+      <v-tooltip activator="parent" location="top">{{ leader?.firstName }} {{ leader?.lastName }}</v-tooltip>
       <span class="ms-1">{{ leader?.nickname || leader?.firstName }}</span>
       <span class="ms-1">({{ Math.trunc(leader?.billableHours) }})</span>
     </span>
