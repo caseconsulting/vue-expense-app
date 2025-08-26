28c41ada190f5785390575a7cbe36cba7b7f2efc
logburn <21106956+logburn@users.noreply.github.com>
2025-08-26 12:29:40 -0400

    POR-3279: add default if no custom contract hours

--- src/components/shared/timesheets/TimePeriodDetails.vue
+++ src/components/shared/timesheets/TimePeriodDetails.vue
@@ -116,6 +116,7 @@ const store = useStore();
 
 const clonedEmployee = ref(props.employee);
 const BONUS_YEAR_TOTAL = ref(1860);
+const CONTRACT_YEAR_DEFAULT = ref(1860);
 const WORK_HOURS_PER_DAY = ref(8); // normal hours per day for full time employees
 const customCompleted = ref(null);
 const customNeeded = ref(null);
@@ -244,7 +245,7 @@ const periodHoursCompleted = computed(() => {
  */
 const proRatedHours = computed(() => {
   if (props.isYearly) {
-    let totalHours = props.isCalendarYear ? BONUS_YEAR_TOTAL.value : contractHours.value;
+    let totalHours = props.isCalendarYear ? BONUS_YEAR_TOTAL.value : (contractHours.value ?? CONTRACT_YEAR_DEFAULT.value);
     let { startDate, endDate } = props.period;
     return (totalHours / getWorkDays(startDate, endDate, true)) * (props.employee.workStatus / 100);
   } else {
