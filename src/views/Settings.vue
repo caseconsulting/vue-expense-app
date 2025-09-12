0e5cabef84d6c71669da18fad09ab3fbb5e35193
rlitscher <rlitscher@consultwithcase.com>
2025-09-12 15:01:07 -0400

    POR-3268 Update notification list

---
+++ src/views/Settings.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <v-card>
+      <v-card-title class="header-slim">
+        <v-icon
+          icon="mdi-cog"
+          size="small"
+          class="mr-1 mb-1"
+        />
+        Settings
+      </v-card-title>
+      <v-card-text>
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script setup>
+import PowerEditTable from '@/components/employees/power-edit/PowerEditTable.vue';
+
+const settings = [
+  {
+    key: 'test',
+    value: 'info@consultwithcase.com',
+  }
+]
+</script>
