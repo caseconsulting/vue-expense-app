d33f880377e95c14f5d880c30c013f65b767948a
rlitscher <rlitscher@consultwithcase.com>
2025-09-15 14:28:33 -0400

    add setting form

---
+++ src/components/settings/SettingsForm.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-dialog
+    persistent
+    scrollable
+  >
+    <v-card id="employee-card">
+      <v-card-title
+        class="header-slim"
+        style="position: sticky; top: 0; z-index: 2"
+      >Add new Setting</v-card-title>
+      <v-card-text>
+        <v-form ref="form" v-model="valid" lazy-validation>
+          <v-container>
+            <v-row>
+              <v-col>
+                <v-text-field
+                  v-model="formData.id"
+                  label="ID"
+                  disabled
+                ></v-text-field>
+              </v-col>
+              <v-col>
+                <v-text-field
+                  v-model="formData.key"
+                  label="Key"
+                  required
+                ></v-text-field> 
+              </v-col>
+              <v-col>
+                <v-text-field
+                  v-model="formData.setting"
+                  label="Setting"
+                  required
+                ></v-text-field> 
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { generateUUID } from '@/utils/utils';
+
+const formData = ref({id: generateUUID()});
+
+const valid = ref(false);
+</script>
--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,13 +1,20 @@
 <template>
   <div>
     <v-card>
-      <v-card-title class="header-slim">
+      <v-card-title class="header-slim d-flex align-center">
         <v-icon
           icon="mdi-cog"
           size="small"
           class="mr-1 mb-1"
-        />
-        Settings
+        />Settings
+
+        <v-spacer />
+
+        <v-btn
+          small
+          variant="text"
+          @click="add = true"
+          icon="mdi-plus" ></v-btn>
       </v-card-title>
       <v-card-text>
           <power-edit-table
@@ -16,31 +23,49 @@
           ></power-edit-table>
       </v-card-text>
     </v-card>
+    <settings-form v-model="add"></settings-form>
   </div>
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue';
+import { inject, onMounted, ref } from 'vue';
 import PowerEditTable from '@/components/shared/power-edit/Table.vue';
+import SettingsForm from '@/components/settings/SettingsForm.vue';
 import { TYPES } from '@/components/shared/power-edit/js/fieldTypes.js';
 import api from '@/shared/api.js';
 
-const settings = ref([
-]);
-
+const emitter = inject('emitter');
+const add = ref(false);
+const settings = ref([]);
 const fields = [{
   title: 'Key',
   key: 'key',
   fixed: true,
   infoType: TYPES.DEFAULT
 }, {
-  title: 'Value',
-  key: 'value',
+  title: 'Setting',
+  key: 'setting',
   editType: TYPES.DEFAULT,
   infoType: TYPES.DEFAULT
   }];
 
+
+emitter.on('save-edit-item', async ({ item, editItem, field }) => {
+  await saveItem(item, editItem, field);
+});
+
+
 onMounted(async () => {
   settings.value = await api.getSettings();
 });
+
+
+async function saveItem(item, editItem, field) {
+  editItem.value = null;
+  let setting = settings.value.find(s => s.id === item.id);
+  if (setting) {
+    setting[field.key] = item[field.key];
+    resp = await api.updateAttribute(api.SETTINGS, { id: item.id, [`${field.key}`]: item[field.key] }, field.key);
+  }
+}
 </script>
