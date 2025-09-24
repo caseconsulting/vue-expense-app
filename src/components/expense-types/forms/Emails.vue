fcdc78001d708a1b627b75ade3c2c2b87eba2f25
rlitscher <rlitscher@consultwithcase.com>
2025-09-24 12:34:06 -0400

    remove helper

--- src/components/expense-types/forms/Emails.vue
+++ src/components/expense-types/forms/Emails.vue
@@ -2,7 +2,6 @@
   <div>
     <strong>Emails</strong>
     <case-email-field v-model="modelValue.to" :required="false" label="To Email"></case-email-field>
-    {{ modelValue.to }}
     <case-email-field v-model="modelValue.cc" :required="false" label="CC Email"></case-email-field>
     <case-email-field v-model="modelValue.bcc" :required="false" label="BCC Email"></case-email-field>
     <case-email-field v-model="modelValue.replyTo" :required="false" label="Reply-To Email"></case-email-field>
