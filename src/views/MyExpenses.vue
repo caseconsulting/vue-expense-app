1f0a06b2a163769ab7aea536cc232e83f2ab34f2
Luke Ogburn <21106956+logburn@users.noreply.github.com>
2025-11-14 15:18:51 -0500

    done

--- src/views/MyExpenses.vue
+++ src/views/MyExpenses.vue
@@ -158,24 +158,31 @@
                       </v-tooltip>
                     </v-btn-toggle>
                   </v-col>
-                  <!-- End Active Filter -->
                   <!-- Status Filter -->
                   <v-col cols="3" :class="!userRoleIsAdmin() && !userRoleIsManager() ? 'ml-3' : ''">
                     <h4>Status:</h4>
                     <v-combobox
+                      density="comfortable"
                       v-model="filter.status"
                       :items="statusFilterOptions"
+                      :prepend-inner-icon="getStateIcon(filter.status.toUpperCase())"
                       variant="underlined"
                       hide-details
                     >
-                    <template #item="{ props, item }">
-                      <v-list-item v-bind="props" :title="item.title">
-                        <v-list-item-title>{{ item }}</v-list-item-title>
-                      </v-list-item>
-                    </template>
-                  </v-combobox>
+                      <template v-slot:item="{ props, item }">
+                        <v-list-item
+                          v-bind="props"
+                          :title="item.raw"
+                        >
+                        <template v-slot:prepend>
+                          <v-avatar>
+                            <v-icon color="#111" :icon="getStateIcon(item.raw.toUpperCase())" />
+                          </v-avatar>
+                        </template>
+                        </v-list-item>
+                      </template>
+                    </v-combobox>
                   </v-col>
-                  <!-- End Status Filter -->
                   <!-- Reimbursed Date Range Filter -->
                   <v-col cols="6" class="pa-2">
                     <h4 class="ml-0 pl-0 mb-1">Reimbursed Date Range:</h4>
@@ -213,7 +220,6 @@
                           "
                         />
                       </v-menu>
-                      <!-- End Start Date Filter-->
 
                       <!-- End Date Filter -->
                       <v-menu v-model="endDateFilterMenu" :close-on-content-click="false" location="start center">
@@ -248,15 +254,12 @@
                           "
                         />
                       </v-menu>
-                      <!-- End End Date Filter-->
                     </v-row>
-                    <!-- End Date Range Filter -->
                   </v-col>
                 </v-row>
               </v-container>
             </fieldset>
             <br />
-            <!-- End Filters -->
 
             <!-- My Expenses Data Table-->
             <v-data-table
@@ -274,44 +277,77 @@
               :no-data-text="'No results :('"
               expand-on-click
             >
-              <!-- State slot -->
+              <!-- State slot, including quick menu -->
               <template #[`item.state`]="{ item }">
                 <td v-if="userRoleIsAdmin() || userRoleIsManager()">
-                  <v-icon :icon="getStateIcon(item.state)" size="large" />
+                  <v-menu>
+                    <template #activator="{ props }">
+                      <v-btn
+                        :icon="getStateIcon(item.state)"
+                        :disabled="expensesStatuses.disabled.has(item.id)"
+                        :color="getStatusIconColor(item)"
+                        variant="text"
+                        v-bind="props"
+                        density="compact"
+                      />
+                    </template>
+                    <v-list v-if="getStatesQuickMenu(item.state).length && !expensesStatuses.errored.has(item.id)">
+                      <v-list-item
+                        v-for="(menuItem, idx) in getStatesQuickMenu(item.state)"
+                        :no-data-text="`No actions for state ${item.state}`"
+                        :key="`menu-${idx}`"
+                        @click="menuItem.action(item)"
+                        slim
+                      >
+                        <template v-slot:prepend>
+                          <v-avatar>
+                            <v-icon color="#111" :icon="menuItem.icon" />
+                          </v-avatar>
+                        </template>
+                        <v-list-item-title>{{ menuItem.text }}</v-list-item-title>
+                      </v-list-item>
+                    </v-list>
+                  </v-menu>
                   <v-tooltip activator="parent" location="right"> {{ getStateTooltip(item) }} </v-tooltip>
                 </td>
               </template>
               <!-- Creation date slot -->
               <template #[`item.createdAt`]="{ item }">
-                <td>{{ monthDayYearFormat(item.createdAt) }}</td>
+                <td :class="getPurchaseCreatedAtDateClass(item)">
+                  {{ monthDayYearFormat(item.createdAt) }}
+                  <v-tooltip v-if="expenseAgeOverLimit(item)" activator="parent" location="top"> Creation date is {{ expenseAgeOverLimit(item) }} days after purchase date </v-tooltip>
+                </td>
               </template>
               <!-- Employee name slot-->
               <template #[`item.employeeName`]="{ item }">
-                <td v-if="userRoleIsAdmin() || userRoleIsManager()">
+                <td :class="getExpenseClass(item)" v-if="userRoleIsAdmin() || userRoleIsManager()">
                   {{ item.employeeName }}
                 </td>
               </template>
               <!-- Budget Name Slot -->
               <template #[`item.budgetName`]="{ item }">
-                <td>{{ item.budgetName }}</td>
+                <td :class="getExpenseClass(item)">{{ item.budgetName }}</td>
               </template>
               <!-- Cost slot -->
               <template #[`item.cost`]="{ item }">
-                <td>{{ convertToMoneyString(item.cost) }}</td>
+                <td :class="getExpenseClass(item)">{{ convertToMoneyString(item.cost) }}</td>
               </template>
               <!-- Purchase date slot -->
               <template #[`item.purchaseDate`]="{ item }">
-                <td>{{ monthDayYearFormat(item.purchaseDate) }}</td>
+                <td :class="getPurchaseCreatedAtDateClass(item)">
+                  {{ monthDayYearFormat(item.purchaseDate) }}
+                  <v-tooltip v-if="expenseAgeOverLimit(item)" activator="parent" location="top"> Creation date is {{ expenseAgeOverLimit(item) }} days after purchase date </v-tooltip>
+                </td>
               </template>
               <!-- Reimburse date Slot -->
               <template #[`item.reimbursedDate`]="{ item }">
-                <td>{{ monthDayYearFormat(item.reimbursedDate) }}</td>
+                <td :class="getExpenseClass(item)">{{ monthDayYearFormat(item.reimbursedDate) }}</td>
               </template>
               <!--Action Items-->
               <template #[`item.actions`]="{ item }">
                 <td class="d-flex justify-end mr-4">
                   <!-- Download Attachment Button -->
-                  <attachment :mid-action="midAction" :expense="item" :mode="'expenses'" />
+                  <attachment :mid-action="midAction || expensesStatuses.disabled.has(item.id)" :expense="item" :mode="'expenses'" />
 
                   <!-- Edit Button -->
                   <v-btn
@@ -319,6 +355,7 @@
                     :disabled="
                       isEditing ||
                       midAction ||
+                      expensesStatuses.disabled.has(item.id) ||
                       !isExpenseEditable(store.getters.user, item) ||
                       (!(userRoleIsAdmin() || userRoleIsManager()) && !canDelete(item))
                     "
@@ -340,6 +377,7 @@
                       isReimbursed(item) ||
                       isEditing ||
                       midAction ||
+                      expensesStatuses.disabled.has(item.id) ||
                       (!(userRoleIsAdmin() || userRoleIsManager()) && !canDelete(item))
                     "
                     variant="text"
@@ -353,23 +391,6 @@
                   >
                     <v-icon size="x-large" class="case-gray" icon="mdi-delete" />
                     <v-tooltip activator="parent" location="top"> Delete </v-tooltip>
-                  </v-btn>
-                  <!-- Unreimburse Button -->
-                  <v-btn
-                    v-if="userRoleIsAdmin() || userRoleIsManager()"
-                    id="unreimburse"
-                    :disabled="!isReimbursed(item) || isEditing || midAction"
-                    variant="text"
-                    icon
-                    size="small"
-                    @click="
-                      unreimbursing = !unreimbursing;
-                      midAction = true;
-                      propExpense = item;
-                    "
-                  >
-                    <v-icon size="x-large" class="case-gray" icon="mdi-currency-usd-off" />
-                    <v-tooltip activator="parent" location="top"> Unreimburse </v-tooltip>
                   </v-btn>
                 </td>
               </template>
@@ -434,7 +455,7 @@
             <v-card-actions>
               <convert-expenses-to-csv
                 v-if="userRoleIsAdmin() || userRoleIsManager()"
-                :mid-action="midAction"
+                :mid-action="midAction || expensesStatuses.disabled.size > 0"
                 :expenses="filteredExpenses"
               />
             </v-card-actions>
@@ -452,14 +473,21 @@
         <expense-form v-if="!initialPageLoading" ref="form" :is-edit="isEditing" :expense="expense" />
       </v-col>
     </v-row>
+
+    <!-- Rejection modal -->
+    <v-dialog v-model="toggleExpenseRejectionModal" persistent width="35%">
+      <expense-rejection-modal :defaultType="defaultRejectionType" />
+    </v-dialog>
   </div>
 </template>
 
 <script setup>
 import api from '@/shared/api.js';
+import { AxiosError } from 'axios';
 import Attachment from '@/components/utils/Attachment.vue';
 import ConvertExpensesToCsv from '@/components/expenses/ConvertExpensesToCsv.vue';
 import DeleteModal from '@/components/modals/DeleteModal.vue';
+import ExpenseRejectionModal from '@/components/modals/ExpenseRejectionModal.vue';
 import employeeUtils from '@/shared/employeeUtils';
 import { isExpenseEditable } from '@/shared/expenseUtils';
 import ExpenseForm from '@/components/expenses/ExpenseForm.vue';
@@ -484,7 +512,7 @@ import {
   userRoleIsManager
 } from '@/utils/utils';
 import { employeeFilter } from '@/shared/filterUtils';
-import { format, isBetween } from '@/shared/dateUtils';
+import { format, isBetween, difference, getTodaysDate } from '@/shared/dateUtils';
 import { getDateOptionalRules } from '@/shared/validationUtils.js';
 import { updateStoreBudgets, updateStoreExpenseTypes, updateStoreEmployees, updateStoreTags } from '@/utils/storeUtils';
 import { mask } from 'vue-the-mask';
@@ -578,6 +606,13 @@ const headers = ref([
 const isEditing = ref(false); // whether or not an expense is being edited
 const loading = ref(true); // loading status
 const initialPageLoading = ref(true); // loading page at startup
+const defaultRejectionType = ref('soft'); // rejection modald default
+const rejectingExpense = ref(null); // for rejection modal
+const expensesStatuses = ref({
+  successes: new Set(), // expenses that succeeded API call
+  disabled: new Set(), // expenses in middle of API call
+  errored: new Set()  // expenses that errored on API call
+});
 const midAction = ref(false);
 const propExpense = ref({
   id: null,
@@ -605,6 +640,7 @@ const tagsInfo = ref({
   selected: [],
   flipped: []
 });
+const toggleExpenseRejectionModal = ref(false);
 const unreimbursing = ref(false); // activate unreimburse model when value changes
 const userInfo = ref(null); // user information
 const vMask = mask; //custom directive
@@ -665,6 +701,27 @@ onBeforeMount(async () => {
   emitter.on('confirm-delete-expense', async () => {
     deleting.value = false;
     await deleteExpense();
+  });
+
+  // expense rejection modal
+  emitter.on('close-expenses-rejection', async () => {
+    defaultRejectionType.value = 'soft';
+    toggleExpenseRejectionModal.value = false;
+    rejectingExpense.value = null;
+  });
+  emitter.on('confirm-expenses-rejection', async (data) => {
+    let { field, reason } = data;
+    let rejType = field.split('.')[1];
+    let isHard = field.includes('hard');
+
+    // update expense with new rejection
+    rejectingExpense.value.rejections ??= {};
+    rejectingExpense.value.rejections[rejType] ??= { reasons: [], revised: false };
+    rejectingExpense.value.rejections[rejType].reasons.push(reason);
+    rejectingExpense.value.state = isHard ? EXPENSE_STATES.REJECTED : EXPENSE_STATES.RETURNED;
+
+    // submit (expense is set to null in close-expenses-rejection)
+    await updateExpense(rejectingExpense.value);
   });
 
   if (store.getters.storeIsPopulated) {
@@ -761,6 +818,42 @@ function moneyFilter(value) {
 // |--------------------------------------------------|
 
 /**
+ * Returns 0 if an expense is not over the age limit, otherwise returns the age that the expense is.
+ * @param expense expense to check
+ * @returns expense age, or 0 if within limit
+ */
+function expenseAgeOverLimit(expense) {
+  let limit = 45;
+  let diff = difference(expense.createdAt, expense.purchaseDate, 'day');
+  if (diff <= limit) return 0;
+  return diff;
+}
+
+const successColor = 'green-darken-2';
+const pendingColor = 'grey';
+const errorColor = 'red-darken-2';
+function getStatusIconColor(expense) {
+  if (expensesStatuses.value.successes.has(expense.id)) return successColor;
+  if (expensesStatuses.value.errored.has(expense.id)) return errorColor;
+  if (expensesStatuses.value.disabled.has(expense.id)) return pendingColor;
+}
+
+function getExpenseClass(expense) {
+  if (expensesStatuses.value.successes.has(expense.id)) return 'text-' + successColor;
+  if (expensesStatuses.value.errored.has(expense.id)) return `text-${errorColor} font-weight-bold`;
+  if (expensesStatuses.value.disabled.has(expense.id)) return 'text-' + pendingColor;
+}
+
+function getPurchaseCreatedAtDateClass(expense) {
+  let classes = [];
+  if (expensesStatuses.value.successes.has(expense.id)) classes.push('text-' + successColor);
+  else if (expensesStatuses.value.errored.has(expense.id)) classes.push(`text-${errorColor} font-weight-bold`);
+  else if (expensesStatuses.value.disabled.has(expense.id)) classes.push('text-' + pendingColor);
+  if (expenseAgeOverLimit(expense)) classes.push('text-' + errorColor);
+  return classes.join(' ');
+}
+
+/**
  * Gets the icon for the state of an expense.
  * 
  * @param state - expenses state field
@@ -768,8 +861,9 @@ function moneyFilter(value) {
  */
 function getStateIcon(state) {
   switch (state) {
+    case 'ALL': return 'mdi-check-all'; // special case for filter
     case EXPENSE_STATES.CREATED: return 'mdi-new-box';
-    case EXPENSE_STATES.APPROVED: return 'mdi-check-decagram'; // mdi-signature-freehand
+    case EXPENSE_STATES.APPROVED: return 'mdi-check-decagram';
     case EXPENSE_STATES.REIMBURSED: return 'mdi-cash-check';
     case EXPENSE_STATES.REJECTED: return 'mdi-close-box';
     case EXPENSE_STATES.RETURNED: return 'mdi-arrow-u-left-top-bold';
@@ -777,7 +871,7 @@ function getStateIcon(state) {
     default: return 'mdi-help-rhombus';
   }
 }
-
+  
 /**
  * Gets tooltip text for an expense based on its state.
  * 
@@ -801,6 +895,134 @@ function getStateTooltip(item) {
     case EXPENSE_STATES.RETURNED: return 'Returned for edits';
     case EXPENSE_STATES.REVISED: return 'Revised';
     default: return 'Unknown State';
+  }
+}
+
+async function updateExpense(newExpense) {
+  let { id } = newExpense;
+
+  // set expense to loading and make API call
+  expensesStatuses.value.errored.delete(id);
+  expensesStatuses.value.disabled.add(id);
+  let resp = await api.updateItem(api.EXPENSES, newExpense);
+
+  // respond to API call results
+  if (resp instanceof AxiosError) {
+    expensesStatuses.value.errored.add(id);
+  } else {
+    expensesStatuses.value.successes.add(id);
+    await setTimeout(() => { expensesStatuses.value.successes.delete(id) }, 2000);
+  }
+  expensesStatuses.value.disabled.delete(id);
+}
+
+/**
+ * Builds a little action menu item for the states quick dropdown
+ */
+const quickStatesMenuActions = {
+  APPROVE: async (e) => {
+    e.state = EXPENSE_STATES.APPROVED;
+    e.approvedBy = store.getters.user.id;
+    await updateExpense(e);
+  },
+  UNAPPROVE: async (e) => {
+    e.state = EXPENSE_STATES.CREATED;
+    e.approvedBy = undefined;
+    await updateExpense(e);
+  },
+  REJECT_RETURN: async (e) => {
+    rejectingExpense.value = e;
+    toggleExpenseRejectionModal.value = true;
+  },
+  REJECT: async (e) => {
+    defaultRejectionType.value = 'hard';
+    rejectingExpense.value = e;
+    toggleExpenseRejectionModal.value = true;
+  },
+  REMOVE_REJECTION: async (e) => {
+    if (e.state === EXPENSE_STATES.REJECTED) {
+      e.rejections.hardRejections = null;
+    }
+    if (e.rejections.softRejections) {
+      e.rejections.softRejections.reasons.pop();
+      e.rejections.softRejections.revised = true;
+      if (e.rejections.softRejections.reasons.length === 0) delete e.rejections;
+    }
+    e.state = EXPENSE_STATES.CREATED;
+    console.log(e.id);
+    await updateExpense(e);
+  },
+  REIMBURSE: async (e) => {
+    e.state = EXPENSE_STATES.REIMBURSED;
+    e.reimbursedDate = getTodaysDate('YYYY-MM-DD');
+    await updateExpense(e);
+  },
+  UNREIMBURSE: async (e) => {
+    e.state = EXPENSE_STATES.APPROVED;
+    e.reimbursedDate = null;
+    await updateExpense(e);
+  },
+}
+function getStatesQuickMenu(state) {
+  switch (state) {
+    case EXPENSE_STATES.CREATED:
+    case EXPENSE_STATES.REVISED:
+      return [
+        {
+          action: quickStatesMenuActions.APPROVE,
+          icon: getStateIcon(EXPENSE_STATES.APPROVED),
+          text: 'Approve'
+        },
+        {
+          action: quickStatesMenuActions.REJECT_RETURN,
+          icon: getStateIcon(EXPENSE_STATES.RETURNED),
+          text: 'Reject or Return'
+        }
+      ];
+    case EXPENSE_STATES.REJECTED:
+      return [
+        {
+          action: quickStatesMenuActions.REMOVE_REJECTION,
+          icon: getStateIcon(EXPENSE_STATES.CREATED),
+          text: 'Remove rejection'
+        }
+      ]
+    case EXPENSE_STATES.RETURNED:
+      return [
+        {
+          action: quickStatesMenuActions.REMOVE_REJECTION,
+          icon: getStateIcon(EXPENSE_STATES.CREATED),
+          text: 'Remove revisal'
+        },
+        {
+          action: quickStatesMenuActions.REJECT,
+          icon: getStateIcon(EXPENSE_STATES.REJECTED),
+          text: 'Reject'
+        }
+      ]
+    case EXPENSE_STATES.APPROVED:
+      return [
+        {
+          action: quickStatesMenuActions.REIMBURSE,
+          icon: getStateIcon(EXPENSE_STATES.REIMBURSED),
+          text: 'Reimburse (as of today)'
+        },
+        {
+          action: quickStatesMenuActions.UNAPPROVE,
+          icon: getStateIcon(EXPENSE_STATES.CREATED),
+          text: 'Remove approval'
+        }
+      ];
+    case EXPENSE_STATES.REIMBURSED:
+      return [
+        {
+          action: quickStatesMenuActions.UNREIMBURSE,
+          icon: getStateIcon(EXPENSE_STATES.APPROVED),
+          text: 'Remove reimbursement'
+        }
+      ];
+    default:
+      return [];
   }
 }
  
@@ -1189,6 +1411,11 @@ function useInactiveStyle(expense) {
 // |                     WATCHERS                     |
 // |                                                  |
 // |--------------------------------------------------|
+
+watch(
+  () => rejectingExpense.value,
+  () => console.log(rejectingExpense.value)
+)
 
 /**
  * watcher for employee, filter.active, filter.reimbursed - filters expenses
