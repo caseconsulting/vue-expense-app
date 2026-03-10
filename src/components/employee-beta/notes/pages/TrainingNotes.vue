f094486fa5af41932ac931edbaaf95548c1d510b
Luke Ogburn <21106956+logburn@users.noreply.github.com>
2026-03-10 11:06:30 -0400

    saving

--- src/components/employee-beta/notes/pages/TrainingNotes.vue
+++ src/components/employee-beta/notes/pages/TrainingNotes.vue
@@ -1,14 +1,20 @@
 <template>
   <v-col>
-    <v-progress-linear v-if="!trainings" indeterminate />
-    <p v-else-if="trainings.length === 0">No trainings found for employee.</p>
+    <v-progress-linear v-if="!filteredTrainings" indeterminate />
+    <p v-else-if="filteredTrainings.length === 0">No trainings found for employee.</p>
     <div v-else>
+      <v-autocomplete
+        :items="categories"
+        v-model="filters.categories"
+        multiple
+        chips
+      />
       <v-row>
         <v-col cols="3" class="font-weight-bold">Purchase Date</v-col>
         <v-col cols="3" class="font-weight-bold">Category</v-col>
         <v-col cols="6" class="font-weight-bold">Description</v-col>
       </v-row>
-      <v-row v-for="training in trainings" :key="training.id" class="clickable-row" @click="gotoExpense(training.id)">
+      <v-row v-for="training in filteredTrainings" :key="training.id" @click="gotoExpense(training.id)">
         <v-col cols="3">{{ format(training.purchaseDate, null, 'MMM DD, YYYY') }}</v-col>
         <v-col cols="3">{{ training.category ?? 'None' }}</v-col>
         <v-col cols="6">{{ training.description }}</v-col>
@@ -20,8 +26,11 @@
 <script setup>
 /**
  * TODO:
- *  - [ ] make for employee, not user (oops)
- *  - [ ] button to goto expense in MyExpenses?
+ *  - [x] make for employee, not user (oops)
+ *  - [ ] category filter with auto to what they likely want
+ *  - [ ] show training hour conversions
+ *  - [ ] summary of training hours conversions
+ *  - [ ] notes sectiond defauling to desc but editable (do not edit expense, just notes)
  */
 
  /*
@@ -41,7 +50,7 @@
 
  */
 
-import { ref, onMounted } from 'vue';
+import { ref, computed, onMounted } from 'vue';
 import { useStore } from 'vuex';
 import { updateStoreUser } from '@/utils/storeUtils';
 import { format, difference } from '@/shared/dateUtils';
@@ -49,8 +58,10 @@ import api from '@/shared/api';
 
 const props = defineProps(['modelValue', 'user']);
 const notes = ref(props.modelValue);
-const trainings = ref(null);
+const categories = ref([]);
+const filters = ref({ categories: null })
 const store = useStore();
+let trainings = null;
 
 onMounted(async () => {
   // fetch all data
@@ -58,19 +69,36 @@ onMounted(async () => {
   let [expenses, expenseTypes] = await Promise.all([api.getAllEmployeeExpenses(id), api.getEmployeeExpenseTypes(id)]);
   // get all training expense type IDs
   let trainingIds = new Set();
-  for (let et of expenseTypes)
-    if (et.budgetName.toLowerCase().includes('training'))
+  let allCategory = new Set();
+  for (let et of expenseTypes) {
+    if (et.budgetName.toLowerCase().includes('training')) {
       trainingIds.add(et.id);
+      for (let c of et.categories || []) allCategory.add(c.name);
+    }
+  }
+  // get categories
+  categories.value = Array.from(allCategory);
   // get all expenses with a training ET ID
-  trainings.value = expenses.filter((e) => trainingIds.has(e.expenseTypeId));
-  trainings.value = trainings.value.sort((a, b) => difference(b.purchaseDate, a.purchaseDate, 'day'));
+  trainings = expenses.filter((e) => trainingIds.has(e.expenseTypeId));
+  trainings = trainings.sort((a, b) => difference(b.purchaseDate, a.purchaseDate, 'day'));
+});
+
+const filteredTrainings = computed(() => {
+  if (!trainings) return null;
+
+  let filtered = [];
+  let add = (item) => filtered.push(item);
+  for (let t of trainings) {
+    // categories filter
+    if (!filters.value.categories?.length) add(t);
+    for (let c of filters.value.categories || []) {
+      if (t.category === c) {
+        add(t);
+        continue;
+      }
+    }
+  }
+
+  return filtered;
 });
 </script>
-
-<style scoped>
-.clickable-row:hover {
-  cursor: pointer;
-  background-color: #f0f0f0;
-  border-radius: 0.7em;
-}
-</style>
