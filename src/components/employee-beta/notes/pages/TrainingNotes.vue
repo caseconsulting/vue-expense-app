61a964cf9aba2a45a85af9526f28f9f96022d797
Luke Ogburn <21106956+logburn@users.noreply.github.com>
2026-03-13 11:00:41 -0400

    saving

--- src/components/employee-beta/notes/pages/TrainingNotes.vue
+++ src/components/employee-beta/notes/pages/TrainingNotes.vue
@@ -1,24 +1,32 @@
 <template>
   <v-col>
     <v-progress-linear v-if="!filteredTrainings" indeterminate />
-    <p v-else-if="filteredTrainings.length === 0">No trainings found for employee.</p>
     <div v-else>
+      <div class="mb-4">
+        <strong>Training Hour Conversions:</strong> ${{ trainingHours.pending.toFixed(2) }} pending, ${{ trainingHours.reimbursed.toFixed(2) }} reimbursed
+      </div>
       <v-autocomplete
         :items="categories"
         v-model="filters.categories"
+        variant="outlined"
+        density="compact"
         multiple
         chips
+        closable-chips
       />
-      <v-row>
-        <v-col cols="3" class="font-weight-bold">Purchase Date</v-col>
-        <v-col cols="3" class="font-weight-bold">Category</v-col>
-        <v-col cols="6" class="font-weight-bold">Description</v-col>
-      </v-row>
-      <v-row v-for="training in filteredTrainings" :key="training.id" @click="gotoExpense(training.id)">
-        <v-col cols="3">{{ format(training.purchaseDate, null, 'MMM DD, YYYY') }}</v-col>
-        <v-col cols="3">{{ training.category ?? 'None' }}</v-col>
-        <v-col cols="6">{{ training.description }}</v-col>
-      </v-row>
+      <div>
+        <v-row>
+          <v-col cols="3" class="font-weight-bold">Purchase Date</v-col>
+          <v-col cols="3" class="font-weight-bold">Category</v-col>
+          <v-col cols="6" class="font-weight-bold">Description</v-col>
+        </v-row>
+        <p v-if="filteredTrainings.length === 0" class="mt-4">No trainings found.</p>
+        <v-row v-else v-for="training in filteredTrainings" :key="training.id" @click="gotoExpense(training.id)">
+          <v-col cols="3">{{ format(training.purchaseDate, null, 'MMM DD, YYYY') }}</v-col>
+          <v-col cols="3">{{ training.category ?? 'None' }}</v-col>
+          <v-col cols="6">{{ training.description }}</v-col>
+        </v-row>
+      </div>
     </div>
   </v-col>
 </template>
@@ -27,10 +35,10 @@
 /**
  * TODO:
  *  - [x] make for employee, not user (oops)
- *  - [ ] category filter with auto to what they likely want
- *  - [ ] show training hour conversions
- *  - [ ] summary of training hours conversions
- *  - [ ] notes sectiond defauling to desc but editable (do not edit expense, just notes)
+ *  - [x] category filter with auto to what they likely want
+ *  - [x] show training hour conversions
+ *  - [x] summary of training hours conversions
+ *  - [ ] notes section defauling to desc but editable (do not edit expense, just notes)
  */
 
  /*
@@ -46,7 +54,8 @@
  "state": "CREATED",
  "cost": 9,
  "category": "Certifications,",
- "receipt": [ "Portal_Report_(Ragnarok_Technologies).xlsx" ] }
+ "receipt": [ "Portal_Report_(Ragnarok_Technologies).xlsx" ]
+ }
 
  */
 
@@ -55,13 +64,17 @@ import { useStore } from 'vuex';
 import { updateStoreUser } from '@/utils/storeUtils';
 import { format, difference } from '@/shared/dateUtils';
 import api from '@/shared/api';
+import { EXPENSE_STATES } from '@/shared/expenseUtils';
 
 const props = defineProps(['modelValue', 'user']);
 const notes = ref(props.modelValue);
 const categories = ref([]);
-const filters = ref({ categories: null })
+const filters = ref({
+  categories: ['Certifications', 'Training', 'Exchange for Training Hours', 'Conferences']
+})
 const store = useStore();
-let trainings = null;
+let trainings = ref(null);
+const trainingHours = ref({ pending: 0, reimbursed: 0 })
 
 onMounted(async () => {
   // fetch all data
@@ -73,22 +86,34 @@ onMounted(async () => {
   for (let et of expenseTypes) {
     if (et.budgetName.toLowerCase().includes('training')) {
       trainingIds.add(et.id);
-      for (let c of et.categories || []) allCategory.add(c.name);
+      for (let c of et.categories || []) {
+        // add to category list
+        allCategory.add(c.name);
+      }
     }
   }
+
+  // get exchanges for training hours
+  for (let e of expenses) {
+    if (e.category?.toLowerCase() === 'exchange for training hours') {
+      if (e.state === EXPENSE_STATES.REIMBURSED) trainingHours.value.reimbursed += e.cost;
+      else trainingHours.value.pending += e.cost
+    }
+  }
+
   // get categories
   categories.value = Array.from(allCategory);
   // get all expenses with a training ET ID
-  trainings = expenses.filter((e) => trainingIds.has(e.expenseTypeId));
-  trainings = trainings.sort((a, b) => difference(b.purchaseDate, a.purchaseDate, 'day'));
+  trainings.value = expenses.filter((e) => trainingIds.has(e.expenseTypeId));
+  trainings.value = trainings.value.sort((a, b) => difference(b.purchaseDate, a.purchaseDate, 'day'));
 });
 
 const filteredTrainings = computed(() => {
-  if (!trainings) return null;
+  if (!trainings.value) return null;
 
   let filtered = [];
   let add = (item) => filtered.push(item);
-  for (let t of trainings) {
+  for (let t of trainings.value) {
     // categories filter
     if (!filters.value.categories?.length) add(t);
     for (let c of filters.value.categories || []) {
